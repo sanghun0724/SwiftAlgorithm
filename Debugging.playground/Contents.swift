634046f72833b55131cfbe7cc717f18562dbb718
sangheonlee <sanghun0724@naver.com>
2020-10-09 22:02:45 +0900

    d

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,12 +1,7 @@
 import Foundation
 
-func minimumRemovals(_ arr: [Int]) -> Int {
-    var result:Int = 0
-    for i in arr {
-        result+=i
-        if result % 2 == 0 {
-            return 0
-        }
-    }
- return 1
+func additiveInverse(_ arr: [Int]) -> [Int] {
+   return arr.map{ $0 * -1 }
+    
 }
+additiveInverse([1,2,3,4,5])
