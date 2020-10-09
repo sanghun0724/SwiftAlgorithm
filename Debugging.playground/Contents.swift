40082ac8faeb15da0a26578645b90c851eecbea6
sangheonlee <sanghun0724@naver.com>
2020-10-09 22:21:12 +0900

    d

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,7 +1,16 @@
 import Foundation
 
-func additiveInverse(_ arr: [Int]) -> [Int] {
-   return arr.map{ $0 * -1 }
+func transform(_ arr: [Int]) -> [Int] {
+    return arr.map {
+        if $0 % 2 == 0 {
+           return $0 + 1
+        }
+        else {
+           return $0 - 1
+        }
+            
+    }
+    
+    
     
 }
-additiveInverse([1,2,3,4,5])
