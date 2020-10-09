d142989b1f2b4d738ab286bd4302f89772c50381
sangheonlee <sanghun0724@naver.com>
2020-10-09 21:16:20 +0900

    e

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,21 +1,13 @@
 import Foundation
 
-//func solution(_ array:[Int], _ commands:[[Int]]) -> [Int] {
-//
-//
-//
-//}
+func isFourLetters(_ arr: [String]) -> [String] {
+    
+    let result:[String] =  arr.filter{$0.count == 4 }
+    return result
+    
+    
+    
+     }
 
-func equal(_ a: Int, _ b: Int, _ c: Int) -> Int {
-    var count:Int = 0
-    var result:[Int] = [a,b,c]
-  
-    for i in result {
-        if a == i {
-            count+=1
-        }
-    }
-    count-=1
-    return count
-}
+isFourLetters(["2","3333"])
 
