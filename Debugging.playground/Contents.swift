a29fd9224172788fbf9012b4c3ee646e219af850
sangheonlee <sanghun0724@naver.com>
2020-10-10 12:15:14 +0900

    d

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,16 +1,26 @@
 import Foundation
 
-func transform(_ arr: [Int]) -> [Int] {
-    return arr.map {
-        if $0 % 2 == 0 {
-           return $0 + 1
+func same(_ a1: [Int], _ a2: [Int]) -> Bool {
+    var result1:Int = a1.count
+    var result2:Int = a2.count
+    for i in a1 {
+        for j in a1{
+            if i == j  {
+                result1-=1
+            }
         }
-        else {
-           return $0 - 1
-        }
-            
     }
-    
-    
-    
+    for i in a2 {
+        for j in a2{
+            if i == j  {
+                result2-=1
+            }
+        }
+    }
+    if result1 == result2 { return true}
+    return false
 }
+    
+    
+    
+
