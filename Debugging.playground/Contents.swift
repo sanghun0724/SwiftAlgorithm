d57dfb35e859f6293baacec8a19a4aa6f24a5929
sangheonlee <sanghun0724@naver.com>
2020-10-10 20:15:30 +0900

    dd

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,26 +1,12 @@
 import Foundation
 
-func same(_ a1: [Int], _ a2: [Int]) -> Bool {
-    var result1:Int = a1.count
-    var result2:Int = a2.count
-    for i in a1 {
-        for j in a1{
-            if i == j  {
-                result1-=1
-            }
-        }
+
+func checkPalindrome(_ str: String) -> Bool {
+    if str[str.startIndex] == str[str.index(before: str.endIndex)] {
+        return true
     }
-    for i in a2 {
-        for j in a2{
-            if i == j  {
-                result2-=1
-            }
-        }
-    }
-    if result1 == result2 { return true}
     return false
 }
-    
     
     
 
