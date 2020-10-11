2d66015837793bca4125cc4b2719da5e0f24c3f3
sangheonlee <sanghun0724@naver.com>
2020-10-11 21:19:40 +0900

    ㅌ

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,10 +1,14 @@
 import Foundation
 
 
-func getAbsSum(_ arr: [Int]) -> Int {
-    return arr.reduce(0) {
-        $0 + abs($1)
+func isOmnipresent(_ arr: [[Int]], _ val: Int) -> Bool {
+    for i in arr {
+        for j in i  {
+            if i.contains(j) == false {
+                return false
+            }
+        }
     }
-    
-    
+    return true
 }
+isOmnipresent(<#T##arr: [[Int]]##[[Int]]#>, <#T##val: Int##Int#>)
