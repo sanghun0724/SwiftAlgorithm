ddeabebc210dfebc8c1233890dac904568392ea6
sangheonlee <sanghun0724@naver.com>
2020-10-11 19:34:57 +0900

    d

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,7 +1,15 @@
 import Foundation
 
 
-func oddProduct(_ arr: [Int]) -> Int {
-    return arr.filter{ $0 % 2 != 0}.reduce(0, { $0 * $1 } )
+func isIdentical(_ s: String) -> Bool {
+    var result:[Character] = []
+    for i in s {
+        result.append(i)
+    }
+    if Set(result).count != 1 {
+        return false
+    }
+    return true
     
 }
+isIdentical("sssssssa")
