fdd6ff313c819311cedc3d340fdcf1dc63da5a2e
sangheonlee <sanghun0724@naver.com>
2020-10-12 21:49:35 +0900

    d

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,8 +1,6 @@
 import Foundation
 
-
-func myPi(_ n: Int) -> Double {
-    var product:Double = pow(10, Double(n))
-    return round((Double.pi) * product) / product
+func isStrangePair(_ str1: String, _ str2: String) -> Bool {
+    return str1[str1.startIndex] == str2[str2.index(before: str2.endIndex)] && str1[str1.index(before: str1.endIndex)] == str2[str2.startIndex]
+    
 }
-myPi(3)
