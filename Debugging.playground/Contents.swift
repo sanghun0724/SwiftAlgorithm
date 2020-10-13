d83d686119be2918051fe7d190c5a7c22d270a6d
sangheonlee <sanghun0724@naver.com>
2020-10-13 23:04:51 +0900

    oh my god

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,7 +1,22 @@
 import Foundation
 
-func dictionary(_ initial: String, _ words: [String]) -> [String] {
-    return words.filter{
-        $0.hasPrefix(initial)
+enum CalculatorError: Error {
+    case zeroDivision
+}
+
+func calculator(_ n: Int, _ op: String, _ m: Int) throws -> Int {
+    switch op {
+    case  "+":
+        return n + m
+    case  "-":
+        return n - m
+    case  "/":
+        if m == 0 {CalculatorError.zeroDivision }
+        return n / m
+    case  "*":
+        return n * m
+    default:
+        print("hi")
     }
+    return n
 }
