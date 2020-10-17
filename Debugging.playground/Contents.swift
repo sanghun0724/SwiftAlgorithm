6d6fd7c6e3bfb9fdf85b6b8fe6a3c96f0dfe4e9c
sangheonlee <sanghun0724@naver.com>
2020-10-17 18:04:10 +0900

    ㅇ

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,7 +1,30 @@
 import Foundation
 
-func charCount(_ myChar: String, _ str: String) -> Int {
-    
-    NSAssertionHandler.willChange(.current)
-    
+enum VendingMachineError:Error  {
+  case invalidInput
+  case insufficientFunds(moneyNeeded: Int)
+  case outOfStock
+}
+
+class VendingMachine {
+  var deposited: Int = 0
+
+  func receiveMoney(_ money: Int) throws {
+    guard money > 0 else {
+       throw VendingMachineError.invalidInput
+    }
+    self.deposited += money
+    print("\(money)원 받음")
+  }
+}
+let machine: VendingMachine = VendingMachine()
+
+do {
+  try machine.receiveMoney(0)
+} catch VendingMachineError.invalidInput {
+  print("입력이 잘못되었습니다")
+} catch VendingMachineError.insufficientFunds(let moneyNeeded) {
+  print("\(moneyNeeded)원이 부족합니다")
+} catch VendingMachineError.outOfStock {
+  print("수량이 부족합니다")
 }
