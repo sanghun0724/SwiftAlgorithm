a756f9c1507a5472d95fb764b9005e12daf87a4d
sangheonlee <sanghun0724@naver.com>
2020-10-22 15:38:46 +0900

    go

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,4 +1,6 @@
 import Foundation
-func solution(_ num:Int) -> String {
-    return num % 2 == 0 ? "Even":"Odd"
-}
+let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
+let (a, b) = (n[0], n[1])
+
+print(a + b)
+ 
