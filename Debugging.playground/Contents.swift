3d21a69a91f02f09db448f9fadf56d7b267de403
sangheonlee <sanghun0724@naver.com>
2020-10-24 16:22:01 +0900

    ㅇ

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,5 +1,12 @@
 import Foundation
 func solution(_ n:Int) -> Int {
-    return 0
+    var result:Int = 0
+    for i in 1...n {
+        if n % i == 0 {
+            result+=i
+        }
+    }
+    if n == 0 {return 0}
+    return result
 }
-
+solution(3000)
