b35523d357ddde276d338c08cb5d9bdcf4c64462
sangheonlee <sanghun0724@naver.com>
2020-10-24 23:43:26 +0900

    내림차순

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,12 +1,5 @@
 import Foundation
-func solution(_ n:Int) -> Int {
-    var result:Int = 0
-    for i in 1...n {
-        if n % i == 0 {
-            result+=i
-        }
-    }
-    if n == 0 {return 0}
-    return result
+func solution(_ s:String) -> String {
+    return String(s.sorted(by: >))
 }
-solution(3000)
+solution("Zwdsad")
