883e08e8b0ba05c6f75f0a27f9b5eaa8b2d3faca
sangheonlee <sanghun0724@naver.com>
2020-10-26 16:37:12 +0900

    save

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,7 +1,12 @@
 import Foundation
-func solution(_ arr:[Int], _ divisor:Int) -> [Int] {
-   
-    var result = arr.filter{ $0 % divisor == 0}.sorted()
-    
-    return result.isEmpty ?  [-1]: result
+func solution(_ n:Int64) -> Int64 {
+    var r:Int64 = 1
+    var sub = String(n).map{Int64(String($0))!}.sorted(by: <)
+    var result:Int64 = 0
+    for i in sub {
+         result+=Int64(i * r)
+        r*=10
+    }
+    return result
 }
+solution(118372)
