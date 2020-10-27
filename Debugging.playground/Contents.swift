f0a6cd71d337edf5feb792cc993e0b6b5a5051ac
sangheonlee <sanghun0724@naver.com>
2020-10-27 13:45:43 +0900

    gg

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,12 +1,22 @@
 import Foundation
-func solution(_ n:Int64) -> Int64 {
-   var x = 1
-    while x*x <= n {
-        if x*x == n {
-             return Int64((x+1)*(x+1))
+func solution(_ s:String, _ n:Int) -> String {
+    let opted = s.unicodeScalars
+      .map { unicodeScala -> Int in
+        let number = Int(unicodeScala.value)
+        switch number  {
+        case 65...90: return (number - 65 + n) % 26 + 65
+        case 97...122: return (number - 97 + n) % 26 + 97
+        case 32: return number
+        default:
+          print("NG")
+          return number
         }
-        x+=1
-    }
-    return -1
+      }
+      .map { Unicode.Scalar.init($0) }
+      .flatMap { $0 }
+      .map { Character.init($0) }
+   
+    return String(opted)
+    
 }
-solution(121)
+solution("e F d", 1)
