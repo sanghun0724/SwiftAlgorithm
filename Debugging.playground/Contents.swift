531f3d47c6dbef3b5ad4bbb48ca0d32b060d78ab
sangheonlee <sanghun0724@naver.com>
2020-10-27 16:12:12 +0900

    answer

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,16 +1,6 @@
 import Foundation
 func solution(_ s:String) -> String {
-   var result = s.components(separatedBy: " ")
-   var result2 = ""
-    for i in result {
-        result2.append(" ")
-        for (index,value) in i.enumerated()  {
-            if index % 2 == 0 {
-                result2.append(value.uppercased())
-            }
-            else {result2.append(value.lowercased()) }
-        }
-    }
-    result2.removeFirst()
-    return result2
+    let a = s.components(separatedBy: " ").map { $0.enumerated().map { $0.offset % 2 == 0 ? $0.element.uppercased() : $0.element.lowercased() } }
+    return a.map{ $0.map { $0 }.joined() }.joined(separator: " ")
 }
+
