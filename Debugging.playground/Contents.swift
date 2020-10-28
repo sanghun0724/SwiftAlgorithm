1efc3b74bfd8fb8957d497120bb2c827947359cf
sangheonlee <sanghun0724@naver.com>
2020-10-28 20:11:48 +0900

    d

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,6 +1,4 @@
 import Foundation
-func solution(_ s:String) -> String {
-    let a = s.components(separatedBy: " ").map { $0.enumerated().map { $0.offset % 2 == 0 ? $0.element.uppercased() : $0.element.lowercased() } }
-    return a.map{ $0.map { $0 }.joined() }.joined(separator: " ")
+func solution(_ n:Int) -> Int {
+   return Array(1...n).filter{ n / $0 != 0 }.count
 }
-
