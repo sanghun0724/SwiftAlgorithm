8ad5725d2fea10324fc12d2f59ab1855830ff8bc
sangheonlee <sanghun0724@naver.com>
2020-10-29 15:34:56 +0900

    fucking

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,14 +1,32 @@
 import Foundation
-func solution(_ n:Int) -> Int {
-    var count = 0
-   var arr = Array.init(repeating: false, count: n)
-    for i in 2...n {
-        if arr[i] == false{
-            count+=1
-        }
-        for j in stride(from: 2, through: n, by: i) {
-            arr[j] = true
+//func solution(_ n:Int) -> Int {
+//    var count = 0
+//   var arr = Array.init(repeating: false, count: n+1)
+//    for i in 2...n {
+//        if arr[i] == false{
+//            count+=1
+//        }
+//        for j in stride(from: 2, through: n, by: i) {
+//            arr[j] = true
+//        }
+//    }
+//    return count
+//}
+func solution(_ n:Int, _ m:Int) -> [Int] {
+    let first =  Array(1...n).filter{$0 % n == 0}
+    let second = Array(1...m).filter{$0 % n == 0}
+    var result:[Int] = []
+    for i in first  {
+        for j in second.reversed()  {
+            if i == j  { result.append(j); break; }
         }
     }
-    return count
+    for i in 1...10000000  {
+        if n * i ==  m * i {
+            result.append(n*i)
+        }
+    }
+    return result
+    
 }
+solution(2, 4)
