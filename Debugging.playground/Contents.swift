d16e39924932ce5cd61bd61a7f7041c7c9fef902
sangheonlee <sanghun0724@naver.com>
2020-10-31 01:50:46 +0900

    d

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,28 +1,11 @@
 import Foundation
-//func solution(_ n:Int) -> Int {
-//    var count = 0
-//   var arr = Array.init(repeating: false, count: n+1)
-//    for i in 2...n {
-//        if arr[i] == false{
-//            count+=1
-//        }
-//        for j in stride(from: 2, through: n, by: i) {
-//            arr[j] = true
-//        }
-//    }
-//    return count
-//}
-func solution(_ n:Int, _ m:Int) -> [Int] {
-    func gcd(_ min:Int , _ max:Int) -> Int {
-        return min % max == 0 ? max : gcd(max, min % max)
+func solution(_ strings:[String], _ n:Int) -> [String] {
+    var comparison:[String] = []
+    for i in strings {
+        comparison.append(String(i[i.index(i.startIndex,offsetBy: n)]))
     }
-    func lcm (_ a:Int,_ b:Int) -> Int {
-        return a * b  / gcd(a, b)
+    comparison.sort()
+    func duplication(_a:[String]) -> Bool {
+        
     }
-    func result (n:Int,m:Int) ->[Int] {
-        return [gcd(n, m),lcm(n, m)]
-    }
-    return result(n: n, m: m)
-    
 }
-solution(2, 4)
