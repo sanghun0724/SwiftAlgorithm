59a52f245267281edfde9973fb03ab058ef63fa0
sangheonlee <sanghun0724@naver.com>
2020-10-31 22:10:27 +0900

    good

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,11 +1,5 @@
 import Foundation
 func solution(_ strings:[String], _ n:Int) -> [String] {
-    var comparison:[String] = []
-    for i in strings {
-        comparison.append(String(i[i.index(i.startIndex,offsetBy: n)]))
-    }
-    comparison.sort()
-    func duplication(_a:[String]) -> Bool {
-        
-    }
+    var dic:[String:String] = [:]
+    
 }
