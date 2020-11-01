906a410e49546e0eb225c50aa0c33ff642f81389
sangheonlee <sanghun0724@naver.com>
2020-11-01 12:37:33 +0900

    d

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,5 +1,14 @@
 import Foundation
 func solution(_ strings:[String], _ n:Int) -> [String] {
-    var dic:[String:String] = [:]
-    
+    let index = strings[0].index(strings[0].startIndex,offsetBy: n)
+    let result = strings.sorted(by: {(a:String,b:String) -> Bool in
+        if a[index] == b[index] {
+               return a < b
+        }
+        else {
+          return a[index] < b[index]
+        }
+    })
+   return result
 }
+solution(["sun","bed","car"], 1)
