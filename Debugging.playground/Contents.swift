e4b6cd5267a7fab1467a4f717598d1d9592a9cb1
sangheonlee <sanghun0724@naver.com>
2020-11-02 18:30:44 +0900

    good

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,5 +1,8 @@
 import Foundation
 func solution(_ x:Int) -> Bool {
-    var result = []
-    
+    var result =  Array(String(x)).map{Int(String($0))!}
+    return  x % result.reduce(0,+) == 0 ? true : false
 }
+solution(12)
+var i = 12
+print (String(i))
