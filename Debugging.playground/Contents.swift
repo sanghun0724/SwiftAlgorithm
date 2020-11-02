ff224340a870c7918d02169dd74e408baed1010e
sangheonlee <sanghun0724@naver.com>
2020-11-02 16:25:41 +0900

    good

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,12 +1,5 @@
 import Foundation
-func solution(_ arr:[Int]) -> [Int] {
-    var arr1 = arr
-    var result = arr.sorted()
-   var check = result.removeFirst()
-    for (i,v) in arr.enumerated() {
-        if check == v {
-            arr1.remove(at: i)
-        }
-    }
-   return arr1.isEmpty ? [-1]:arr1
+func solution(_ x:Int) -> Bool {
+    var result = []
+    
 }
