f550cfc294fb1616b7dcf0e67b726ebf702d5a8d
sangheonlee <sanghun0724@naver.com>
2020-11-03 18:50:53 +0900

    어려운게아닌데말이지..

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,8 +1,25 @@
 import Foundation
-func solution(_ x:Int) -> Bool {
-    var result =  Array(String(x)).map{Int(String($0))!}
-    return  x % result.reduce(0,+) == 0 ? true : false
+func solution(_ num:Int) -> Int {
+    var number = num
+    var count = 0
+    
+    func even<T>(_ a:T) -> T {
+        number = a as! Int / 2
+         count += 1
+        if count < 500 {
+            return number % 2 == 0 ? even(number) : odd(number)
+        }
+    }
+    
+    func odd<T>(_ b:T) -> T {
+        number = b as! Int * 3 + 1
+        count += 1
+        if count < 500 {
+            return number % 2 == 0 ? even(number) : odd(number)
+        }
+    }
+    if count >= 500 { return -1 }
+    number % 2 == 0 ? even(number) : odd(number)
+    
+    return count
 }
-solution(12)
-var i = 12
-print (String(i))
