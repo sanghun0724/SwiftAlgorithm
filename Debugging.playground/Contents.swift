bf9ac8b84e03043b6284155478e279b55d25a232
sanghun0724 <https://github.com/sanghun0724>
2020-11-05 23:07:36 +0900

    d

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -9,17 +9,7 @@ func solution(_ num:Int) -> Int {
         if count < 500 {
             return number % 2 == 0 ? even(number) : odd(number)
         }
-    }
-    
-    func odd<T>(_ b:T) -> T {
-        number = b as! Int * 3 + 1
-        count += 1
-        if count < 500 {
-            return number % 2 == 0 ? even(number) : odd(number)
-        }
-    }
-    if count >= 500 { return -1 }
-    number % 2 == 0 ? even(number) : odd(number)
+  
     
     return count
 }
