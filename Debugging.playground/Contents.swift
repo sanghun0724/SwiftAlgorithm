e145d38b8a12b1eb5a24cf8979d8ba27356e56d5
sanghun0724 <sanghun0724@naver.com>
2020-11-24 16:19:27 +0900

    date

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,15 +1,17 @@
 import Foundation
-func solution(_ num:Int) -> Int {
-    var number = num
-    var count = 0
+
+func solution(_ a:Int, _ b:Int) -> String {
     
-    func even<T>(_ a:T) -> T {
-        number = a as! Int / 2
-         count += 1
-        if count < 500 {
-            return number % 2 == 0 ? even(number) : odd(number)
-        }
-  
+    var dateCount = 0
+    let weekend = ["THU","FRI","SAT","SUN","MON","TUE","WED"]
+    let date = [31,29,31,30,31,30,30,31,30,29,30,31]
     
-    return count
+    for i in stride(from: 1, to: a-1, by: 1) {
+        dateCount+=date[i]
+    }
+    dateCount += b
+    
+    return weekend[dateCount%7]
+    
 }
+solution(1, 12)
