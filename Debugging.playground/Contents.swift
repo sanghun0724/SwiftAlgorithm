65076057c88d2e1a8a2e58ecefc61084d7c1ba04
sanghun0724 <sanghun0724@naver.com>
2020-12-17 17:34:27 +0900

    Fix

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,20 +1,30 @@
 import Foundation
-
 class Solution {
-    func isPalindrome(_ x: Int) -> Bool {
-        var result = x
-        var revertedNumber = 0
-        if result < 0 || (result % 10 == 0 && result != 0) {
-            return false
+    let numerals: [Character:Int] = ["I": 1,
+                                     "V": 5,
+                                     "X": 10,
+                                     "L": 50,
+                                     "C": 100,
+                                     "D": 500,
+                                     "M": 1000]
+
+    func romanToInt(_ s: String) -> Int {
+        let array = Array(s)
+        var i = 0
+        var num = 0
+
+        while i < array.count - 1 {
+
+            let val = numerals[array[i]]!
+            let next = numerals[array[i+1]]!
+            num += (val < next) ? -val : val
+
+            i += 1
         }
-        
-        while result > revertedNumber {
-            revertedNumber = revertedNumber * 10 + result % 10
-            result /= 10
-        }
-    
-        return result == revertedNumber || result == revertedNumber/10
+        num += numerals[array.last!]!
+
+        return num
     }
 }
-
-
+ var a = Solution()
+a.romanToInt("MCMXCIV")
