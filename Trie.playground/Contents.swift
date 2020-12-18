e5c5f6986eebfc9fba301243c126e2c4e3ce1451
sanghun0724 <sanghun0724@naver.com>
2020-12-18 21:52:34 +0900

    Update

--- Trie.playground/Contents.swift
+++ Trie.playground/Contents.swift
@@ -69,9 +69,47 @@ public class Trie<CollectionType : Collection> where CollectionType.Element : Ha
         current.isTerminating = false //dot 먼저 지워줌
         while current.children.isEmpty && !current.isTerminating {
             //1.자식노드가 있으면 중복되지만 어 긴 단어가 있다는 것이므로 삭제 x
-            //2. dot 찍혀 있으면 다른 단어가 있는 것이니 더이상 삭제 x 
+            //2. dot 찍혀 있으면 다른 단어가 있는 것이니 더이상 삭제 x
+            current.parent!.children[current.key!] = nil
+            current = current.parent!
         }
+    }
+}
+    //collections 메소드 구현- 핵심: 접두사로 검색하면 검색되는 단어 반환해주는 메소드
+    //RangeReplacebleCollection 프로토콜 채택한 이유는 prefix에 append 메소드 쓰기 위해서
+    //RangeReplacebleCollection 을 채택하지 않으면 append 메소드 쓸 수 없음. 우리가 쓰는 배열이 이 프로토콜을 채택해서 append 메소드 쓸 수 있는 거임
+    //재귀호출로 반복시킬 예정  (재귀호출은 종료조건과 문제의 범위가 작은 방향으로 간다는 조건을 만족 해야함)
+extension Trie where CollectionType : RangeReplaceableCollection {
+        //RangeReplaceableCollection 프로토콜은 Collection 프로토콜을 상속한 프로토콜
+        // Array 의 append 메소드는 Array가 RangeReplacebleCollection 프로토콜을 상속해서 쓸수 있는 것
+        public func collections(startingWith prefix: CollectionType) ->[CollectionType] {
+            var current = root
+            for element in prefix {
+                guard let child = current.children[element] else { return [] } //if문 실행
+                current = child
+            }
+            return collections(startingWith: prefix,after: current)
+            //접두어 끝이 현재의 현재노드니깐 현재노드에 딸려있는 단어들 배열로 가져올 메소드 따로 만들자.
+        }
+    private func collections(startingWith prefix: CollectionType, after node: Node) -> [CollectionType] {
+        var result = [CollectionType]() // 단어들 여기에 담을거
+        if node.isTerminating { // prefix 자체가 한단어이면 반환배열에 포함
+            result.append(prefix)
+        }
+        for child in node.children.values {
+            //values 는 dictionary 구조체의 프로퍼티
+            //자식 노드의 수만큼만 반복 -> 자식노드 없으면 반복 안함
+            var prefix = prefix
+            prefix.append(child.key!)
+            //자식노드 있는지 확인 안하고 강제 언래핑 가능. 반복 횟수가 이미 정해져 있으니깐
+            //prefix는 RangeReplacebleCollection 프로토콜을 채택한 타입이기 때문에 append 사용가능
+            result.append(contentsOf: collections(startingWith: prefix, after: child))
+            //재귀호출 시작: 1. 종료조건 : 반복문의 node.children.values 이 0이면 종료 ->만족
+            // 2. 작은방향으로 이루어 지는지 : prefix 가 커져서 트리의 자식노드인 하위로 이동하므로 문제의 범위는 작아짐 -> 만족
+        }
+        return result
+    }
     }
     
     
-}
+
