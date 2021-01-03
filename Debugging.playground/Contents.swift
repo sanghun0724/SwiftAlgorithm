077fb6a9d4c42589a77e8ba30b58d4c58765b463
sanghun0724 <sanghun0724@naver.com>
2021-01-03 16:11:07 +0900

    list

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,24 +1,34 @@
 import Foundation
 
-    func isValid(_ s: String) -> Bool {
-        //stack 사용!
-        var arr = [Character]()
-        for i in s {
-            switch i {
-            case ")":
-                if arr.isEmpty {return false}
-                if arr.removeLast() != "(" {return false}
-            case "}":
-                if arr.isEmpty {return false}
-                if arr.removeLast() != "{" {return false}
-            case "]":
-                if arr.isEmpty {return false}
-                if arr.removeLast() != "[" {return false}
-            default:
-                arr.append(i)
-            }
-        }
-        return arr.isEmpty
-    }
+public class ListNode {
+      public var val: Int
+      public var next: ListNode?
+     public init() { self.val = 0; self.next = nil; }
+     public init(_ val: Int) { self.val = val; self.next = nil; }
+     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+  }
 
-isValid("{ ([]) }")
+func mergeTwoLists( l1: inout ListNode?, l2: inout ListNode?) -> ListNode? {
+        var l = ListNode(0)
+        var p = l
+        while(l1 != nil && l2 != nil) {
+            if(l1!.val < l2!.val) {
+                p.next = l1
+                l1 = l1!.next
+            }
+            else {
+                p.next = l2
+                l2 = l2!.next
+            }
+            p = p.next!
+        }
+        if(l1 != nil) {
+            p.next = l1!
+        }
+        if(l2 != nil) {
+            p.next = l2!
+        }
+        
+        return l.next
+    }
+mergeTwoLists(l1: &[1,3,5], l2: &[2,4,5])
