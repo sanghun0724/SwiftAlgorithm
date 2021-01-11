64e56cd9a3b257846656653ed6e6f499dff56136
sanghun0724 <sanghun0724@naver.com>
2021-01-11 21:01:20 +0900

    MergeSorting

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,13 +1,6 @@
 import Foundation
 
-func searchInsert(_ nums: [Int], _ target: Int) -> Int {
-    var another = nums
-    if nums.contains(target) {
-        return nums.firstIndex(of: target)!
-    } else  {
-       another.append(target)
-        another.sort()
-        return another.firstIndex(of: target)!
-    }
-   }
 
+func noting () {
+     return 
+}
