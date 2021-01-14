26f6a4c885ee30ad6bc47c98357832c81ee78aa6
sanghun0724 <sanghun0724@naver.com>
2021-01-14 22:16:12 +0900

    algorithm

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,7 +1,16 @@
 import Foundation
 
 
-func maxSubArray(_ nums: [Int]) -> Int {
+    func maxSubArray(_ nums: [Int]) -> Int {
+        var m = 0
+        var result = Int.min
+        for i in 0..<nums.count {
+            m = max(nums[i], nums[i] + m)
+            result = max(result, m)
+        }
         
+        return result
     }
 
+
+ maxSubArray([-2,1,-3,4,-1,2,1,-5,4])
