7aa12b7660f20c3b0c42e358891f0a30a4cb3d3e
sanghun0724 <sanghun0724@naver.com>
2021-01-15 22:06:31 +0900

    fix

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,19 +1,36 @@
 import Foundation
 
-
-    func plusOne(_ digits: [Int]) -> [Int] {
-        var result = digits
-        var check = true
-        for i in digits {
-            if i < 0 {
-                check = false
-                break
-            }
+func addBinary(_ a: String, _ b: String) -> String {
+       var result = [Int]()
+    
+    let arrayA = a.compactMap{Int(String($0))}
+    let arrayB = b.compactMap{Int(String($0))}
+    
+    var aIndex = arrayA.count - 1
+    var bIndex = arrayB.count - 1
+    
+    var carry = 0
+    
+    while aIndex >= 0 || bIndex >= 0 {
+        
+        if aIndex >= 0 {
+            carry += arrayA[aIndex]
         }
-        if check == true {
-           var k = result.removeLast()
-            result.append(k+1)
+        
+        if bIndex >= 0 {
+            carry += arrayB[bIndex]
         }
-        return result
+        
+        result.append(carry % 2)
+        
+        carry = carry >= 2 ? 1 : 0
+        
+        aIndex -= 1
+        bIndex -= 1
     }
+    if carry != 0 {result.append(1)}
+    var a = result.reversed().map(String.init)
+    return result.reversed().map(String.init).joined()
+   }
 
+addBinary("11", "1")
