4636395227d1c05efc3dbf7e4d5720bd26985091
sanghun0724 <sanghun0724@naver.com>
2021-01-16 00:51:12 +0900

    DP

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,36 +1,14 @@
-import Foundation
 
-func addBinary(_ a: String, _ b: String) -> String {
-       var result = [Int]()
-    
-    let arrayA = a.compactMap{Int(String($0))}
-    let arrayB = b.compactMap{Int(String($0))}
-    
-    var aIndex = arrayA.count - 1
-    var bIndex = arrayB.count - 1
-    
-    var carry = 0
-    
-    while aIndex >= 0 || bIndex >= 0 {
+    func climbStairs(_ n: Int) -> Int {
+        if n == 1 { return 1}
+       
+        var dpArray:[Int] = Array(repeating: 0, count: n+1)
         
-        if aIndex >= 0 {
-            carry += arrayA[aIndex]
+        dpArray[0] = 1
+        dpArray[1] = 1
+        for i in 2...n {
+            dpArray[i] = dpArray[i - 1] + dpArray[i - 2]
         }
-        
-        if bIndex >= 0 {
-            carry += arrayB[bIndex]
-        }
-        
-        result.append(carry % 2)
-        
-        carry = carry >= 2 ? 1 : 0
-        
-        aIndex -= 1
-        bIndex -= 1
+        return dpArray[n]
     }
-    if carry != 0 {result.append(1)}
-    var a = result.reversed().map(String.init)
-    return result.reversed().map(String.init).joined()
-   }
 
-addBinary("11", "1")
