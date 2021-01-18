8476184f86f9299014c36d5f2e4c79db71f9587c
sanghun0724 <sanghun0724@naver.com>
2021-01-18 20:44:33 +0900

    tuple

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -16,26 +16,21 @@
 //      }
 // }
 class Solution {
-    var count = 0
-    func isBalanced(_ root: TreeNode?) -> Bool {
-        if root?.val == nil {
-            return true
-        }
-        return checkDFS(root) != -1
+    func runningSum(_ nums: [Int]) -> [Int] {
+        var sum = 0
+        return nums.map{(sum+=$0,sum).1}
     }
-    func checkDFS(_ root:TreeNode?) -> Int {
-        guard  let root = root  else {
-            return 0
-        }
-        
-        var leftHeight = checkDFS(root.left)
-        var rightHeight = checkDFS(root.right)
-        
-        if leftHeight == -1 || rightHeight == -1 || abs(leftHeight - rightHeight) > 1 {
-            return -1
-        }
-        return max(leftHeight,rightHeight) + 1
-        
-    }
-    
 }
+
+let alpa:[String:String] = ["a":"A","b":"B"]
+
+var keys:[String] = alpa.map{(tuple:(String,String)) -> String in
+    return tuple.0
+}
+print(keys)
+
+keys = alpa.map{$0.0}
+print(keys)
+let values:[String] = alpa.map{$0.1}
+print(values)
+ 
