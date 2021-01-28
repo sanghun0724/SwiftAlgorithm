6150ca3f7f9fb2808621e9bd8e4837c7a2c33c36
sanghun0724 <sanghun0724@naver.com>
2021-01-28 15:29:27 +0900

    Same Tree

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -3,47 +3,42 @@
  
 
  
-//  public class TreeNode {
-//      public var val: Int
-//      public var left: TreeNode?
-//      public var right: TreeNode?
-//      public init() { self.val = 0; self.left = nil; self.right = nil; }
-//      public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
-//      public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
-//          self.val = val
-//         self.left = left
-//         self.right = right
-//      }
-// }
-
-  public class ListNode {
+  public class TreeNode {
       public var val: Int
-      public var next: ListNode?
-      public init(_ val: Int) {
+      public var left: TreeNode?
+      public var right: TreeNode?
+      public init() { self.val = 0; self.left = nil; self.right = nil; }
+      public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+      public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
           self.val = val
-          self.next = nil
+         self.left = left
+         self.right = right
       }
-  }
+ }
+//
+//  public class ListNode {
+//      public var val: Int
+//      public var next: ListNode?
+//      public init(_ val: Int) {
+//          self.val = val
+//          self.next = nil
+//      }
+//  }
 
 
 
-func makeGood(_ s: String) -> String {
-    var stack : [Character] = []
-    for c in s{
-        if stack.isEmpty{
-            stack.append(c)
+    func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
+        if p == nil || q == nil {
+            return p === q
         }
-        else{
-            if stack.last?.uppercased() == String(c) && stack.last!.isLowercase && c.isUppercase{
-                stack.popLast()
-            }
-            else if stack.last?.lowercased() == String(c) && c.isLowercase && stack.last!.isUppercase{
-                stack.popLast()
-            }
-            else{
-                stack.append(c)
-            }
+        
+        if  p?.val != q?.val {
+            return  false
         }
+        
+        return isSameTree(p?.right, q?.right) && isSameTree(p?.left, q?.left)
+    
+        
     }
-    return String(stack)
-}
+    
+    
