612a33d9f9e5555425975dde3f2dc04fb178ab5e
sanghun0724 <sanghun0724@naver.com>
2021-01-29 21:59:16 +0900

    fix

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -15,33 +15,26 @@
          self.right = right
       }
  }
-//
-//  public class ListNode {
-//      public var val: Int
-//      public var next: ListNode?
-//      public init(_ val: Int) {
-//          self.val = val
-//          self.next = nil
-//      }
-//  }
+
+  public class ListNode {
+      public var val: Int
+      public var next: ListNode?
+      public init(_ val: Int) {
+          self.val = val
+          self.next = nil
+      }
+  }
 
 
 
-func isSymmetric(_ root: TreeNode?) -> Bool {
-    if root?.val == nil {
-        return true
+class Solution {
+    func removeElements(_ head: ListNode?, _ val: Int) -> ListNode? {
+        
     }
-       
-    return isMirror(tree1: root?.left, tree2: root?.right)
-}
-func isMirror(tree1:TreeNode?,tree2:TreeNode?) -> Bool {
-    if tree1?.val != tree2?.val {
-        return false
+    
+    func checkNode(at index:Int,head:ListNode?) -> ListNode? {
+        var currentNode = head
+        var current
     }
-    guard let Tree1 = tree1 , let Tree2 = tree2 else {
-        return tree1 == nil && tree2 == nil
-    }
-    return isMirror(tree1: tree1?.left, tree2: tree2?.right) &&
-        isMirror(tree1: tree1?.right, tree2: tree2?.left)
 }
     
