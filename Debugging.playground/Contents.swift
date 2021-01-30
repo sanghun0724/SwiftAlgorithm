f771099e30131dda72be478e70ae20c6d54c0442
sanghun0724 <sanghun0724@naver.com>
2021-01-30 22:33:16 +0900

    fix

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -27,7 +27,31 @@
 
 
 
-    func countPrimes(_ n: Int) -> Int {
-    
+func isIsomorphic(_ s: String, _ t: String) -> Bool {
+        var charsS = Array(s)
+        var charsT = Array(t)
+        var map = [Character: Character]()
+        
+        if charsS.count != charsT.count {
+            return false
+        }
+        
+        for i in 0..<charsS.count {
+            let charS = charsS[i]
+            let charT = charsT[i]
+            if let value = map[charS] {
+                if value == charT {
+                    continue
+                } else {
+                    return false
+                }
+            } else {
+               
+                    map[charS] = charT
+                
+            }
+        }
+        
+        return true
     }
 
