7e9814b57b50378c90afb0d266c4c0242beb75cc
sanghun0724 <sanghun0724@naver.com>
2021-02-03 22:41:39 +0900

    BST

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -27,7 +27,13 @@ import UIKit
 
 class Solution {
     func lowestCommonAncestor(_ root: TreeNode?, _ p: TreeNode?, _ q: TreeNode?) -> TreeNode? {
-        
+        if p!.val < root!.val && q!.val < root!.val {
+            return lowestCommonAncestor(root?.left,p,q)
+        } else if p!.val > root!.val && q!.val > root!.val {
+            return lowestCommonAncestor(root?.right, p, q)
+        } else {
+            return root
+        }
     }
 }
 
