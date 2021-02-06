dcd8882d57a311a994ac805c330c19ca641a65df
sanghun0724 <sanghun0724@naver.com>
2021-02-06 14:28:18 +0900

    fix

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -25,24 +25,14 @@ import UIKit
       }
   }
 
-class Solution {
-    func binaryTreePaths(_ root: TreeNode?) -> [String] {
-        var result:[String] = []
-        var stack:[String] = []
-        description(root: root, result: &result, stack: &stack)
-        return result
-    }
-    func description(root:TreeNode?,result: inout [String],stack: inout [String]) {
-        guard root != nil else { return }
-        stack.append(String(root!.val))
-        if root?.right == nil && root?.left == nil {
-           return result.append(stack.joined(separator: "-"))
-        }
-        description(root: root?.right, result: &result, stack: &stack)
-        description(root: root?.left, result: &result, stack: &stack)
-    }
-}
 
+
+func isBadVersion(_ version: Int) -> Bool{}
+
+
+    func firstBadVersion(_ n: Int) -> Int {
+        
+    }
 
  
 
