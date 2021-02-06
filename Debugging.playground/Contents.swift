0fba0d4a134a8b5eef886014e4283eb7010a1a30
sanghun0724 <sanghun0724@naver.com>
2021-02-06 17:32:49 +0900

    fix

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -27,12 +27,26 @@ import UIKit
 
 
 
-func isBadVersion(_ version: Int) -> Bool{}
+func isBadVersion(_ version: Int) -> Bool{
+    return true
+}
 
 
     func firstBadVersion(_ n: Int) -> Int {
+        var left = 1
+        var right = n
         
+        while left < right {
+            let mid = left + (right - left) / 2
+            if isBadVersion(mid) {
+                right = mid
+            } else {
+                left = mid + 1
+            }
+        }
+        return left
     }
+
 
  
 
