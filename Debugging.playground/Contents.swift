ab66d391d47b4d92854284996de79183e34a6a92
sanghun0724 <sanghun0724@naver.com>
2021-02-12 22:02:13 +0900

    fix

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -26,12 +26,30 @@ import UIKit
   }
 
 
-
-    func repeatedSubstringPattern(_ s: String) -> Bool {
-        let r = s+s
-        return String(
-            r[r.index(after: r.startIndex)..<r.index(before:r.endIndex)]
-        ).contains(s)
+    func hammingDistance(_ x: Int, _ y: Int) -> Int {
+        
+       let first = pad(string: String(x,radix: 2), toSize: 31)
+       let second = pad(string: String(y,radix: 2), toSize: 31)
+        var count = 0
+        
+        for i in 0..<first.count {
+            print("g")
+            if first[first.index(first.startIndex, offsetBy: i)] != second[second.index(second.startIndex, offsetBy: i)] {
+                count += 1
+            }
+        }
+        
+        return count
     }
+    
+    func pad(string:String,toSize:Int) -> String {
+        var padded = string
+        for _ in 0..<(toSize - string.count) {
+            padded = "0" + padded
+        }
+        return padded
+    }
+    
+  hammingDistance(1, 4)
 
 
