568ce38360a9a7616761d2d6fefac59cfb9d799e
sanghun0724 <sanghun0724@naver.com>
2021-02-15 21:30:50 +0900

    good

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -25,9 +25,15 @@ import UIKit
       }
   }
 
-class Solution {
+
     func findComplement(_ num: Int) -> Int {
+       var i = 1
         
+        while i <= num {
+            i<<=1
+        }
+        return (i-1)^num
     }
-}
+
+findComplement(5)
  
