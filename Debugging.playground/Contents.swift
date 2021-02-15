f8196aad4cfb82b9058c1e8d339c7cc1efa6ab6e
sanghun0724 <sanghun0724@naver.com>
2021-02-16 01:19:37 +0900

    fix

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -26,8 +26,35 @@ import UIKit
   }
 
 
-class Solution {
+
     func findMaxConsecutiveOnes(_ nums: [Int]) -> Int {
+        if nums.count <= 1 {
+            return nums[0] == 1 ? 1 : 0
+        }
+        var resultCount = 0
+        var count = 0
+        var num = nums[0]
         
+        if nums[0] == 1 {
+            count+=1
+        }
+        
+        for i in 1..<nums.count {
+            
+            if nums[i] == 1 {
+                count+=1
+                num = nums[i]
+            }
+            
+            if nums[i] == 0 {
+                resultCount = resultCount > count ? resultCount : count
+                count = 0
+                num = 0
+            }
+            
+        }
+        resultCount = resultCount > count ? resultCount : count
+        return resultCount
     }
-}
+
+findMaxConsecutiveOnes([1,0,1,1,0,1])
