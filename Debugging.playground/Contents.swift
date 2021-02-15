ebc086dffb5c187073b49b46e11b5e0903a50647
sanghun0724 <sanghun0724@naver.com>
2021-02-15 14:43:19 +0900

    fix

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -24,23 +24,10 @@ import UIKit
           self.next = nil
       }
   }
-   
-  func islandPerimeter(_ grid: [[Int]]) -> Int {
-    var up = 0 ,down = 0, right = 0,left = 0
-    var result = 0
- 
-    for i in 0..<grid.count {
-        for j in 0..<grid[0].count {
-            if grid[i][j] == 1 {
-                result+=4
-                if i > 0 && grid[i-1][j] == 1 {
-                    result-=2
-                }
-                if j > 0 && grid[i][j-1] == 1 {
-                    result-=2
-                }
-            }
-        }
+
+class Solution {
+    func findComplement(_ num: Int) -> Int {
+        
     }
-    return result
 }
+ 
