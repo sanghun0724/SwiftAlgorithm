0f31551937c7d9803e5145e124b786b9c21b84a2
sanghun0724 <sanghun0724@naver.com>
2021-02-16 21:26:17 +0900

    fib

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -26,43 +26,16 @@ import UIKit
   }
 
 class Solution {
-    func travelMode(root:TreeNode?,arr:inout [Int]) {
-        guard let root = root else {
-            return
+    func fib(_ n: Int) -> Int {
+        if n == 0 {
+            return 0
         }
-        travelMode(root: root.left, arr: &arr)
-        arr.append(root.val)
-        travelMode(root: root.right, arr: &arr)
+        if n == 1 {
+            return 1
+        }
+        
+        return fib(n-1) + fib(n-2)
     }
-    
-    func findMode(_ root: TreeNode?) -> [Int] {
-     var arr  = [Int]()
-      
-        guard let root = root else {
-            return arr
-        }
-        
-        travelMode(root: root, arr: &arr)
-        
-        var count = [Int:Int]()
-        
-        arr.forEach{ count[$0] = (count[$0] ?? 0) + 1}
-        
-        arr.removeAll()
-        
-        let max = count.values.max()
-        
-        for (k,v) in count {
-            if v == max {
-                arr.append(k)
-            }
-        }
-        return arr
-    }
-    
- 
 }
-
-
    
 
