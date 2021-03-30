09c4652c69ef9e5d3c9b2bf713ae1d5cd11a947c
sanghun0724 <sanghun0724@naver.com>
2021-03-30 18:01:22 +0900

    fix

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,62 +1,65 @@
-
-
-
-import Foundation
-
-  public class TreeNode {
-      public var val: Int
-      public var left: TreeNode?
-      public var right: TreeNode?
-      public init() { self.val = 0; self.left = nil; self.right = nil; }
-      public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
-      public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
-          self.val = val
-         self.left = left
-         self.right = right
-      }
- }
-
-  public class ListNode {
-      public var val: Int
-      public var next: ListNode?
-      public init(_ val: Int) {
-          self.val = val
-          self.next = nil
-      }
-  }
-
-public class Node {
-     public var val: Int
-     public var children: [Node]
-     public init(_ val: Int) {
-        self.val = val
-         self.children  = []
-     }
- }
-public struct Queue<T> {
-    private var leftStack:[T] = [] //dequeue
-    private var rightStack:[T] = [] //enqueueu
-
-    public init() {}
-
-    public mutating func enqueue(_ element:T) -> Bool  {
-        rightStack.append(element)
-        return true
-    }
-
-    public mutating func dequeue() -> T? {
-        if leftStack.isEmpty {
-            leftStack = rightStack.reversed()
-            rightStack.removeAll()
-        }
-        return leftStack.popLast()
-    }
-    public mutating func Empty() -> Bool {
-        return leftStack.isEmpty && rightStack.isEmpty
-    }
-}
-
-func findRestaurant(_ list1: [String], _ list2: [String]) -> [String] {
-       
-   return []
-   }
+//
+//
+//
+//import Foundation
+//
+//  public class TreeNode {
+//      public var val: Int
+//      public var left: TreeNode?
+//      public var right: TreeNode?
+//      public init() { self.val = 0; self.left = nil; self.right = nil; }
+//      public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+//      public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+//          self.val = val
+//         self.left = left
+//         self.right = right
+//      }
+// }
+//
+//  public class ListNode {
+//      public var val: Int
+//      public var next: ListNode?
+//      public init(_ val: Int) {
+//          self.val = val
+//          self.next = nil
+//      }
+//  }
+//
+//public class Node {
+//     public var val: Int
+//     public var children: [Node]
+//     public init(_ val: Int) {
+//        self.val = val
+//         self.children  = []
+//     }
+// }
+//public struct Queue<T> {
+//    private var leftStack:[T] = [] //dequeue
+//    private var rightStack:[T] = [] //enqueueu
+//
+//    public init() {}
+//
+//    public mutating func enqueue(_ element:T) -> Bool  {
+//        rightStack.append(element)
+//        return true
+//    }
+//
+//    public mutating func dequeue() -> T? {
+//        if leftStack.isEmpty {
+//            leftStack = rightStack.reversed()
+//            rightStack.removeAll()
+//        }
+//        return leftStack.popLast()
+//    }
+//    public mutating func Empty() -> Bool {
+//        return leftStack.isEmpty && rightStack.isEmpty
+//    }
+//}
+//
+//func findRestaurant(_ list1: [String], _ list2: [String]) -> [String] {
+//       
+//   return []
+//   }
+var a:Int = 2
+var d:Double = Double(a)
+print(d)
