5caa162c0ec973c75d95be40174f74c882e50f14
sanghun0724 <sanghun0724@naver.com>
2021-04-08 19:32:55 +0900

    processor scheduling

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -59,7 +59,6 @@ public struct Queue<T> {
 
 func findRestaurant(_ list1: [String], _ list2: [String]) -> [String] {
        
+   print("thread having affinity for a processor")
    
-    return [] 
-   print("hepp")
    }
