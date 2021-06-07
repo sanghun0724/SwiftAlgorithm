1769b81e6fa001627d2dd3891f5bda5afebe8357
sanghun0724 <sanghun0724@naver.com>
2021-06-07 21:23:37 +0900

    new ps

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -56,26 +56,3 @@ public struct Queue<T> {
         return leftStack.isEmpty && rightStack.isEmpty
     }
 }
-func maximumProduct(_ nums: [Int]) -> Int {
-       var ma: [Int] = [Int.min, Int.min, Int.min]
-       var mi: [Int] = [Int.max, Int.max]
-       for num in nums {
-           if num > ma[2] {
-               ma[0] = ma[1]
-               ma[1] = ma[2]
-               ma[2] = num
-           } else if num > ma[1] {
-               ma[0] = ma[1]
-               ma[1] = num
-           } else if num > ma[0] {
-               ma[0] = num
-           }
-           if num < mi[0] {
-               mi[1] = mi[0]
-               mi[0] = num
-           } else if num < mi[1] {
-               mi[1] = num
-           }
-       }
-       return max(mi[0] * mi[1] * ma[2], ma[0] * ma[1] * ma[2])
-   }
