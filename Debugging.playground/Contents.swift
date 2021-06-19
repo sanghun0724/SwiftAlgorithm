03c328f91d6b7e8da95866a7484d975bf31b0029
sanghun0724 <sanghun0724@naver.com>
2021-06-19 16:07:26 +0900

    BFS

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -57,79 +57,34 @@ public struct Queue<T> {
     }
 }
 
+//bfs
 func averageOfLevels(_ root: TreeNode?) -> [Double] {
-    guard let root = root else {
-        return [Double]()  //nil 반환을 이렇게
-    }
+    var temp:[TreeNode?] = []
+    var result:[Double] = []
     
-    var res = [Double]()
-    var queue = [TreeNode]()
-    queue.append(root)
-    res.append(Double(root.val))
+    temp.append(root)
+    result.append(Double(root!.val))
     
-    while !queue.isEmpty {
-        var next = [TreeNode]()
+    while !temp.isEmpty {
+        var next:[TreeNode?] = []
         
-        for parent in queue {
-            if let right = parent.right {
+        for parent in temp {
+            if let right = parent?.right {
                 next.append(right)
             }
-            if let left = parent.left {
+            if let left = parent?.left {
                 next.append(left)
             }
         }
-    
-        queue = next // 트리의 끝에 도달하면 nil이니 큐가 비워지게 됨
-        if queue.count > 0 {
-            var sum = 0
-            for node in queue {
-                sum += node.val
+        temp = next
+        if temp.count > 0 {
+            var sum = 0.0
+            for  i in temp {
+                sum += Double(i!.val)
             }
-            res.append(Double(sum)/Double(queue.count))
+            result.append(sum/Double(temp.count))
         }
-    }
-    return res
-}
-
-
-
-func averageOfLevels2(_ root: TreeNode?) -> [Double] {
-    var nodes = [root].compactMap{ $0 } //컴팩트맵으로 nil제거나 옵셔널 바인딩 해줌
-    var result = [Double]()
-    
-    while nodes.count > 0 {
-        result.append(Double(nodes.map{ $0.val }.reduce(0,+)) / Double(nodes.count) )
-        nodes = nodes.flatMap{ [$0.left,$0.right].compactMap{$0}} //[A,B]형태의 배열 컴팩트맵 해주는거임}
     }
     return result
     
-    
-}
-
-//DFS
-class Solution {
-    func averageOfLevels12(_ root: TreeNode?) -> [Double] {
-        var total:[Int] = []
-        var count:[Int] = []
-        helper(root, &total, &count, 0)
-        var res:[Double] = []
-        for i in 0..<total.count{
-            res.append(Double(total[i])/Double(count[i]))
-        }
-        return res
     }
-    
-    func helper(_ root: TreeNode?, _ total: inout [Int], _ count: inout [Int], _ depth:Int){
-        guard let node = root else { return }
-        if total.count > depth{
-            total[depth] += node.val
-            count[depth] += 1
-        }else{
-            total.append(node.val)
-            count.append(1)
-        }
-        helper(node.left, &total, &count, depth+1)
-        helper(node.right, &total, &count, depth+1)
-    }
-}
-
