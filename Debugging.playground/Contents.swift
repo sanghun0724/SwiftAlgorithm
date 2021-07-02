af275d7e38ffc182c64c8ddf92b990c7d453d50e
sanghun0724 <sanghun0724@naver.com>
2021-07-02 23:34:02 +0900

    CodingTest Day

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -69,11 +69,4 @@ func decode(_ encoded: [Int], _ first: Int) -> [Int] {
     return resultArr
     
    }
-// Create Target Array in the Given order
-func createTargetArray(_ nums: [Int], _ index: [Int]) -> [Int] {
-    var res = [Int]()
-    for i in 0..<nums.count {
-        res.insert(nums[i], at: index[i])
-    }
-    return res
-}
+//CodingTest Day
