51777879bdc08b7278a82ddd8d717c65dfff8ae2
sanghun0724 <sanghun0724@naver.com>
2021-07-14 20:47:41 +0900

    Cells with Odd Values in a Matrix

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -56,94 +56,14 @@ import Foundation
 //        return leftStack.isEmpty && rightStack.isEmpty
 //    }
 //}
+// Cells with Odd Values in a Matrix
 
-//Filipping an image
-func flipAndInvertImage(_ image: [[Int]]) -> [[Int]] {
-    var flipedArr:[[Int]] = []
-    //1.filp
-    for i in image {
-        var flip = i
-        for j in 0..<i.count/2 {
-           flip.swapAt(j, i.count - 1 - j)
-    
+    func oddCells(_ n: Int, _ m: Int, _ indices: [[Int]]) -> Int {
+        var result: [[Int]] = Array(repeating: Array(repeating: 0, count: m), count: n)
+        for index in indices {
+            for i in 0..<m { result[index[0]][i] += 1 }
+            for i in 0..<n { result[i][index[1]] += 1 }
         }
-        flipedArr.append(flip)
+        return result.reduce(into: Int(0), { $0 += $1.reduce(into: Int(0), { $0 += $1 % 2 == 1 ? 1 : 0 }) })
     }
-  
-    var reversedArr:[[Int]] = []
-    //reversed
-    for i in flipedArr {
-        var reversed = i
-        for j in 0..<i.count {
-            if reversed[j] == 0 {
-                reversed[j] = 1
-            } else {
-                reversed[j] = 0
-            }
-        }
-        reversedArr.append(reversed)
-    }
-    
-    
-    
-    return reversedArr
-    }
-flipAndInvertImage([[1,1,0],[1,0,1],[0,0,0]])
-// 나는 바보였던건가..!!
 
-func flipAndInvertImage1(_ image: [[Int]]) -> [[Int]] {
-    return image.map({$0.map({1-$0}).reversed()})
-}
-
-
-func flipAndInvertImage2(_ image: [[Int]]) -> [[Int]] {
-    return image.map{$0.map{1-$0}.reversed()}   //느림
-            //() 추가로 빠름
-}
-
-func processTime(closure: () -> ()){
-    let start = CFAbsoluteTimeGetCurrent()
-    closure()
-    let processTime = CFAbsoluteTimeGetCurrent() - start
-    print("경과 시간: \(processTime)")
-
-}
-var test:[[Int]] = []
-for i in 0...100 {
-    test.append([1,1,0,1,1])
-}
-
-processTime {
-    test.map{$0.map{1-$0}.reversed()}
-    
-}
-processTime {
-    test.map({$0.map({1-$0}).reversed()})
-}
-
-
-
-
-@inlinable
-  public func map<T>(
-    _ transform: (Element) throws -> T
-  ) rethrows -> [T] {
-    // TODO: swift-3-indexing-model - review the following
-    let n = self.count
-    if n == 0 {
-      return []
-    }
-​
-    var result = ContiguousArray<T>()
-    result.reserveCapacity(n)
-​
-    var i = self.startIndex
-​
-    for _ in 0..<n {
-      result.append(try transform(self[i]))
-      formIndex(after: &i)
-    }
-​
-    _expectEnd(of: self, is: i)
-    return Array(result)
-  }
