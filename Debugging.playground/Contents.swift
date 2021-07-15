a5b7b3bf39d64f53d4caa8fda05da9519e6ca419
sanghun0724 <sanghun0724@naver.com>
2021-07-16 00:23:44 +0900

    much faster!!!

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -58,7 +58,15 @@ import Foundation
 //}
 // Maximum Product of Two Elements in an Array
 func maxProduct(_ nums: [Int]) -> Int {
-       var nums = nums
-       nums.sort()
-    return (nums.last! - 1) * (nums[nums.count-2] - 1)
+    var max1 = nums[0]
+    var max2 = Int.min
+    for i in 1..<nums.count {
+        if max1 < nums[i] {
+            max2 = max1
+            max1 = nums[i]
+        } else if max2 < nums[i] {
+            max2 = nums[i]
+        }
+    }
+    return (max1-1) * (max2-1)
 }
