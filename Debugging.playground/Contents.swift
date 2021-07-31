7e9ff5e3cf1fbd66407d661ce0afc48418334dd5
sanghun0724 <sanghun0724@naver.com>
2021-07-31 18:15:38 +0900

    Linked List Cycle with two Pointer slow  1 fast 2

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -100,128 +100,31 @@ import UIKit
 //        }
 //    }
 //}
+public class ListNode {
+      public var val: Int
+     public var next: ListNode?
+      public init(_ val: Int) {
+         self.val = val
+         self.next = nil
+      }
+  }
 
 
-
-class Node {
-    var val:Int
-    var next:Node?
-    
-    init(val:Int) {
-        self.val = val
+func hasCycle(_ head: ListNode?) -> Bool {
+    if head == nil {
+        return false
     }
+
+    var slow = head
+    var fast = head
+    
+    while fast?.next != nil && fast?.next?.next != nil {
+        slow = slow?.next
+        fast = fast?.next?.next
+        if slow === fast {
+            return true
+        }
+    }
+    
+    return false
 }
-
-class MyLinkedList {
-    var head:Node?
-    var tail:Node?
-    var count:Int
-    
-    /** Initialize your data structure here. */
-    init() {
-        count = 0
-    }
-    
-    /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
-    func get(_ index: Int) -> Int {
-        if index > count - 1 { return -1}
-        
-        var i = 0
-        var node = head
-        
-        while node != nil {
-            if i == index {
-                return node!.val
-            }
-            i+=1
-            node = node!.next
-        }
-        return -1
-    }
-    
-    /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
-    func addAtHead(_ val: Int) {
-        if count == 0 {
-            let newHead = Node(val: val)
-            head = newHead
-            tail = head
-        } else {
-            let newHead = Node(val: val)
-            newHead.next = head
-            head = newHead
-        }
-        count+=1
-    }
-    
-    /** Append a node of value val to the last element of the linked list. */
-    func addAtTail(_ val: Int) {
-        if count == 0 {
-            let newTail = Node(val: val)
-            tail = newTail
-            head = tail
-        } else {
-            let newTail = Node(val: val)
-            tail?.next = newTail
-            tail = newTail
-        }
-        count+=1
-    }
-    
-    /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
-    func addAtIndex(_ index: Int, _ val: Int) {
-          if index == 0 {
-            addAtHead(val)
-        } else if index == count {
-            addAtTail(val )
-        } else {
-            var i = 0
-            var node = head
-            while node != nil {
-                if i == index-1 {
-                    let newNode = Node(val: val)
-                    newNode.next = node?.next
-                    node?.next = newNode
-                    count+=1
-                    break
-                }
-                i+=1
-                node = node?.next
-            }
-        }
-    }
-    
-    /** Delete the index-th node in the linked list, if the index is valid. */
-    func deleteAtIndex(_ index: Int) {
-        if index >= count { return }
-        if index == 0 {
-            head = head?.next
-            
-        } else {
-            
-            var i = 0
-            var node = head
-            while node != nil {
-                if index-1 == i {
-                    if index == count-1 {
-                        tail = node
-                    }
-                    node?.next = node?.next?.next
-                    break
-                }
-                i+=1
-                node = node?.next
-            }
-            count-=1
-        }
-    }
-}
-
-/**
- * Your MyLinkedList object will be instantiated and called as such:
- * let obj = MyLinkedList()
- * let ret_1: Int = obj.get(index)
- * obj.addAtHead(val)
- * obj.addAtTail(val)
- * obj.addAtIndex(index, val)
- * obj.deleteAtIndex(index)
- */
