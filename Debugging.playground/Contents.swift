723d2de2516d9fbd1e69beef6863373d971d161f
sanghun0724 <sanghun0724@naver.com>
2021-07-31 17:06:06 +0900

    Design Linked List 기본 Linked List

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,7 +1,4 @@
 
-
-
-import Foundation
 import UIKit
 
 //  public class TreeNode {
@@ -57,22 +54,174 @@ import UIKit
 //        return leftStack.isEmpty && rightStack.isEmpty
 //    }
 //}
-func plusOne(_ digits: [Int]) -> [Int] {
-    var result = digits
-    for i in (0..<digits.count).reversed() {
-        if result[i] != 9 {
-            result[i] = result[i] + 1
-            return result
-        } else {
-            result[i] = 0
-        }
+
+
+//protocol ContentPresentable {
+//    var frame:CGRect { get set }
+//    var canPresentContent:Bool { get }
+//}
+//
+//extension ContentPresentable {
+//    var canPresentContent:Bool {
+//        return true
+//    }
+//}
+//
+//extension UIImageView:ContentPresentable {}
+//extension AVPlayerLayer:ContentPresentable {}
+//
+//
+//protocol MediaContainer {
+//    var media:ContentPresentable { get }
+//    var content:Content? { get  set }
+//    var videoLayer:AVPlayerLayer { get }
+//    var mediaImageView:UIImageView { get }
+//
+//    func contentChanged()
+//}
+//
+//struct Content {
+//    enum MediaType {
+//        case image,video
+//    }
+//    var type:Content.MediaType
+//}
+//
+//extension MediaContainer {
+//    func contentChanged() {
+//        //Update View
+//    }
+//    var media:ContentPresentable {
+//        switch content!.type {
+//        case .image:
+//            return mediaImageView
+//        case .video:
+//            return videoLayer
+//        }
+//    }
+//}
+
+
+
+class Node {
+    var val:Int
+    var next:Node?
+    
+    init(val:Int) {
+        self.val = val
     }
-    if result.first == 0 {
-        result.insert(1, at: 0)
+}
+
+class MyLinkedList {
+    var head:Node?
+    var tail:Node?
+    var count:Int
+    
+    /** Initialize your data structure here. */
+    init() {
+        count = 0
     }
     
-    return result
+    /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
+    func get(_ index: Int) -> Int {
+        if index > count - 1 { return -1}
+        
+        var i = 0
+        var node = head
+        
+        while node != nil {
+            if i == index {
+                return node!.val
+            }
+            i+=1
+            node = node!.next
+        }
+        return -1
+    }
+    
+    /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
+    func addAtHead(_ val: Int) {
+        if count == 0 {
+            let newHead = Node(val: val)
+            head = newHead
+            tail = head
+        } else {
+            let newHead = Node(val: val)
+            newHead.next = head
+            head = newHead
+        }
+        count+=1
+    }
+    
+    /** Append a node of value val to the last element of the linked list. */
+    func addAtTail(_ val: Int) {
+        if count == 0 {
+            let newTail = Node(val: val)
+            tail = newTail
+            head = tail
+        } else {
+            let newTail = Node(val: val)
+            tail?.next = newTail
+            tail = newTail
+        }
+        count+=1
+    }
+    
+    /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
+    func addAtIndex(_ index: Int, _ val: Int) {
+          if index == 0 {
+            addAtHead(val)
+        } else if index == count {
+            addAtTail(val )
+        } else {
+            var i = 0
+            var node = head
+            while node != nil {
+                if i == index-1 {
+                    let newNode = Node(val: val)
+                    newNode.next = node?.next
+                    node?.next = newNode
+                    count+=1
+                    break
+                }
+                i+=1
+                node = node?.next
+            }
+        }
+    }
+    
+    /** Delete the index-th node in the linked list, if the index is valid. */
+    func deleteAtIndex(_ index: Int) {
+        if index >= count { return }
+        if index == 0 {
+            head = head?.next
+            
+        } else {
+            
+            var i = 0
+            var node = head
+            while node != nil {
+                if index-1 == i {
+                    if index == count-1 {
+                        tail = node
+                    }
+                    node?.next = node?.next?.next
+                    break
+                }
+                i+=1
+                node = node?.next
+            }
+            count-=1
+        }
+    }
 }
-plusOne([9,9,9,9])
 
-
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * let obj = MyLinkedList()
+ * let ret_1: Int = obj.get(index)
+ * obj.addAtHead(val)
+ * obj.addAtTail(val)
+ * obj.addAtIndex(index, val)
+ * obj.deleteAtIndex(index)
+ */
