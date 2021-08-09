72f9521c4b56ea05c6cbd9a367e823a1ba2767e5
sanghun0724 <sanghun0724@naver.com>
2021-08-09 17:48:32 +0900

    Binary Tree Inorder Traversal

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,18 +1,18 @@
 
 import UIKit
 
-//  public class TreeNode {
-//      public var val: Int
-//      public var left: TreeNode?
-//      public var right: TreeNode?
-//      public init() { self.val = 0; self.left = nil; self.right = nil; }
-//      public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
-//      public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
-//          self.val = val
-//         self.left = left
-//         self.right = right
-//      }
-// }
+  public class TreeNode {
+      public var val: Int
+      public var left: TreeNode?
+      public var right: TreeNode?
+      public init() { self.val = 0; self.left = nil; self.right = nil; }
+      public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+      public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+          self.val = val
+         self.left = left
+         self.right = right
+      }
+ }
 //
 //  public class ListNode {
 //      public var val: Int
@@ -100,87 +100,22 @@ import UIKit
 //        }
 //    }
 //}
-//class Solution2 {
-//var numbers = Int
-//var target: Int!
-//var memory: [[Int: Int]]! //For creating the memory, I have used an array of dictionaries. Each index denotes the index in original array and at each index for. a particular sum, the count of successful iteration is saved
-//
-//func findTargetSumWays2(_ nums: [Int], _ S: Int) -> Int {
-//    if nums.count == 0{
-//        return 0
-//    }
-//    numbers = nums
-//    memory = Array(repeating: [Int: Int](), count: nums.count)
-//    target = S
-//    return dfs(0, 0)
-//}
-//
-//func dfs(_ index: Int, _ sum: Int) -> Int{
-//    if index == numbers.count{
-//        if sum == target{
-//            return 1
-//        }
-//        return 0
-//    }
-//    if memory[index][sum] != nil{
-//        return memory[index][sum]!
-//    }
-//    let sum1 = dfs(index+1, sum+numbers[index])
-//    let sum2 = dfs(index+1, sum-numbers[index])
-//    memory[index][sum] = sum1+sum2
-//    return sum1+sum2
-//}
-//}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 class Solution {
-    var targetNum:Int?
-    var map:[[Int:Int]]?
-    var numsArr:[Int]?
-    
-    func findTargetSumWays(_ nums: [Int], _ target: Int) -> Int {
-        guard nums.count != 0 else { return 0}
-        map = Array(repeating: [Int: Int](), count: nums.count)
-        numsArr = nums
-        targetNum = target
-        return DFS(0, 0)
+    func inorderTraversal(_ root: TreeNode?) -> [Int] {
+        if root == nil { return [] }
+        var arr = [Int]()
+        inorder(root, &arr)
+        return arr
+        
     }
-    func DFS(_ index:Int,_ sum:Int) -> Int {
-        if index == numsArr?.count {
-            if targetNum == sum {
-                return 1
-            }
-            return 0
-        }
-        if map![index][sum] != nil {
-            return map![index][sum]!
+    func inorder(_ root : TreeNode?,_ arr:inout [Int]) {
+        if root == nil {
+            return
         }
         
-        let sum1 = DFS(index+1, sum+numsArr![index])
-        let sum2 = DFS(index+1, sum-numsArr![index])
-        map![index][sum] = sum1+sum2
+        inorder(root?.left, &arr)
+        arr.append(root!.val)
+        inorder(root?.right, &arr)
         
-        return sum1+sum2
     }
 }
-
-
