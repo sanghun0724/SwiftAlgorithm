8cbe5853a777f2b34495c8042d1e0acd1fc7229b
sanghun0724 <sanghun0724@naver.com>
2021-08-13 14:48:09 +0900

    Design HashMap (simple)

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -100,100 +100,27 @@ import UIKit
 //        }
 //    }
 //}
-class MyHashSet {
-    var hashTable: [Node?]
-    var size: Int
-    var loadFactor: Double
+class MyHashMap {
+   var values = [Int?]()
     
-    init() {
-        hashTable = Array(repeating: nil, count: 16)
-        size = 0
-        loadFactor = 0.75
+    func put(_ key:Int,_ value: Int) {
+        if key >= values.count {
+            values += Array(repeating: nil, count: key + 1 - values.count)
+        }
+        values[key] = value
     }
     
-    func add(_ key:Int) {
-        var index = abs(key.hashValue) % hashTable.count
-        if hashTable[index] == nil {
-            hashTable[index] = Node(key)
-            size+=1
-        } else {
-            var cur = hashTable[index]
-            var pre = Node(-1)
-            while cur != nil {
-                if cur!.key == key {
-                    return
-                }
-                cur = cur!.next
-                pre = pre.next! //해당 인덱스  ---링크 끝까지로 이동
-            }
-            pre.next = Node(key) //connected
-            size += 1
-        }
-        if Double(size) / Double(hashTable.count) >= loadFactor {
-            reload()
-        }
+    func get(_ key:Int) -> Int {
+        if key >= values.count { return -1 }
+        guard let value = value[key] else { return }
+        return value
     }
     
     func remove(_ key:Int) {
-        var index = abs(key.hashValue) % hashTable.count
-        if hashTable[index] != nil {
-            var cur = hashTable[index]
-            var pre = Node(-1)
-            pre.next = cur
-            var dummy = pre
-            while cur != nil {
-                if cur!.key == key {
-                    pre.next = pre.next?.next
-                    size -= 1
-                    hashTable[index] = dummy.next
-                    return
-                }
-                cur = cur!.next
-                pre = pre.next!
-            }
-        }
+        if key >= values.count { return }
+        values[key] = nil
+        
     }
-   
-    func contains(_ key:Int) -> Bool {
-        var index = abs(key.hashValue) % hashTable.count
-        if hashTable[index] != nil {
-            var cur = hashTable[index]
-            
-            while cur != nil {
-                if cur!.key == key {
-                    return true
-                }
-                cur = cur!.next
-            }
-            return false
-        } else {
-            return false
-        }
-    }
-    
-    func reload() {
-        var temp = hashTable
-        hashTable = Array(repeating: nil , count: hashTable.count * 2)
-        size = 0
-        for node in temp {
-            var node = node
-            while node != nil {
-                add(node!.key)
-                node = node!.next
-            } //Refill
-        }
-    }
-    
-    
-    
-    class Node {
-        var key:Int
-        var next:Node?
-        init(_ key:Int) {
-            self.key = key
-        }
-    }
-
 }
 
 
