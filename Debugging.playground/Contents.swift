80e3f7e149fc669282324129a16adce881e662d9
sanghun0724 <sanghun0724@naver.com>
2021-08-15 23:04:05 +0900

    First Unique Character in a String using hashMap EASY!!

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,29 +1,29 @@
 
-import Foundation
+import UIKit
 
 
-func isHappy(_ n: Int) -> Bool {
-    var input = n
-    var seen = Set<Int>()
-    
-    while input != 1 {
-        if seen.contains(input) {
-            return false
+
+
+func firstUniqChar(_ s: String) -> Int {
+    var hashMap = [Character:Int]()
+    var s = Array(s)
+    let sCount = s.count
+    for i in 0..<sCount {
+        if let redundant = hashMap[s[i]] {
+            hashMap[s[i]] = redundant+1
+        } else {
+            hashMap[s[i]] = 1
         }
-        seen.insert(input)
-        input = getnext(input)
     }
-    return true 
-    }
-func getnext(_ n: Int) -> Int {
-    var sum = 0
-    var input = n
-    
-    while input > 0 {
-        let remainder = input % 10
-        sum += remainder * remainder
-        input /= 10
+    var first = Int.max
+    for (char,count) in hashMap {
+        if count == 1 {
+            if first > s.firstIndex(of: char)!  {
+                first = s.firstIndex(of: char )!
+            }
+        }
     }
     
-    return sum
-}
+    return  first != Int.max ? first : -1 
+   }
+firstUniqChar("12345")
