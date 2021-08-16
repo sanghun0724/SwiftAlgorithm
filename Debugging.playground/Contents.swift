2702b4b9c4f130742d1e24a9149e496d6a1de7ae
sanghun0724 <sanghun0724@naver.com>
2021-08-16 17:05:27 +0900

    Contain Duplicate ll if you think toward hash way, it's easy

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -2,21 +2,15 @@
 import Foundation
 
 
-func intersect(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
+func containsNearbyDuplicate(_ nums: [Int], _ k: Int) -> Bool {
     var dict = [Int:Int]()
     
-    for num in nums1 {
-        dict[num,default: 0] += 1
+    for i in 0..<nums.count {
+        if let found = dict[nums[i]],i - found <= k  {
+            return true
+        } else {
+            dict[nums[i]] = i
+        }
     }
-    var res = [Int]()
-    
-    for num in nums2 {
-        guard let count = dict[num],count > 0  else { continue }
-        res.append(num)
-        dict[num]!-=1
+    return false
     }
-    return res 
-    }
-
-
-
