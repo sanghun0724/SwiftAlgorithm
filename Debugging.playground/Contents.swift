5102ab950eee1fdbf262518751bc25d50a93df7a
sanghun0724 <sanghun0724@naver.com>
2021-08-19 23:31:03 +0900

    Insert Delete GetRandom O(1)

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,19 +1,41 @@
 import Foundation
 
-func topKFrequent(_ nums: [Int], _ k: Int) -> [Int] {
-    if k == 0 { return [] }
-    var dict = [Int:Int]()
-    
-    for i in 0..<nums.count {
-        dict[nums[i],default: 0] += 1
+class RandomizedSet {
+    var hashSet = [Int]()
+    /** Initialize your data structure here. */
+    init() {
+        hashSet = []
     }
-    var res = [Int]()
-    let sortedDict = dict.sorted{ $0.1 > $1.1 }
     
-    for i in 0..<k {
-        res.append(sortedDict[i].key)
-    }
-    return res
-   }
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    func insert(_ val: Int) -> Bool {
+        if hashSet.contains(val) {
+            return false
+        }
+            hashSet.append(val)
 
-topKFrequent([1,1,1,2,2,3], 2)
+        return true
+    }
+    
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    func remove(_ val: Int) -> Bool {
+        if hashSet.contains(val) {
+            for i in 0..<hashSet.count {
+                if hashSet[i] == val {
+                    hashSet.remove(at: i)
+                    return true 
+                }
+            }
+            
+        }
+        return false
+    }
+    
+    /** Get a random element from the set. */
+    func getRandom() -> Int {
+        if hashSet == nil {
+            return 0
+        }
+        return hashSet.randomElement()!
+    }
+}
