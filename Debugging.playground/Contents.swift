90a97df6ffd424066229b453a76a50290796b188
sanghun0724 <sanghun0724@naver.com>
2021-08-21 15:04:51 +0900

    Populating Next Right Pointers in Each Node

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,47 +1,41 @@
 import Foundation
 
-public class TreeNode {
-     public var val: Int
-     public var left: TreeNode?
-     public var right: TreeNode?
-     public init() { self.val = 0; self.left = nil; self.right = nil; }
-     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
-     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
-         self.val = val
-         self.left = left
-         self.right = right
-     }
- }
-var idx = 0
-func buildTree(_ preorder: [Int], _ inorder: [Int]) -> TreeNode? {
-    if preorder.count != inorder.count {
-        return nil
+
+public class Node {
+    public var val: Int
+    public var left: Node?
+    public var right: Node?
+      public var next: Node?
+    public init(_ val: Int) {
+        self.val = val
+        self.left = nil
+        self.right = nil
+        self.next = nil
     }
-    if preorder.count == 0 {
-        return nil
-    }
-    return getNode(preorder,inorder,idx,preorder.count-1)
-   }
-func getNode(_ preorder:[Int],_ inorder:[Int],_ start:Int,_ end:Int) -> TreeNode? {
-    if start>end { return nil }
-    let node = TreeNode(preorder[idx])
-    idx+=1
-    if start == end { return node }
-    let index = findIdx(inorder, node.val, end)
-    node.left = getNode(preorder, inorder, start, index-1)
-    node.right = getNode(preorder, inorder, index+1, end)
+}
+
+
+class Solution {
+   func connect(_ root: Node?) -> Node? {
+       var queue = [Node?]()
+    queue.append(root)
     
-    return node
-}
-func findIdx(_ inorder:[Int],_ value:Int,_ end:Int) -> Int {
-    for i in 0...end {
-        if inorder[i] == value {
-            return i
-            
+    while !queue.isEmpty {
+        let count = queue.count
+        
+        for i in 0..<count {
+            if i+1 < count , let next = queue[i+1] {
+                queue[i]?.next = next
+            } else {
+                queue[i]?.next = nil
+            }
+            if let left = queue[i]?.left { queue.append(left) }
+            if let right = queue[i]?.right { queue.append(right) }
         }
+        queue.removeFirst(count)
+        print(queue)
     }
-    return 0
+    
+    return root
+   }
 }
-buildTree([3,9,20,15,7]
-          ,
-          [9,3,15,20,7])
