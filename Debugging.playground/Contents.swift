1d450b23ef6dd042ed8c62567486f049f88b4f44
sanghun0724 <sanghun0724@naver.com>
2021-08-21 20:50:22 +0900

    Lowest Common Ancestor of a Binary Tree recursive 답지보면 쉽긴한데... 0에서 만드는게 너무 어렵다 !!!! 100+문제 더풀면 익숙해진다고하니.. 쭉 풀어보는 수 밖에

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,41 +1,29 @@
 import Foundation
 
 
-public class Node {
+public class TreeNode {
     public var val: Int
-    public var left: Node?
-    public var right: Node?
-      public var next: Node?
+    public var left: TreeNode?
+    public var right: TreeNode?
     public init(_ val: Int) {
         self.val = val
         self.left = nil
         self.right = nil
-        self.next = nil
     }
 }
 
 
-class Solution {
-   func connect(_ root: Node?) -> Node? {
-       var queue = [Node?]()
-    queue.append(root)
-    
-    while !queue.isEmpty {
-        let count = queue.count
-        
-        for i in 0..<count {
-            if i+1 < count , let next = queue[i+1] {
-                queue[i]?.next = next
-            } else {
-                queue[i]?.next = nil
-            }
-            if let left = queue[i]?.left { queue.append(left) }
-            if let right = queue[i]?.right { queue.append(right) }
-        }
-        queue.removeFirst(count)
-        print(queue)
+func lowestCommonAncestor(_ root: TreeNode?, _ p: TreeNode?, _ q: TreeNode?) -> TreeNode? {
+    if root == nil { return nil }
+    if root?.val == p?.val || root?.val == q?.val {
+        return root
     }
+    
+    let left = lowestCommonAncestor(root?.left, p, q)
+    let right = lowestCommonAncestor(root?.right, p, q)
+    
+    if left == nil { return right }
+    if right == nil { return left }
     
     return root
    }
-}
