832f5829ed0b63127057be98c821b9e822a9c1db
sanghun0724 <sanghun0724@naver.com>
2021-08-21 13:31:53 +0900

    Construct Binary Tree from Preorder and Inorder Traversal

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -12,14 +12,36 @@ public class TreeNode {
          self.right = right
      }
  }
-     
-func hasPathSum(_ root: TreeNode?, _ targetSum: Int) -> Bool {
-    guard let root = root else {
-        return false
+var idx = 0
+func buildTree(_ preorder: [Int], _ inorder: [Int]) -> TreeNode? {
+    if preorder.count != inorder.count {
+        return nil
     }
+    if preorder.count == 0 {
+        return nil
+    }
+    return getNode(preorder,inorder,idx,preorder.count-1)
+   }
+func getNode(_ preorder:[Int],_ inorder:[Int],_ start:Int,_ end:Int) -> TreeNode? {
+    if start>end { return nil }
+    let node = TreeNode(preorder[idx])
+    idx+=1
+    if start == end { return node }
+    let index = findIdx(inorder, node.val, end)
+    node.left = getNode(preorder, inorder, start, index-1)
+    node.right = getNode(preorder, inorder, index+1, end)
     
-    if root.left == nil && root.right == nil {
-        return root.val == targetSum
-    }
-    return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val)
+    return node
 }
+func findIdx(_ inorder:[Int],_ value:Int,_ end:Int) -> Int {
+    for i in 0...end {
+        if inorder[i] == value {
+            return i
+            
+        }
+    }
+    return 0
+}
+buildTree([3,9,20,15,7]
+          ,
+          [9,3,15,20,7])
