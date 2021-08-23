795be85d01f8dfe21a1bb60d8924fe294e04d118
sanghun0724 <sanghun0724@naver.com>
2021-08-23 13:46:55 +0900

    Search in a Binary Search Tree

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -8,17 +8,26 @@ public class ListNode {
     public init(_ val: Int) { self.val = val; self.next = nil; }
     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
 }
-
-
-func swapPairs(_ head: ListNode?) -> ListNode? {
-    guard let head = head else { return nil }
-    if let next = head.next {
-        head.next = swapPairs(next.next)
-        next.next = head
-        return next
-    } else {
-        return head
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
     }
-    
 }
+
+func searchBST(_ root: TreeNode?, _ val: Int) -> TreeNode? {
+    if root == nil { return nil }
+    if root?.val == val { return root }
+    
+    let left = searchBST(root?.left,val)
+    let right = searchBST(root?.right, val)
+    
+    return left != nil ? left : right
+    }
 
