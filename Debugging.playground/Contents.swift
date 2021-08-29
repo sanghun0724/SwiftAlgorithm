a60653401fdf25581774bd8871a9a78cda1c7abc
sanghun0724 <sanghun0724@naver.com>
2021-08-29 17:51:31 +0900

    Coin Change  (DP) Bottom UP

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,16 +1,19 @@
 import Foundation
 
 
-func minCostClimbingStairs(_ cost: [Int]) -> Int {
-        var size = cost.count
-        var dp = Array(repeating: 0, count: size)
-        dp[0] = cost[0]
-        dp[1] = cost[1]
-        
-        for i in 2..<size
-        {
-            dp[i] += min(cost[i] + dp[i-1],  cost[i] + dp[i-2])
+func coinChange(_ coins: [Int], _ amount: Int) -> Int {
+    guard amount > 0 else { return -1 }
+    
+    var dp = Array(repeating: amount + 1, count: amount+1)
+    dp[0] = 0
+    
+    for amountNum in 1...amount {
+        for coin in coins {
+            if coin <= amountNum {
+                dp[amountNum] = min(dp[amountNum],dp[amountNum - coin]+1) //to protect index bounds
+            }
         }
-        
-        return min(dp[size-1],dp[size-2])
     }
+    return dp[amount] != amount + 1 ? dp[amount] : -1
+   }
+coinChange([2], 3)
