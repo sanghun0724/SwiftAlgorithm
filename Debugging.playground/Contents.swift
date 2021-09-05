c24beb8c7c648f89547d21d6e21a674bc59f70dd
sanghun0724 <sanghun0724@naver.com>
2021-09-05 18:32:16 +0900

    Triangle DP 삼각형 문제들이랑 비슷 time complexity : o(n^2) space complexity: o(n)

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,7 +1,25 @@
 import Foundation
 
-func getConcatenation(_ nums: [Int]) -> [Int] {
-     return nums + nums
+func minimumTotal(_ triangle: [[Int]]) -> Int {
+    if triangle.count == 0 {
+        return 0;
     }
 
+    var dp = [Int](repeating: 10*10*10*10, count: triangle.count)
     
+    dp[0] = triangle[0][0]
+    
+    for i in 1..<triangle.count {
+        var temp = dp
+        for j in 0..<triangle[i].count {
+            if j == 0 {
+                dp[j] += triangle[i][0]
+            } else {
+                dp[j] = min(temp[j], temp[j-1]) + triangle[i][j]
+            }
+        }
+    }
+    return dp.min()!
+   }
+
+minimumTotal([[2],[3,4],[6,5,7],[4,1,8,3]])
