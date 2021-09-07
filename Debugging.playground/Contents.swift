29db2389e99e230e6e4fe1933043cbdd012f579b
sanghun0724 <sanghun0724@naver.com>
2021-09-07 20:50:02 +0900

    Insert Interval 어레이 구현문제 처음에 잘못접근해서 낭패봄 while여러번 을 분할시켜 돌려서 해결 O(n)

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,26 +1,94 @@
 import Foundation
 
 
-    // O(m*n*w) time | O(m*n) space
-    func findMaxForm(_ strs: [String], _ m: Int, _ n: Int) -> Int {
-        var dp = Array(
-            repeating: Array(repeating: 0, count: n+1),
-            count: m+1
-        )
-        
-        for word in strs {
-            let ones = word.filter { $0 == "1" }.count
-            let zeroes = word.filter { $0 == "0" }.count
-            
-            for i in stride(from: m, through: zeroes, by: -1) {
-                for j in stride(from: n, through: ones, by: -1){
-                    dp[i][j] = max(dp[i][j], dp[i-zeroes][j-ones]+1)
-                   print(dp)
-                }
-            }
-        }
-        
-        return dp[m][n]
-    }
+//func insert(_ intervals: [[Int]], _ newInterval: [Int]) -> [[Int]] {
+//    var intervals = intervals
+//
+//    if intervals.count == 0 {
+//        intervals.append(newInterval)
+//        return intervals
+//    }
+//    if newInterval.count == 0 {
+//        return intervals
+//    }
+//
+//    var checkRange:(Int?,Int?) = (nil,nil)
+//
+//
+//    for index in 0..<intervals.count {
+//        if intervals[index][0] <= newInterval[0] && newInterval[0] <= intervals[index][1] {
+//            checkRange.0 = index
+//        }
+//        if intervals[index][0] <= newInterval[1] && newInterval[1] <= intervals[index][1] {
+//            checkRange.1 = index
+//        }
+//    }
+//    if checkRange == (nil,nil) {
+//
+//        if (intervals.first?.first)! >= newInterval.first! && (intervals.last?.last)! <= newInterval.last! {
+//            return [newInterval]
+//        }
+//        for index in 0..<intervals.count {
+//            if intervals[index][0] > newInterval[0] && newInterval[1] > intervals[index][1]
+//            {
+//                intervals[index] = newInterval
+//                return intervals
+//            }
+//        }
+//
+//        if let index = intervals.firstIndex(where: { $0[0] > newInterval[0] }) {
+//            intervals.insert(newInterval, at: index)
+//        } else {
+//            intervals.append(newInterval)
+//        }
+//        return intervals
+//    }
+//
+//    print(checkRange)
+//    switch checkRange {
+//    case (let x,nil):
+//        intervals[x!][1] = newInterval[1]
+//    case (nil,let y):
+//        intervals[y!][0] = newInterval[0]
+//    case let (x,y):
+//        let newValue = [intervals[x!][0],intervals[y!][1]]
+//        intervals.removeSubrange(x!...y!)
+//        intervals.insert(newValue, at: x!)
+//    }
+//    return intervals
+//    }
+//insert([[0,5],[9,12]], [7,16])
+//insert([[0,7],[8,8],[9,11]]
+//       ,[4,13])
 
-findMaxForm(["10","0","1"], 1, 1)
+
+
+func insert(_ intervals: [[Int]], _ newInterval: [Int]) -> [[Int]] {
+    var newInterval = newInterval
+    var res = [[Int]]()
+    
+    
+    var i = 0 , n = intervals.count
+    
+    //안겹치는경우 왼쪽(작을떄)
+    while i < n && intervals[i][1] < newInterval[0] {
+        res.append(intervals[i])
+        i+=1
+    }
+    
+    //merge
+    while i < n && intervals[i][0] <= newInterval[1] {
+        newInterval[0] = min(intervals[i][0], newInterval[0])
+        newInterval[1] = max(intervals[i][1], newInterval[1])
+        i+=1
+    }
+    res.append(newInterval)
+    
+    //rest
+    while(i < n) {
+        res.append(intervals[i])
+        i+=1
+    }
+    
+    return res
+    }
