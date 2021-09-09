7b3bbf3e0e78ce1f59c0b976e2646f1769bfe321
sanghun0724 <sanghun0724@naver.com>
2021-09-09 22:04:03 +0900

    Maximal Square Time & Space :O(N*M) matrix가 주어졌을때는 그 주어진 거를 어떻게 활용할지 이번문제에서도 배열을 활용하면서도 + 정사각형안의 정사각형 이것도 활용  key Point 2가지

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,26 +1,20 @@
 import Foundation
 
-func insert2(_ intervals: [[Int]], _ newInterval: [Int]) -> [[Int]] {
-    var new = newInterval
-    var ans = [[Int]]()
-    var isInserted = false //new 가 들어갔는지 확인 
+func maximalSquare(_ matrix: [[Character]]) -> Int {
+    guard matrix.count > 0 && matrix[0].count > 0 else { return 0 }
+    var dp = [[Int]](repeating: [Int](repeating: 0, count: matrix[0].count), count: matrix.count)
+    var result = 0
     
-    for interval in intervals {
-        if new[0] > interval[1] {
-            ans.append(interval)
-        } else if new[1] < interval[0] {
-            if isInserted == false {
-                ans.append(new)
-                isInserted = true
+    for i in 0..<matrix.count {
+        for j in 0..<matrix[0].count {
+            if i == 0 || j == 0 {
+                dp[i][j] = Int(String(matrix[i][j]))!
+            } else if matrix[i][j] == "1" {
+                dp[i][j] = min(dp[i-1][j], dp[i-1][j-1],dp[i][j-1]) + 1
             }
-            ans.append(interval)
-        } else {
-            new[0] = min(interval[0],new[0])
-            new[1] = max(interval[1],new[1])
+            result = max(result,dp[i][j])
         }
     }
-    if isInserted { return ans }
-    ans.append(new)
-    return ans
-}
-}
+    return result * result
+   }
+
