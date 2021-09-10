df85c4d1fa9a5252852e9fe1927a3d2080377df4
sanghun0724 <sanghun0724@naver.com>
2021-09-10 18:07:32 +0900

    Unique Paths DP

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,16 +1,16 @@
 import Foundation
 
-func climbStairs(_ n: Int) -> Int {
-    if n < 3 {
-        return n
-    }
-       var dp = [Int](repeating: 0, count: n+1)
-    dp[1] = 1
-    dp[2] = 2
-    for i in 3..<n+1 {
-        for step in 1...2 {
-            dp[i] += dp[i-step]
+func uniquePaths(_ m: Int, _ n: Int) -> Int {
+    guard m > 0, n > 0 else {return 0}
+        var dp = [Int](repeating: 1, count: n)
+    
+    for i in 1..<m {
+        var temp = dp
+        for j in 1..<n {
+            temp[j] = temp[j-1] + dp[j]
         }
+        dp = temp
     }
-    return dp[n]
-   }
+    return dp.last!
+    }
+uniquePaths(3, 7)
