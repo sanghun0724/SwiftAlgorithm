d8563a92e6f0565c94efe08726a01d37b2ffd091
sanghun0724 <sanghun0724@naver.com>
2021-09-16 00:36:48 +0900

    * https://www.acmicpc.net/problem/8979 구현 굳

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -6,18 +6,42 @@ import Foundation
 
 
 
-func merge2(_ intervals: [[Int]]) -> [[Int]] {
-        return intervals
-            .sorted { $0[0] < $1[0] }
-            .reduce(into: [], { (merged, interval) in
-                if let last = merged.last, interval[0] <= last[1] {
-                    merged[merged.count - 1][1] = max(last[1], interval[1])
-                } else {
-                    merged.append(interval)
-                }
-            })
-    }
+var lines = [[Int]]()
+var firstLine = readLine()!.split(separator: " ").map{ Int(String($0))!}
+for _ in 1...firstLine.first! {
+    lines.append(Array(readLine()!.split(separator: " ").map{ Int(String($0))!}))
+}
+var target = firstLine.last!
 
+var dict = [Int:[Int]]()
+
+for i in 0..<lines.count {
+    let contry = lines[i].removeFirst()
+    dict[contry] = lines[i]
+}
+
+var dict2 = [[Int]:Int]()
+//var setArr = Set(lines)
+//var arr = Array(setArr)
+
+var sortedArr = lines.sorted {
+    if $0[0] == $1[0] {
+        if $0[1] == $1[1] {
+           return $0[2] > $1[2]
+        }
+        return $0[1] > $1[1]
+    }
+   return $0[0] > $1[0]
+}
+
+for i in 1...sortedArr.count {
+    if dict2[sortedArr[i-1]] != nil {
+        continue;
+    }
+    dict2[sortedArr[i-1]] = i
+}
+
+print(dict2[dict[target]!]!)
 
 
 
