b64b1ec8a4fcf9601ed37313758bd44e28496009
sanghun0724 <sanghun0724@naver.com>
2021-09-17 19:31:19 +0900

    https://www.acmicpc.net/problem/14500 골드 구현문제 시작

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -5,33 +5,103 @@ import Foundation
 
 
 
-
 var lines = [[Int]]()
 var firstLine = readLine()!.split(separator: " ").map{ Int(String($0))!}
 for _ in 1...firstLine.first! {
     lines.append(Array(readLine()!.split(separator: " ").map{ Int(String($0))!}))
 }
-var paper = [[Bool]](repeating: [Bool](repeating: false, count: 100), count: 100)
-var count = 0
+var values = [Int]()
+let row = lines.count
+let col = lines[0].count
 
-for line in lines{
-    for row in line.first!..<line.first!+10 {
-        for col in line.last!..<line.last!+10 {
-            if paper[row][col] == true {continue;}
-            paper[row][col] = true
+for i in 0..<row {
+    for j in 0..<col {
+        //길쭉이
+        if j+3 < col {
+            let value = lines[i][j] + lines[i][j+1] + lines[i][j+2] + lines[i][j+3]
+            values.append(value)
+        }
+        if i+3 < row {
+            let value = lines[i][j] + lines[i+1][j] + lines[i+2][j] + lines[i+3][j]
+            values.append(value)
+        }
+        //사각형
+        if i+1 < row && j+1 < col {
+            let value = lines[i][j] + lines[i][j+1] + lines[i+1][j] + lines[i+1][j+1]
+            values.append(value)
+        }
+        //fuck u
+        if i+1 < row && j+2 < col {
+            let value = lines[i][j] + lines[i][j+1] + lines[i+1][j+1] + lines[i][j+2]
+            values.append(value)
+        }
+        if i+2 < row && j+1 < col {
+            let value = lines[i+1][j] + lines[i][j+1] + lines[i+1][j+1] + lines[i+2][j+1]
+            values.append(value)
+        }
+        if i+1 < row && j+2 < col {
+            let value = lines[i][j+1] + lines[i+1][j] + lines[i+1][j+1] + lines[i+1][j+2]
+            values.append(value)
+        }
+        if i+2 < row && j+1 < col {
+            let value = lines[i][j] + lines[i+1][j] + lines[i+2][j] + lines[i+1][j+1]
+            values.append(value)
+        }
+        // ㄹ 모양
+        if i+2 < row && j+1 < col {
+            let value = lines[i][j] + lines[i+1][j] + lines[i+1][j+1] + lines[i+2][j+1]
+            values.append(value)
+        }
+        if i+1 < row && j+2 < col {
+            let value = lines[i][j+1] + lines[i][j+2] + lines[i+1][j] + lines[i+1][j+1]
+            values.append(value)
+        }
+        if i+2 < row && j+1 < col {
+            let value = lines[i][j+1] + lines[i+1][j] + lines[i+1][j+1] + lines[i+2][j]
+            values.append(value)
+        }
+        if i+1 < row && j+2 < col {
+            let value = lines[i][j] + lines[i][j+1] + lines[i+1][j+1] + lines[i+1][j+2]
+            values.append(value)
+        }
+        //ㄱ 모양
+        if i+2 < row && j+1 < col {
+            let value = lines[i][j] + lines[i+1][j] + lines[i+2][j] + lines[i+2][j+1]
+            values.append(value)
+        }
+        if i+1 < row && j+2 < col {
+            let value = lines[i][j] + lines[i+1][j] + lines[i][j+1] + lines[i][j+2]
+            values.append(value)
+        }
+        if i+2 < row && j+1 < col {
+            let value = lines[i][j] + lines[i][j+1] + lines[i+1][j+1] + lines[i+2][j+1]
+            values.append(value)
+        }
+        if i+1 < row && j+2 < col {
+            let value = lines[i+1][j] + lines[i+1][j+1] + lines[i+1][j+2] + lines[i][j+2]
+            values.append(value)
+        }
+        
+        if i+2 < row && j+1 < col {
+            let value = lines[i][j+1] + lines[i+1][j+1] + lines[i+2][j+1] + lines[i+2][j]
+            values.append(value)
+        }
+        
+        if i+1 < row && j+2 < col {
+            let value = lines[i][j] + lines[i+1][j] + lines[i+1][j+1] + lines[i+1][j+2]
+            values.append(value)
+        }
+        if i+2 < row && j+1 < col {
+            let value = lines[i][j] + lines[i][j+1] + lines[i+1][j] + lines[i+2][j]
+            values.append(value)
+        }
+        if i+1 < row && j+2 < col {
+            let value = lines[i][j] + lines[i][j+1] + lines[i][j+2] + lines[i+1][j+2]
+            values.append(value)
         }
     }
 }
-
-for i in 0..<100 {
-    for j in 0..<100 {
-        if paper[i][j] == true {
-            count+=1
-        }
-    }
-}
-
-print(count)
+print(values.max()!)
 
 
 
