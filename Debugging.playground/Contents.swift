bfd9cffaf808cd0ea5d01105b1dd809d64ae85e3
sanghun0724 <sanghun0724@naver.com>
2021-09-21 23:02:38 +0900

    https://www.acmicpc.net/problem/17837

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -4,99 +4,120 @@
 import Foundation
 
 
-var lines = [[Int]]()
-var firstLine = readLine()!.split(separator: " ").map{ Int(String($0))!}
-for _ in 1...firstLine.first! {
-    lines.append(Array(readLine()!.split(separator: " ").map{ Int(String($0))!}))
+
+let dx = [0, 0, 0, -1, 1]
+let dy = [0, 1, -1, 0, 0]
+struct horse{
+    var x: Int
+    var y: Int
+    var d: Int
 }
-let length = firstLine.last!
-let count = lines.count
-var res = count * 2
-var lines2 = lines
-var hash = [[Int]:Bool]()
-var hash2 = [[Int]:Bool]()
-for i in 0..<count {
-    outerLoop:for j in 1..<count {
-        //가로 3->2
-        if lines[i][j] == (lines[i][j-1] - 1) {
-            for k in 0..<length {
-                if let val = hash[[i,j+k]] {
-                    res-=1
-                    break outerLoop;
+func reverseDirection(_ dir: Int) -> Int{
+    if(dir == 1){
+        return 2
+    }else if(dir == 2){
+        return 1
+    }else if(dir == 3){
+        return 4
+    }else{
+        return 3
+    }
+}
+let nk = readLine()!.split(separator: " ").map{ Int(String($0))! }
+
+let n = nk[0], k = nk[1]
+var arr: [[Int]] = []
+for _ in 0..<n{
+    let tmp = readLine()!.split(separator: " ").map{ Int(String($0))! }
+    arr.append(tmp)
+}
+var horses: [horse] = []
+for _ in 0..<k{
+    let tmp = readLine()!.split(separator: " ").map{ Int(String($0))!}
+    let h = horse(x: tmp[0]-1, y: tmp[1]-1, d: tmp[2])
+    horses.append(h)
+}
+
+var horseMap: [[[Int]]] = Array(repeating: Array(repeating: [Int](), count: n), count: n)
+
+var num = 0
+for horse in horses{
+    horseMap[horse.x][horse.y].append(num)
+    num += 1
+}
+var t = 0
+while(t<=1000){
+    t += 1
+    for i in 0..<k{
+        let x = horses[i].x
+        let y = horses[i].y
+        let d = horses[i].d
+        var nx = x + dx[d]
+        var ny = y + dy[d]
+        // 파란색이거나 범위를 벗어나면
+        if(nx < 0 || nx >= n || ny < 0 || ny >= n || arr[nx][ny] == 2){
+            horses[i].d = reverseDirection(d)
+            nx = x + dx[horses[i].d]
+            ny = y + dy[horses[i].d]
+        }
+        if(nx < 0 || nx >= n || ny < 0 || ny >= n || arr[nx][ny] == 2){
+            continue
+        }
+        //흰색이면
+        if(arr[nx][ny] == 0){
+            var idx = -1
+            for j in 0..<horseMap[x][y].count{
+                let num = horseMap[x][y][j]
+                if i == num{
+                    idx = j
                 }
-                if (j+k) > count-1 {
-                    res-=1
-                    break outerLoop;
-                } else if lines[i][j+k] != (lines[i][j]) {
-                    res-=1
-                    break outerLoop;
+                if(idx == -1){
+                    continue
+                }
+                horses[num].x = nx
+                horses[num].y = ny
+                horseMap[nx][ny].append(num)
+                if(horseMap[nx][ny].count >= 4){
+                    print(t)
+                    exit(0)
                 }
             }
-            for k in 0..<length {hash[[i,j+k]] = true }
-            //2 -> 3
-        } else if lines[i][j] == (lines[i][j-1] + 1) {
-            for k in 1...length {
-                if let val = hash[[i,j-k]] {
-                    res-=1
-                    break outerLoop;
-                }
-                if (j-k) < 0 {
-                    res-=1
-                    break outerLoop;
-                } else if lines[i][j-k] != lines[i][j] - 1 {
-                    res-=1
-                    break outerLoop;
+            for _ in idx..<horseMap[x][y].count{
+                horseMap[x][y].removeLast()
+            }
+        }else if(arr[nx][ny] == 1){ // 빨간색이면
+            var idx = -1
+            var tmpArr:[Int] = []
+            for j in 0..<horseMap[x][y].count{
+                let num = horseMap[x][y][j]
+                if(i == num){
+                    idx = j
+                    break
                 }
             }
-        } else if abs(lines[i][j] - lines[i][j-1]) > 1 {
-            res-=1
-            break outerLoop;
+            if(idx == -1){
+                continue
+            }
+            for j in idx..<horseMap[x][y].count{
+                let num = horseMap[x][y][j]
+                tmpArr.append(num)
+                horses[num].x = nx
+                horses[num].y = ny
+            }
+            tmpArr.reverse()
+            horseMap[nx][ny] += tmpArr
+            if(horseMap[nx][ny].count >= 4){
+                print(t)
+                exit(0)
+            }
+
+            for _ in idx..<horseMap[x][y].count{
+                horseMap[x][y].removeLast()
+            }
         }
     }
 }
-
-for j in 0..<count {
-    outerLoop:for i in 1..<count {
-        //가로
-        if lines2[i][j] == (lines2[i-1][j] - 1) {
-            for k in 0..<length {
-                if let val = hash2[[i+k,j]] {
-                    res-=1
-                    break outerLoop;
-                }
-                if (i+k) > count-1 {
-                    res-=1
-                    break outerLoop;
-                } else if lines2[i+k][j] != (lines2[i][j]) {
-                    res-=1
-                    break outerLoop;
-                }
-            }
-            for k in 0..<length {hash2[[i+k,j]] = true }
-        } else if lines2[i][j] == (lines2[i-1][j] + 1) {
-            for k in 1...length {
-                if let val = hash2[[i-k,j]] {
-                    res-=1
-                    break outerLoop;
-                }
-                if (i-k) < 0 {
-                    res-=1
-                    break outerLoop;
-                } else if lines2[i-k][j] != lines2[i][j] - 1 {
-                    res-=1
-                    break outerLoop;
-                }
-            }
-        } else if abs(lines2[i][j] - lines2[i-1][j]) > 1 {
-            res-=1
-            break outerLoop;
-        }
-       
-    }
-}
-
-print(res)
-
+print(-1)
 processTime{
     
     
