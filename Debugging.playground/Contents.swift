38d98f5c59a0f8928ce27530124d41063dbd82eb
sanghun0724 <sanghun0724@naver.com>
2021-09-22 23:18:26 +0900

    https://www.acmicpc.net/problem/15683

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -3,152 +3,276 @@
 
 import Foundation
 
+var dirX = [0,0,0,-1,1]
+var dirY = [0,1,-1,0,0]
 
+struct CCTV {
+    var x:Int
+    var y:Int
+    var type:Int
+}
+var cctvs = [CCTV]()
 
-var moving = [[Int]]()
 var board = [[Int]]()
 var firstLine = Array(readLine()!.split(separator: " ").map{ Int(String($0))!})
-for _ in 1...firstLine.first! {
-    board.append(Array(readLine()!.split(separator: " ").map{ Int(String($0))!}))
-}
-for _ in 1...firstLine.last! {
-    moving.append(Array(readLine()!.split(separator: " ").map{ Int(String($0))!}))
-}
-
-var turnCount = 0
-
-func change(_ num:Int) -> Int {
-    switch num {
-    case 1:
-        return 2
-    case 2:
-        return 1
-    case 3:
-        return 4
-    case 4:
-        return 3
-    default:break
-    }
-    return -1
-}
-func helper(_ num: inout[Int]) -> Void {
-    let check = num[2]
-    switch check {
-    case 1:
-        num[1]+=1
-    case 2:
-        num[1]-=1
-    case 3:
-        num[0]-=1
-    case 4:
-        num[0]+=1
-    default: break
+for i in 0..<firstLine.first! {
+    let cluster = Array(readLine()!.split(separator: " ").map{ Int(String($0))!})
+    board.append(cluster)
+    for j in 0..<cluster.count {
+        if cluster[j] > 0 && cluster[j] < 6 {
+            cctvs.append(CCTV(x: j, y: i, type: cluster[j]))
+        }
     }
 }
-var dict = [[Int]:[Int]]()
 
-for i in 0..<moving.count {
-    dict[[moving[i][0]-1,moving[i][1]-1]] = [i]
-}
+var minCount = Int.max
 
-outerLoop:for count in 0...1001 {
-    for Index in 0..<moving.count {
-        let prev = [moving[Index][0]-1,moving[Index][1]-1]
-        helper(&moving[Index])
-        var row = moving[Index][0]-1
-        var col = moving[Index][1]-1
-       
-        
-        if row >= board.count || col >= board[0].count || row < 0 || col < 0 {
-            moving[Index][2] = change(moving[Index][2])
-            helper(&moving[Index])
-            var temp = moving[Index]
-            helper(&moving[Index])
-            row = moving[Index][0] - 1
-            col = moving[Index][1] - 1
-            if row >= board.count || col >= board[0].count || row < 0 || col < 0 || board[row][col] == 2 {
-                moving[Index] = temp
+
+func fill(_ board: inout [[Int]],_ cctv:CCTV,_ direction:Int) {
+    var x = cctv.x
+    var y = cctv.y
+    var nx = x
+    var ny = y
+    switch cctv.type {
+    case 1:
+        switch direction {
+        case 1:
+            while y < board.count - 1 && board[y][x] != 6 {
+                y += dirY[1]
+                if  0 < board[y][x] && board[y][x] < 6 || board[y][x] == -1 {
+                    continue;
+                }
+                board[y][x] = -1
+            }
+        case 2:
+            while y > 0 && board[y][x] != 6 {
+                y += dirY[2]
+                if 0 < board[y][x] && board[y][x] < 6 || board[y][x] == -1  {
+                    continue;
+                }
+                board[y][x] = -1
+            }
+        case 3:
+            while x > 0 && board[y][x] != 6 {
+                x += dirX[3]
+                if 0 < board[y][x] && board[x][y] < 6 || board[y][x] == -1  {
+                    continue;
+                }
+                board[y][x] = -1
+            }
+        case 4:
+            while x < board[0].count - 1 && board[y][x] != 6 {
+                x += dirX[4]
+                if 0 < board[y][x] && board[y][x] < 6 || board[y][x] == -1 {
+                    continue;
+                }
+                board[y][x] = -1
+            }
+        default: break;
+        }
+    case 2:
+        switch direction {
+        case 1,2:
+            while y < board.count - 1 && board[y][x] != 6 {
+                y += dirY[1]
+                if  0 < board[y][x] && board[y][x] < 6 || board[y][x] == -1 {
+                    continue;
+                }
+                board[y][x] = -1
+            }
+            while ny > 0 && board[ny][x] != 6 {
+                ny += dirY[2]
+                if  0 < board[ny][x] && board[ny][x] < 6 || board[ny][x] == -1 {
+                    continue;
+                }
+                board[ny][x] = -1
+            }
+        case 3,4:
+            while x > 0 && board[y][x] != 6 {
+                x += dirX[3]
+                if 0 < board[y][x] && board[y][x] < 6 || board[y][x] == -1  {
+                    continue;
+                }
+                board[y][x] = -1
+            }
+            while nx < board[0].count - 1 && board[y][nx] != 6 {
+                nx += dirX[4]
+                if 0 < board[y][nx] && board[y][nx] < 6 || board[y][nx] == -1  {
+                    continue;
+                }
+                board[y][nx] = -1
+            }
+        default: break;
+        }
+    case 3:
+        switch direction {
+        case 1,2:
+            while y < board.count - 1 && board[y][x] != 6 {
+                y += dirY[1]
+                if  0 < board[y][x] && board[y][x] < 6 || board[y][x] == -1 {
+                    continue;
+                }
+                board[y][x] = -1
+            }
+            
+            if direction == 1 {
+                while x > 0 && board[y][x] != 6 {
+                    x += dirX[3]
+                    if 0 < board[y][x] && board[y][x] < 6 || board[y][x] == -1  {
+                        continue;
+                    }
+                    board[y][x] = -1
+                }
+                
+            } else {
+                while nx < board[0].count - 1 && board[y][x] != 6 {
+                    nx += dirX[4]
+                    if 0 < board[y][nx] && board[y][nx] < 6 || board[y][nx] == -1  {
+                        continue;
+                    }
+                    board[y][nx] = -1
+                }
+            }
+            
+        case 3,4:
+            while y > 0 && board[y][x] != 6 {
+                y += dirY[2]
+                if 0 < board[y][x] && board[y][x] < 6 || board[y][x] == -1  {
+                    continue;
+                }
+                board[y][x] = -1
+            }
+            if direction == 3 {
+                while x > 0 && board[y][x] != 6 {
+                    x += dirX[3]
+                    if 0 < board[y][x] && board[y][x] < 6 || board[y][x] == -1  {
+                        continue;
+                    }
+                    board[y][x] = -1
+                }
+            } else {
+                while nx < board[0].count - 1 && board[y][x] != 6 {
+                    nx += dirX[4]
+                    if 0 < board[y][nx] && board[y][nx] < 6 || board[y][nx] == -1  {
+                        continue;
+                    }
+                    board[y][nx] = -1
+                }
+            }
+            
+        default: break;
+        }
+    case 4:
+        while x > 0 && board[y][x] != 6 {
+            x += dirX[3]
+            if 0 < board[y][x] && board[y][x] < 6 || board[y][x] == -1  {
                 continue;
             }
+            board[y][x] = -1
+        }
+        while nx < board[0].count - 1 && board[y][x] != 6 {
+            nx += dirX[4]
+            if 0 < board[y][nx] && board[y][nx] < 6 || board[y][nx] == -1  {
+                continue;
+            }
+            board[y][nx] = -1
+        }
+        switch direction {
+        case 1,2:
+            while y < board.count - 1 && board[y][x] != 6 {
+                y += dirY[1]
+                if  0 < board[y][x] && board[y][x] < 6 || board[y][x] == -1 {
+                    continue;
+                }
+                board[y][x] = -1
+            }
+        case 3,4:
+            while y > 0 && board[y][x] != 6 {
+                y += dirY[2]
+                if 0 < board[y][x] && board[y][x] < 6 || board[y][x] == -1  {
+                    continue;
+                }
+                board[y][x] = -1
+            }
+        default:break;
+        }
+    case 5:
+        while x > 0 && board[y][x] != 6 {
+            x += dirX[3]
+            if 0 < board[y][x] && board[y][x] < 6 || board[y][x] == -1  {
+                continue;
+            }
+            board[y][x] = -1
+        }
+        while nx < board[0].count - 1 && board[y][x] != 6 {
+            nx += dirX[4]
+            if 0 < board[y][nx] && board[y][nx] < 6 || board[y][nx] == -1  {
+                continue;
+            }
+            board[y][nx] = -1
+        }
+        while y < board.count - 1 && board[y][x] != 6 {
+            y += dirY[1]
+            if  0 < board[y][x] && board[y][x] < 6 || board[y][x] == -1 {
+                continue;
+            }
+            board[y][x] = -1
+        }
+        while y > 0 && board[y][x] != 6 {
+            y += dirY[2]
+            if 0 < board[y][x] && board[y][x] < 6 || board[y][x] == -1  {
+                continue;
+            }
+            board[y][x] = -1
+        }
+            
+    default:break;
         }
         
-        if board[row][col] == 2 {
-            moving[Index][2] = change(moving[Index][2])
-            helper(&moving[Index])
-            var temp = moving[Index]
-            helper(&moving[Index])
-            row = moving[Index][0] - 1
-            col = moving[Index][1] - 1
-            if row >= board.count || col >= board[0].count || row < 0 || col < 0 || board[row][col] == 2 {
-                moving[Index] = temp
-                continue
+    }
+
+func check(_ board:[[Int]]) -> Int {
+        var count = 0
+    for i in 0..<board.count {
+        for j in 0..<board[0].count {
+            if board[i][j] == 0 {
+                count+=1
             }
-        }
- 
-        if board[row][col] == 0 {
-            if let value = dict[[prev[0],prev[1]]] {
-                
-                var temp = [Int]()
-                for (i,v) in value.enumerated() {
-                    if Index == v {
-                        temp = Array(value[i...])
-                        dict[[prev[0],prev[1]]]!.removeLast(value.count - i)
-                        break;
-                    }
-                }
-                
-                //가려는곳에 말들 있을때
-                if let val = dict[[row,col]], val != [] {
-                    dict[[row,col]]! += temp
-                    for k in temp {
-                        moving[k][0] = row + 1
-                        moving[k][1] = col + 1
-                    }
-                } else {// 없을때
-                    dict[[row,col]] = temp
-                    for k in temp {
-                        moving[k][0] = row + 1
-                        moving[k][1] = col + 1
-                    }
-                }
-            }
-        } else if board[row][col] == 1 {
-            if let value = dict[[prev[0],prev[1]]] {
-                var temp = [Int]()
-                for (i,v) in value.enumerated() {
-                    if Index == v {
-                        temp = Array(value[i...]).reversed()
-                        dict[[prev[0],prev[1]]]!.removeLast(value.count - i)
-                        break;
-                    }
-                }
-                
-                //가려는곳에 말들 있을때
-                if let val = dict[[row,col]], val != [] {
-                    dict[[row,col]]! += temp
-                    for k in temp {
-                        moving[k][0] = row + 1
-                        moving[k][1] = col + 1
-                    }
-                } else {// 없을때
-                    dict[[row,col]] = temp
-                    for k in temp {
-                        moving[k][0] = row + 1
-                        moving[k][1] = col + 1
-                    }
-                }
-            }
-        }
-        if dict[[row,col]]!.count >= 4 {
-            print("\(turnCount+1)")
-            break outerLoop;
         }
     }
-    turnCount+=1
-    if turnCount == 1001 {
-        print(-1)
-    }
+    return count
 }
+var temp = board
+
+for Index in 0..<cctvs.count {
+  
+    fill(&temp, cctvs[Index], 1)
+}
+
+
+
+   
+    for cctv in cctvs {
+       for direction in 1...4 {
+            fill(&, cctv, direction)
+            minCount = min(check(),minCount)
+            
+        }
+    }
+    
+
+
+print(minCount)
+
+
+
+//var t = [1,2,3,4,5]
+//for i in 0..<t.count {
+//    var a = t.remove(at: i)
+//    print(t)
+//
+//}
+
 
 processTime{
     
