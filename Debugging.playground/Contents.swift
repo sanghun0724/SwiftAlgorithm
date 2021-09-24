3198a7f31b94d9f83698efa80873dab4bf5ff0aa
sanghun0724 <sanghun0724@naver.com>
2021-09-25 01:04:08 +0900

    https://www.acmicpc.net/problem/17144 More Clear

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -3,109 +3,125 @@
 
 import Foundation
 
-let dx = [0,0,1,-1]
-let dy = [-1,1,0,0]
-
-let ccw = [2,0,3,1]
-let cw = [2,1,3,0]
-
-var arr = [[Int]]()
+let first = readLine()!.split(separator: " ").map{Int(String($0))!}
+let dirR = [0,1,0,-1]
+let dirC = [1,0,-1,0]
+var spreadArr = [[Int]]()
 let (row,col) = (first[0],first[1])
 var timer = first[2]
 
 (0..<first[1]).forEach { (_) in
-    arr.append(readLine()!.split(separator: " ").map{Int(String($0))!})
+    spreadArr.append(readLine()!.split(separator: " ").map{Int(String($0))!})
 }
 
-var dict = [[Int]:Int]()
 var air = 0
 for r in 0..<row {
-    if arr[r][0] == -1 { air = r }
-    for c in 0..<col {
-        if arr[r][c] != -1 && arr[r][c] != 0 {
-            dict[[r,c]] = arr[r][c]
+    if spreadArr[r][0] == -1 { air = r }
+}
+
+for _ in 1...timer {
+    diffuse()
+    circulate()
+}
+print(getResult())
+
+
+
+func diffuse() {
+    var temp = [[Int]](repeating: [Int](repeating: 0, count: col), count: row)
+    for i in 0..<row {
+        for j in 0..<col {
+            if spreadArr[i][j] != 0 && spreadArr[i][j] != 1 {
+                var count = 0
+                let diffuse = spreadArr[i][j] / 5
+                for k in 0..<4 {
+                    let next = (i+dirR[k],j+dirC[k])
+                    if next.0 >= row || next.1 >= col || next.0 < 0 || next.1 < 0 { continue }
+                    if spreadArr[next.0][next.1] != -1 {
+                        temp[next.0][next.1] += diffuse
+                        count+=1
+                    }
+                }
+                spreadArr[i][j] -= (diffuse * count)
+            }
+        }
+    }
+    sum(temp)
+}
+
+func sum(_ temp:[[Int]]) {
+    for i in 0..<row {
+        for j in 0..<col {
+            if spreadArr[i][j] != -1 {
+                spreadArr[i][j] += temp[i][j]
+            }
         }
     }
 }
 
-var spreadArr = [[Int]](repeating: [Int](repeating: 0, count: col), count: row)
-spreadArr[air][0] = -1
-spreadArr[air-1][0] = -1
 
-func circulate(_ cleanerX:Int,cleanerY:Int,dir:[Int]) {
-    var y = cleanerY
-    var x = cleanerX + 1
-    spreadArr[y][x] = 0
-    for k in 0...3 {
-        while true {
-           let nx = x + dx[dir[k]]
-           let ny = y + dy[dir[y]]
-            
-            if (!(0 <= ny && ny < row && 0 <= nx && nx <= col)) {
-                break;
-            }
-            if cleanerY == ny && cleanerX == nx {
-                break;
-            }
-            
-            spreadArr[ny][nx] =
+func circulate() {
+    //high
+    for r in (0..<air-1).reversed() {
+        if r+1 == air-1 {
+            spreadArr[r][0] = 0
+        } else {
+            spreadArr[r+1][0] = spreadArr[r][0]
         }
     }
     
+    for c in 1..<col {
+        spreadArr[0][c-1] = spreadArr[0][c]
+    }
+    
+    for r in 1...air-1 {
+        spreadArr[r-1][col-1] = spreadArr[r][col-1]
+    }
+    for c in (1..<col-1).reversed() {
+        
+        spreadArr[air-1][c+1] = spreadArr[air-1][c]
+        if c == 1 {
+            spreadArr[air-1][c] = 0
+        }
+    }
+    
+    //down
+    for r in air+1..<row {
+        if r-1 == air {
+            spreadArr[r][0] = 0
+        } else {
+            spreadArr[r-1][0] = spreadArr[r][0]
+        }
+    }
+    
+    for c in (1..<col) {
+        spreadArr[row-1][c-1] = spreadArr[row-1][c]
+    }
+    
+    for r in (air..<row-1).reversed() {
+        spreadArr[r+1][col-1] = spreadArr[r][col-1]
+    }
+    
+    for c in (1..<col-1).reversed() {
+        spreadArr[air][c+1] = spreadArr[air][c]
+        if c == 1 {
+            spreadArr[air][c] = 0
+        }
+    }
 }
 
 
-
-for _ in 0..<timer {
-  for (k,v) in dict {
-  var r = k[0]
-  var c = k[1]
-  var count = 0
-    if r+1 < row && spreadArr[r+1][c] != -1 {
-        dict[[r+1,c],default: 0] += v / 5 //소수점check
-        spreadArr[r+1][c] = dict[[r+1,c]]!
-        count+=1
+func getResult() -> Int {
+    var res = 0
+    for r in 0..<row {
+        for c in 0..<col {
+            if spreadArr[r][c] != -1 {
+                res += spreadArr[r][c]
+            }
+        }
     }
-    if r-1 >= 0 && spreadArr[r-1][c] != -1  {
-        dict[[r-1,c],default: 0] += v / 5 //소수점check
-        spreadArr[r-1][c] = dict[[r-1,c]]!
-        count+=1
-    }
-    if c+1 < col {
-        dict[[r,c+1],default: 0] += v / 5 //소수점check
-        spreadArr[r][c+1] = dict[[r,c+1]]!
-        count+=1
-    }
-    if c-1 >= 0 && spreadArr[r][c-1] != -1{
-        dict[[r,c-1],default: 0] += v / 5 //소수점check
-        spreadArr[r][c-1] = dict[[r,c-1]]!
-        count+=1
-    }
-    dict[k,default: 0] -= ((v/5) * count)
-    spreadArr[r][c] = dict[k]!
+    return res
 }
-    
-    
-    
-
-    
-}
-print(spreadArr)
-var res = 0
-for r in 0..<row {
-    for c in 0..<col {
-        res += spreadArr[r][c]
-    }
-}
-print(res+2)
-
-
-//var t = [1,2,3,4,5]
-//for i in 0..<t.count {
-//    var a = t.remove(at: i)
-//    print(t)
-//
-//}
 
 
 processTime{
