f8ac47612bea8b556e0176b907bf851e931b76bd
sanghun0724 <sanghun0724@naver.com>
2021-09-24 23:10:09 +0900

    https://www.acmicpc.net/problem/17144

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -3,100 +3,101 @@
 
 import Foundation
 
-let ip: (() -> [Int]) = {
-    let S = readLine()!
-    var result:[Int] = []
-    S.forEach {
-        if $0 == " " { return }
-        result.append(Int($0.unicodeScalars.first!.value) -  48)
-    }
-    return result
+let dx = [0,0,1,-1]
+let dy = [-1,1,0,0]
+
+let ccw = [2,0,3,1]
+let cw = [2,1,3,0]
+
+var arr = [[Int]]()
+let (row,col) = (first[0],first[1])
+var timer = first[2]
+
+(0..<first[1]).forEach { (_) in
+    arr.append(readLine()!.split(separator: " ").map{Int(String($0))!})
 }
 
-typealias Tuple = (Int,Int,Int)
-let dx = [-1,0,1,0]
-let dy = [0,1,0,-1]
-let arr = ip()
-let (N,M) = (arr[0],arr[1]) //첫줄/
-var G:[[Int]] = [] // 사무실그래프 입력받을
-var cctvList:[Tuple] = []
-var zeroCnt:Int = 0
-
-(0..<N).forEach { _ in
-    G.append(ip())
-} // 걍 2차언배열 저장
-
-for i in G.indices {
-    for j in G[i].indices {
-        if G[i][j] >= 1 && G[i][j] <= 5 {
-            cctvList.append((G[i][j],i,j))
-        } else if G[i][j] == 0 {
-            zeroCnt+=1 //count
+var dict = [[Int]:Int]()
+var air = 0
+for r in 0..<row {
+    if arr[r][0] == -1 { air = r }
+    for c in 0..<col {
+        if arr[r][c] != -1 && arr[r][c] != 0 {
+            dict[[r,c]] = arr[r][c]
         }
     }
 }
 
-//한 방향에서 cctv작동했던범위의 카운트를 알려줌!
-func coverZeroOneline(_ tmpG: inout [[Int]], _ pos:(Int,Int), _ dir:Int) -> Int {
-    var posX = pos.0 + dx[dir]
-    var posY = pos.1 + dy[dir]
-    var coverCnt = 0
-    while posX >= 0 && posY >= 0 && posX < N && posY < M && tmpG[posX][posY] != 6 {
-        if tmpG[posX][posY] == 0 {
-            tmpG[posX][posY] = -1
-            coverCnt += 1
-        }
-        posX += dx[dir]
-        posY += dy[dir]
-    }
-    return coverCnt
-}
+var spreadArr = [[Int]](repeating: [Int](repeating: 0, count: col), count: row)
+spreadArr[air][0] = -1
+spreadArr[air-1][0] = -1
 
-func runcCCTV(_ cctvNum:Int, _ pos:(Int,Int), _ dir:Int, _ tmpG: inout [[Int]]) -> Int {
-    var coverToCnt = 0
-    if cctvNum == 1 {
-        coverToCnt += coverZeroOneline(&tmpG, pos, dir)
-    } else if cctvNum == 2 {
-        [0,2].forEach { (dirCnt) in
-            coverToCnt += coverZeroOneline(&tmpG, pos, (dir + dirCnt) % 4)
-        }
-    } else if cctvNum == 3 {
-            (0...1).forEach { (dirCnt) in
-                coverToCnt += coverZeroOneline(&tmpG, pos, (dir + dirCnt) % 4)
+func circulate(_ cleanerX:Int,cleanerY:Int,dir:[Int]) {
+    var y = cleanerY
+    var x = cleanerX + 1
+    spreadArr[y][x] = 0
+    for k in 0...3 {
+        while true {
+           let nx = x + dx[dir[k]]
+           let ny = y + dy[dir[y]]
+            
+            if (!(0 <= ny && ny < row && 0 <= nx && nx <= col)) {
+                break;
             }
-    } else if cctvNum == 4 {
-        (0...2).forEach { (dirCnt) in
-            coverToCnt += coverZeroOneline(&tmpG, pos, (dir + dirCnt) % 4)
+            if cleanerY == ny && cleanerX == nx {
+                break;
+            }
+            
+            spreadArr[ny][nx] =
         }
-    } else if cctvNum == 5 {
-        (0...3).forEach { (dirCnt) in
-            coverToCnt += coverZeroOneline(&tmpG, pos, (dir + dirCnt) % 4)
-        }
-    }
-    return coverToCnt
-    }
-
-// cctvList의 모든 cctv방향을 설정한 상태일때 사각지대 크기를 확인하고 메소드를 종료 재귀깊이 1줄임
-var Ans = Int.max
-func DFS(_ cctvIdx:Int,_ coverCnt:Int,_ tmpG:[[Int]]) {
-    if cctvIdx >= cctvList.count {
-        let blindSpotCnt = zeroCnt - coverCnt //기존 0개수에서 메소드돌린 -1커버 개수만큼 뺴기
-        Ans = Ans > blindSpotCnt ? blindSpotCnt : Ans
-        return
     }
     
-    var nowG = tmpG
-    let nowCCTV = cctvList[cctvIdx]
-    for dir in (0..<4) {
-        let cctvNum = nowCCTV.0
-        let cctvPos = (nowCCTV.1,nowCCTV.2)
-        let nowCoverCnt = runcCCTV(cctvNum, cctvPos, dir, &nowG)
-        DFS(cctvIdx+1, coverCnt+nowCoverCnt, nowG) //커버개수 체크
-        nowG = tmpG
+}
+
+
+
+for _ in 0..<timer {
+  for (k,v) in dict {
+  var r = k[0]
+  var c = k[1]
+  var count = 0
+    if r+1 < row && spreadArr[r+1][c] != -1 {
+        dict[[r+1,c],default: 0] += v / 5 //소수점check
+        spreadArr[r+1][c] = dict[[r+1,c]]!
+        count+=1
+    }
+    if r-1 >= 0 && spreadArr[r-1][c] != -1  {
+        dict[[r-1,c],default: 0] += v / 5 //소수점check
+        spreadArr[r-1][c] = dict[[r-1,c]]!
+        count+=1
+    }
+    if c+1 < col {
+        dict[[r,c+1],default: 0] += v / 5 //소수점check
+        spreadArr[r][c+1] = dict[[r,c+1]]!
+        count+=1
+    }
+    if c-1 >= 0 && spreadArr[r][c-1] != -1{
+        dict[[r,c-1],default: 0] += v / 5 //소수점check
+        spreadArr[r][c-1] = dict[[r,c-1]]!
+        count+=1
+    }
+    dict[k,default: 0] -= ((v/5) * count)
+    spreadArr[r][c] = dict[k]!
+}
+    
+    
+    
+
+    
+}
+print(spreadArr)
+var res = 0
+for r in 0..<row {
+    for c in 0..<col {
+        res += spreadArr[r][c]
     }
 }
-DFS(0, 0, G)
-print(Ans)
+print(res+2)
 
 
 //var t = [1,2,3,4,5]
