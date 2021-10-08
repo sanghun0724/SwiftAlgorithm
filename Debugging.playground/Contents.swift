bd7d0ed84c1b1b3ccb9008c10b20a33972013eeb
sanghun0724 <sanghun0724@naver.com>
2021-10-08 20:47:05 +0900

    https://www.acmicpc.net/problem/16235

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,48 +1,92 @@
 
-let dirR = [-1,0,1,0]
-let dirC = [0,1,0,-1]
+let dirR = [-1,0,1,0,-1,-1,1,1]
+let dirC = [0,1,0,-1,-1,1,-1,1]
 
-var firstLine = [Int]()
-firstLine = readLine()!.split(separator: " ").map{ Int(String($0))! }
+var Line = [Int]()
+Line = readLine()!.split(separator: " ").map{ Int(String($0))! }
 
-let N = firstLine[0]
-let robot = readLine()!.split(separator: " ").map{ Int(String($0))! }
+let N = Line[0]
+let M = Line[1]
+let K = Line[2]
 
-var graph = [[Int]]()
+var feed = [[Int]]()
 for _ in 1...N {
-     graph.append(Array(readLine()!.split(separator: " ").map{ Int(String($0))!}))
+     feed.append(Array(readLine()!.split(separator: " ").map{ Int(String($0))!}))
 }
-var r = robot[0]
-var c = robot[1]
-var dir = robot[2]
-var res = 0
 
-loop: while true {
-    if graph[r][c] == 0 {
-        graph[r][c] = 2
-        res+=1
-    }
-    
-    for i in 1...4 {
-        let tmpDir = (dir+4-i) % 4
-        let nr = r + dirR[tmpDir]
-        let nc = c + dirC[tmpDir]
-        if graph[nr][nc] == 0 {
-            r = nr
-            c = nc
-            dir = tmpDir
-            continue loop
+var graph = [[Int]](repeating: [Int](repeating: 5, count: N), count: N)
+
+
+var trees = [[Int]]()
+for _ in 1...M {
+    trees.append(Array(readLine()!.split(separator: " ").map{ Int(String($0))! }))
+}
+trees.sort{ $0[2] < $1[2] }
+var deadTrees = [[Int]]()
+var removeTree = [Int]()
+
+for _ in 1...K {
+    spring()
+    summer()
+    autumn()
+    winter()
+}
+
+
+
+func spring() {
+    for i in 0..<trees.count {
+        let r = trees[i][0]-1
+        let c = trees[i][1]-1
+        
+        if graph[r][c] >= trees[i][2] {
+            graph[r][c] -= trees[i][2]
+            trees[i][2] += 1
+        } else {
+            deadTrees.append(trees[i])
+            removeTree.append(i)
         }
     }
     
-    let back = (dir + 2) % 4
-    r = r + dirR[back]
-    c = c + dirC[back]
-    
-    if graph[r][c] == 1 {
-        break loop
+    for remove in removeTree.reversed() {
+        trees.remove(at: remove)
+    }
+    removeTree.removeAll()
+}
+
+func summer () {
+    for i in 0..<deadTrees.count {
+        let r = deadTrees[i][0]-1
+        let c = deadTrees[i][1]-1
+        graph[r][c] += deadTrees[i][2] / 2
     }
 }
 
-print(res)
- //다시해보기
+func autumn() {
+    for i in 0..<trees.count {
+        if trees[i][2] % 5 == 0 {
+        let r = trees[i][0]-1
+        let c = trees[i][1]-1
+            for d in 0...7 {
+                let nr = r + dirR[d]
+                let nc = c + dirC[d]
+                if nr >= 0 && nc >= 0 && nr < graph.count && nc < graph[0].count {
+                    trees.append([nr+1,nc+1,1])
+                }
+            }
+        }
+    }
+}
+
+func winter() {
+    for r in 0..<graph.count {
+        for c in 0..<graph[0].count {
+         graph[r][c] += feed[r][c]
+        }
+    }
+}
+
+print(trees.count)
+
+
+
