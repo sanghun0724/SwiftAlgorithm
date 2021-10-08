85f876c666a4a748118463b310dbc7f3a660df53
sanghun0724 <sanghun0724@naver.com>
2021-10-08 15:15:13 +0900

    * https://www.acmicpc.net/problem/14503

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,32 +1,48 @@
 
+let dirR = [-1,0,1,0]
+let dirC = [0,1,0,-1]
 
+var firstLine = [Int]()
+firstLine = readLine()!.split(separator: " ").map{ Int(String($0))! }
 
-import Foundation
+let N = firstLine[0]
+let robot = readLine()!.split(separator: " ").map{ Int(String($0))! }
 
+var graph = [[Int]]()
+for _ in 1...N {
+     graph.append(Array(readLine()!.split(separator: " ").map{ Int(String($0))!}))
+}
+var r = robot[0]
+var c = robot[1]
+var dir = robot[2]
+var res = 0
 
-
-func eraseOverlapIntervals(_ intervals: [[Int]]) -> Int {
-    if intervals.count == 0 { return 0 }
-    var sortedIntervals = intervals.sorted{
-        if $0[1] == $1[1] {
-            return $0[0] > $1[0]
-        }
-        return $0[1] < $1[1]
+loop: while true {
+    if graph[r][c] == 0 {
+        graph[r][c] = 2
+        res+=1
     }
-    var rightEdge = sortedIntervals[0][1]
-    var answer = 0
     
-    for i in 1..<sortedIntervals.count {
-        var temp = sortedIntervals[i]
-        if rightEdge > temp[0] {
-            answer+=1
-            rightEdge = min(rightEdge, temp[1]) //interval좁은애가 살아남고 큰애는 삭제  <<< KEY 
-        }  else {
-            rightEdge = temp[1]
+    for i in 1...4 {
+        let tmpDir = (dir+4-i) % 4
+        let nr = r + dirR[tmpDir]
+        let nc = c + dirC[tmpDir]
+        if graph[nr][nc] == 0 {
+            r = nr
+            c = nc
+            dir = tmpDir
+            continue loop
         }
     }
-     return answer
+    
+    let back = (dir + 2) % 4
+    r = r + dirR[back]
+    c = c + dirC[back]
+    
+    if graph[r][c] == 1 {
+        break loop
     }
+}
 
-eraseOverlapIntervals(
-    [[0,1],[3,4],[1,2]])
+print(res)
+ //다시해보기
