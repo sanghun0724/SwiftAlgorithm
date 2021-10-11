10c61380624e73c75caff8f413e7ba619a15c6f1
sanghun0724 <sanghun0724@naver.com>
2021-10-11 22:44:13 +0900

    * https://www.acmicpc.net/problem/1527

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,87 +1,24 @@
 
-let dirR = [1,-1,0,0]
-let dirC = [0,0,-1,1]
+let line = readLine()!.split(separator: " ").map { Int(String($0))!}
+let A = line[0]
+let B = line[1]
 
-let N = Int(readLine()!)!
-
-var graph = [[Int]]()
-for _ in 1...N {
-        graph.append(Array(readLine()!.split(separator: " ").map{ Int(String($0))!}))
+func dfs(low:Int,top:Int,depth:Int,num:Int)->Int {
+   var res = 0
+    if depth >= 10 {
+        return 0
+    }
+    if num > top {
+        return 0
+    }
+    if low <= num && num <= top {
+        res+=1
+    }
+    res += dfs(low: low, top: top, depth: depth+1, num: num*10+4)
+    res += dfs(low: low, top: top, depth: depth+1, num: num*10+7)
+    return res
 }
 
-struct shark {
-    var r:Int
-    var c:Int
-    var size:Int
-    var eat:Int
-}
-var ans = 0
+var ans = dfs(low: A, top: B, depth: 0, num: 0)
+print(ans)
 
-func feedShark(s: inout shark) -> Bool {
-    var dist = [[Int]](repeating: [Int](repeating: -1, count: 21), count: 21)
-    var queue = [(Int,Int)]()
-    queue.append((s.r,s.c))
-    dist[s.r][s.c] = 0
-    while !queue.isEmpty {
-        let current = queue.removeFirst()
-        let r = current.0
-        let c = current.1
-        if graph[r][c] != 9 && graph[r][c] != 0 && graph[r][c] < s.size { continue }
-        for i in 0...3 {
-            let nr = r + dirR[i]
-            let nc = c + dirC[i]
-            if nr >= 0 && nr < N && nc >= 0 && nc < N && dist[nr][nc] == -1 && graph[nr][nc] <= s.size {
-                dist[nr][nc] = dist[r][c] + 1
-                queue.append((nr,nc))
-            }
-        }
-    }
-    
-    var mr = 100
-    var mc = 100
-    var m_dist = 401
-    for r in (0..<N).reversed() { //역순으로 하면 위 왼쪽이 마지막에 업데이트 (결국이걸로 값설정되니 먼저 하는걸로댐 )
-        for c in (0..<N).reversed() {
-            if graph[r][c] != 0 && graph[r][c] != 9 && graph[r][c] < s.size && dist[r][c] != -1 && dist[r][c] <= m_dist {
-                m_dist = dist[r][c]
-                mr = r
-                mc = c
-            }
-        }
-    }
-    if m_dist == 401 {
-        return false
-    } else {
-        graph[s.r][s.c] = 0
-        s.r = mr
-        s.c = mc
-        s.eat+=1
-        if s.eat == s.size {
-            s.eat = 0
-            s.size+=1
-        }
-        graph[mr][mc] = 9
-        ans+=dist[mr][mc]
-        return true
-    }
-}
-
-var r = 0
-var c = 0
-for i in 0..<graph.count {
-    for j in 0..<graph[0].count {
-        if graph[i][j] == 9 {
-           r = i
-           c = j
-        }
-    }
-}
-var s = shark(r: r, c: c, size: 2, eat: 0)
-while true {
-    if feedShark(s: &s) == true {
-        continue
-    } else {
-        break
-    }
-}
-  print(ans)
