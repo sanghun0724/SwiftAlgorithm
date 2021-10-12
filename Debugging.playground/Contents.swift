8f55a15e0752c1f7613198644f045af15e63ce0b
sanghun0724 <sanghun0724@naver.com>
2021-10-13 00:22:03 +0900

    * https://www.acmicpc.net/problem/1021

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,22 +1,43 @@
 
-let line = readLine()!.split(separator: " ").map{ Int(String($0))!}
+let line = readLine()!.split(separator: " ").map { Int(String($0))! }
 let N = line[0]
 let M = line[1]
 
-var graph = [[Int]]()
-for _ in 1...N {
-    graph.append(Array(readLine()!.map{ Int(String($0))!}))
-}
-let lay = N < M ? N : M
+let targets = readLine()!.split(separator: " ").map { Int(String($0))! }
+var queue = [Int]()
+for i in 1...N { queue.append(i) }
+var index = 0
 var ans = 0
-for d in (0..<lay).reversed() {
-    for i in 0..<graph.count - d {
-        for j in 0..<graph[0].count - d {
-            if graph[i][j] == graph[i+d][j] && graph[i+d][j] == graph[i][j+d] && graph[i][j+d] == graph[i+d][j+d] {
-                ans = max(ans,(d+1)*(d+1))
+
+func goLeft() {
+    let q = queue.removeFirst()
+    queue.append(q)
+    ans+=1
+}
+
+func goRight() {
+    let q = queue.removeLast()
+    queue.insert(q, at: 0)
+    ans+=1
+}
+
+for i in 0..<targets.count {
+    if targets[i] != queue[index] {
+        let target = queue.firstIndex(of: targets[i])! + 1
+        let left = queue.count - target
+        let right = target - 1
+        while targets[i] != queue[index] {
+            if left < right {
+               goRight()
+            } else {
+                goLeft()
             }
         }
     }
+    if targets[i] == queue[index] {
+        queue.removeFirst()
+    }
 }
 print(ans)
+
 
