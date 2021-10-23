0ec208dcac7c90a485b20b325c435e08689620f5
sanghun0724 <sanghun0724@naver.com>
2021-10-23 13:33:03 +0900

    https://programmers.co.kr/learn/courses/30/lessons/42746

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,30 +1,11 @@
 
 import Foundation
 
-func solution(_ bridge_length:Int, _ weight:Int, _ truck_weights:[Int]) -> Int {
-    var queue = [(truck_weights.first!,1)]
-    var time = 1
-    var currentWeight = weight - queue.first!.0
-    var maximum = bridge_length - 1
-    var cur = 1
-    while !queue.isEmpty {
-            if queue[0].1 == bridge_length {
-                let w = queue.removeFirst()
-                currentWeight+=w.0
-                maximum+=1
-            }
-        if  cur < truck_weights.count , currentWeight >= truck_weights[cur] && maximum != 0  {
-                queue.append((truck_weights[cur],0))
-                currentWeight-=truck_weights[cur]
-                maximum-=1
-                cur+=1
-            }
-        for j in 0..<queue.count {
-            queue[j].1 += 1
-        }
-        time+=1
+func solution(_ numbers:[Int]) -> String {
+    let sortedArr = numbers.sorted{ Int("\($0)\($1)")! > Int("\($1)\($0)")! }
+    if sortedArr[0] == 0 {
+        return "0"
     }
-    return time
+    return sortedArr.reduce("") { $0 + "\($1)"}
 }
-
-solution(100, 100, [10, 10, 10, 10, 10, 10, 10, 10, 10, 10])
+solution([6,10,2])
