133d938d1f92e0c74c88bdd6fca8c1d5476d684a
sanghun0724 <sanghun0724@naver.com>
2021-10-24 17:28:10 +0900

    https://www.acmicpc.net/problem/2606

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,28 +1,27 @@
-
-import Foundation
-
-func solution(_ priorities:[Int], _ location:Int) -> Int {
-    var queue = priorities
-    var ans = 1
-    var locationArr = [Int]()
-    for i in 0..<priorities.count {
-        locationArr.append(i)
-    }
-   
- 
-    while true {
-        let cur = queue.removeFirst()
-        let loc = locationArr.removeFirst()
-        if queue.max() ?? -1 > cur {
-            queue.append(cur)
-            locationArr.append(loc)
-        } else {
-            if loc == location {
-                return ans
-            }
-            ans+=1
-        }
-    }
-    return 0
+let computers = Int(readLine()!)!
+let K = Int(readLine()!)!
+var links:[[Int]] = Array(repeating: [], count: computers+1)
+for _ in 0..<K {
+    let input = readLine()!.split(separator: " ").map { Int($0)! }
+      let src = input[0]
+      let dest = input[1]
+    
+    links[src].append(dest)
+    links[dest].append(src)
 }
-solution([1,1,9,1,1], 0)
+var queue:[Int] = [1]
+var visited:[Int] = []
+
+
+while !queue.isEmpty {
+    let cur = queue.removeFirst()
+    
+    if !visited.contains(cur) {
+        visited.append(cur)
+        queue.append(contentsOf: links[cur])
+    }
+}
+
+print(visited.count-1)
+
+
