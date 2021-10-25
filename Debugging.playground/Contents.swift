fb36b1e8e8f99309ef08790cd00863b77ed08c22
sanghun0724 <sanghun0724@naver.com>
2021-10-25 18:37:54 +0900

    https://www.acmicpc.net/problem/11403

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,27 +1,35 @@
-let computers = Int(readLine()!)!
-let K = Int(readLine()!)!
-var links:[[Int]] = Array(repeating: [], count: computers+1)
-for _ in 0..<K {
-    let input = readLine()!.split(separator: " ").map { Int($0)! }
-      let src = input[0]
-      let dest = input[1]
-    
-    links[src].append(dest)
-    links[dest].append(src)
+
+let k = Int(readLine()!)!
+var graph = [[Int]]()
+for _ in 1...k {
+    graph.append(Array(readLine()!.split(separator: " ").map{ Int(String($0))! }))
 }
-var queue:[Int] = [1]
-var visited:[Int] = []
-
-
-while !queue.isEmpty {
-    let cur = queue.removeFirst()
-    
-    if !visited.contains(cur) {
-        visited.append(cur)
-        queue.append(contentsOf: links[cur])
+var ans:[[Int]] = Array(repeating: Array(repeating: 0, count: k), count: k)
+var visited = Array(repeating: 0, count: k)
+func dfs(start:Int,limit:Int) {
+    for v in 0..<limit {
+        if visited[v] == 0 && graph[start][v] == 1 {
+            visited[v] = 1
+            dfs(start: v, limit: limit)
+        }
     }
 }
 
-print(visited.count-1)
+for i in 0..<k {
+    dfs(start: i, limit: k)
+    
+    for j in 0..<k {
+        ans[i][j] = visited[j]
+    }
+    
+    for n in 0..<k {
+        visited[n] = 0
+    }
+}
 
-
+for i in 0..<ans.count {
+    for j in 0..<ans[0].count {
+        print("\(ans[i][j])",terminator:" ")
+    }
+    print()
+}
