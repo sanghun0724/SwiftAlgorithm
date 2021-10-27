cbccc0b42c28d89c5a52a03b343cd2d10b8f7a23
sanghun0724 <sanghun0724@naver.com>
2021-10-27 16:36:47 +0900

    https://www.acmicpc.net/problem/1260

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,57 +1,64 @@
 
-let k = readLine()?.split(separator: " ").map{ Int(String($0))! }
-let n = k?[0] ?? 0
-let m = k?[1] ?? 0
-var links:[[Int]] = Array(repeating: [], count: n+2)
-for _ in 0..<m {
-    let tmp = readLine()?.split(separator: " ").map{ Int(String($0))! }
-    links[tmp?[1] ?? 0].append(tmp?[0] ?? 0)
+let line = readLine()!.split(separator: " ").map{ Int(String($0))! }
+let N = line[0]
+let M = line[1]
+let startPoint = line[2]
+var links:[[Int]] = Array(repeating: [], count: N+1)
+for _ in 1...M {
+    let tmp = readLine()!.split(separator: " ").map{ Int(String($0))! }
+    links[tmp[0]].append(tmp[1])
+    links[tmp[1]].append(tmp[0])
 }
-var visited = Array(repeating: 0, count: n+1)
+for i in 0..<links.count {
+    links[i].sort(by: <)
+}
 
-
-func dfs(count:Int,net:Int) -> Int {
-    if links[net].isEmpty {
-        return 0
-    }
-    var res = count
-    for i in 0..<links[net].count {
-        if visited[i] == 0 {
-            visited[i] = 1
-            res += dfs(count:res+1, net: links[net][i])
+var dfsArr = [startPoint]
+var bfsArr = [startPoint]
+var visited = Array(repeating: 0, count: N+1)
+visited[startPoint] = 1
+func dfs(start:Int) {
+    
+    for i in 0..<links[start].count {
+        if visited[links[start][i]] == 0 {
+            visited[links[start][i]] = 1
+            dfsArr.append(links[start][i])
+            dfs(start: links[start][i])
         }
     }
-    
-    return res
 }
-var resArr = [Int]()
-for i in 0..<n {
-    resArr.append(dfs(count: 1, net: i))
+
+func bfs(start:Int) {
+    var queue = [start]
     
-    
-    for i in 0..<n+1 {
+    while !queue.isEmpty {
+        let cur = queue.removeFirst()
+        for i in 0..<links[cur].count {
+            let v = links[cur][i]
+            if visited[v] == 0 {
+                visited[v] = 1
+                queue.append(v)
+                bfsArr.append(v)
+            }
+        }
+        
+    }
+}
+func resetVisited() {
+    for i in 1...N {
         visited[i] = 0
     }
 }
 
-var ansArr = [Int]()
-for (i,v) in resArr.enumerated() {
-    if resArr.max() == v {
-        ansArr.append(i)
-    }
+dfs(start: startPoint)
+resetVisited()
+visited[startPoint] = 1
+bfs(start: startPoint)
+
+for i in 0..<dfsArr.count {
+    print(dfsArr[i],terminator:" ")
 }
-
-
-if m == 0 {
-    if n != 0 {
-        for i in ansArr {
-            print(i+1,terminator:" ")
-        }
-    }
-} else {
-    for i in ansArr {
-        print(i,terminator:" ")
-    }
-}
-
+print()
+for i in 0..<bfsArr.count {
+    print(bfsArr[i],terminator:" ")
 }
