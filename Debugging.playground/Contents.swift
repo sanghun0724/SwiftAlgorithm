91497cc447d7639aa50d056293d557b9a67edd65
sanghun0724 <sanghun0724@naver.com>
2021-11-01 16:32:45 +0900

    https://www.acmicpc.net/problem/7569 BFS

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,36 +1,78 @@
 
-let first = Int(readLine()!)!
-var count = 0
-var res = [Int]()
-for _ in 1...first {
-    let k = Int(readLine()!)!
-    let map = readLine()!.split(separator: " ").map { Int(String($0))! }
-    var visitied = Array(repeating: 0, count: k+1)
-    var links = Array(repeating: 0, count:  k+1)
-    
-    for i in 1..<links.count {
-        links[i] = map[i-1]
-    }
-    
-    func dfs(start:Int) {
-            if visitied[start] == 0 {
-                visitied[start] = 1
-                dfs(start: links[start])
+
+
+
+let dirX = [1,0,-1,0,0,0]
+let dirY = [0,1,0,-1,0,0]
+let dirZ = [0,0,0,0,1,-1]
+let firstLine = Array(readLine()!.split(separator: " ").map{Int(String($0))!})
+let M = firstLine[0]
+let N = firstLine[1]
+let H = firstLine[2]
+var graph3D = [[[Int]]]()
+var queue = [(Int,Int,Int)]()
+for z in 0..<H {
+    var graph2D = [[Int]]()
+    for y in 0..<N {
+        graph2D.append(Array(readLine()!.split(separator: " ").map{Int(String($0))!}))
+        for x in 0..<graph2D[0].count {
+            if graph2D[y][x] == 1 {
+                queue.append((z,y,x))
             }
-    }
-    
-    
-    for j in 0..<k {
-        if visitied[map[j]] == 0 {
-            count+=1
-            dfs(start: map[j])
         }
     }
-    
-    res.append(count)
-    count = 0
+    graph3D.append(graph2D)
 }
 
-for r in res {
-    print(r)
+var days = 0
+
+var temp = [(Int,Int,Int)]()
+if queue.count == M * N * H {
+    print(0)
+} else {
+    bfs()
+    if check() == -1 {
+        print(-1)
+    } else {
+        print(days)
+    }
+}
+var index = 0
+func bfs() {
+    while queue.count > index {
+        let cur = queue[index]
+
+        for d in 0..<dirX.count {
+            let x = cur.2 + dirX[d]
+            let y = cur.1 + dirY[d]
+            let z = cur.0 + dirZ[d]
+
+            if z < H && y < N && x < M && z >= 0 && y >= 0 && x >= 0{
+                if graph3D[z][y][x] == 0 {
+                    graph3D[z][y][x] = 1
+                    temp.append((z,y,x))
+                }
+            }
+        }
+        if queue.count-1 == index,!temp.isEmpty {
+            days+=1
+            queue+=temp
+            temp.removeAll()
+        }
+        index+=1
+    }
+}
+
+func check() -> Int {
+    loop:for z in 0..<H {
+        for y in 0..<N {
+            for x in 0..<M {
+                if graph3D[z][y][x] == 0 {
+                    return -1
+                    break loop;
+                }
+            }
+        }
+    }
+    return 1
 }
