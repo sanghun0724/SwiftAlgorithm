217a773479d4fa87e17bb6f676705adeec7153cb
sanghun0724 <sanghun0724@naver.com>
2021-11-02 17:42:35 +0900

    BFS 통과

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -13,22 +13,29 @@ for _ in 1...N {
 }
 var visitied = Array(repeating: Array(repeating: 0, count: M), count: N)
 var ans = Int.max
-func dfs(count:Int,row:Int,col:Int) {
-    if row == N-1 && col == M-1 {
-        ans = min(ans, count)
-    }
-    
-    for d in 0..<4 {
-        let r = row + dirR[d]
-        let c = col + dirC[d]
-        if r >= 0 && c >= 0 && r < N && c < M && visitied[r][c] != 1 , graph[r][c] == 1 {
-            visitied[r][c] = 1
-             dfs(count: count+1, row: r, col: c)
-            visitied[r][c] = 0
+var queue = [(0,0,1)]
+func bfs() {
+    var index = 0
+    while queue.count > index {
+        let cur = queue[index]
+        
+        for d in 0..<4 {
+            let r = cur.0 + dirR[d]
+            let c = cur.1 + dirC[d]
+            let count = cur.2 + 1
+            if r >= 0 && c >= 0 && r < N && c < M && visitied[r][c] == 0, graph[r][c] == 1 {
+                if r == N-1 && c == M-1 {
+                    ans = min(count, ans)
+                }
+                visitied[r][c] = 1
+                queue.append((r,c,count))
+                
+            }
         }
+        index+=1
     }
 }
 
-dfs(count: 1, row: 0, col: 0)
+bfs()
 print(ans)
 
