7caed8bfbb3618b84451477416d98145ba5b8a09
sanghun0724 <sanghun0724@naver.com>
2021-11-03 21:17:46 +0900

    * https://www.acmicpc.net/problem/2468

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -5,45 +5,44 @@
 let k = Int(readLine()!)!
 var graph = [[Int]]()
 for _ in 1...k {
-    graph.append(Array(readLine()!).map{ Int(String($0))!})
+    graph.append(Array(readLine()!.split(separator: " ").map{ Int($0)!}))
 }
-var visitied = Array(repeating: Array(repeating: 0, count: graph[0].count), count: k)
 let dirR = [-1,1,0,0]
 let dirC = [0,0,-1,1]
-var counts = [Int]()
 
-func bfs(_ start:(Int,Int)) -> Int {
+//var visitied = Array(repeating: Array(repeating: 0, count: graph[0].count), count: graph.count)
+var res = 0
+var temp = graph
+func bfs(start:(Int,Int),height:Int)  {
     var queue = [start]
     var index = 0
-    var res = 0
     while queue.count > index {
         let cur = queue[index]
         
         for d in 0..<4 {
             let r = cur.0 + dirR[d]
             let c = cur.1 + dirC[d]
-            
-            if r >= 0 && c >= 0 && r < graph.count && c < graph[0].count && visitied[r][c] == 0,graph[r][c] == 1 {
-                visitied[r][c] = 1
-                res+=1
+        
+            if r >= 0 && c >= 0 && r < graph.count && c < graph[0].count && graph[r][c] > height {
                 queue.append((r,c))
+                graph[r][c] = -1
             }
-        }
+         }
         index+=1
     }
-    return res == 0 ? 1 : res
+    res+=1
 }
-
-for i in 0..<graph.count {
-    for j in 0..<graph[0].count {
-        if graph[i][j] == 1 && visitied[i][j] == 0 {
-            counts.append(bfs((i,j)))
+var ans = 0
+for h in 1...100 {
+    for i in 0..<graph.count {
+        for j in 0..<graph[0].count {
+            if graph[i][j] > h {
+                bfs(start: (i,j), height: h)
+            ans = max(ans, res)
+            }
         }
     }
+    graph = temp
+    res = 0
 }
-
-print(counts.count)
-counts.sort(by: <)
-for c in counts {
-    print(c)
-}
+print(ans == 0 ? 1 : ans)
