4c6b8a70ccc0dedb1e9bcd297e5b50025c883a92
sanghun0724 <sanghun0724@naver.com>
2021-11-03 14:43:57 +0900

    https://www.acmicpc.net/problem/2667 BFS

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -2,40 +2,48 @@
 
 
 
-let first = Array(readLine()!.split(separator: " ").map { Int($0)! })
-let N = first[0]
-let M = first[1]
+let k = Int(readLine()!)!
+var graph = [[Int]]()
+for _ in 1...k {
+    graph.append(Array(readLine()!).map{ Int(String($0))!})
+}
+var visitied = Array(repeating: Array(repeating: 0, count: graph[0].count), count: k)
 let dirR = [-1,1,0,0]
 let dirC = [0,0,-1,1]
-var graph = [[Int]]()
-for _ in 1...N {
-    graph.append(Array(readLine()!).map{ Int(String($0))! })
-}
-var visitied = Array(repeating: Array(repeating: 0, count: M), count: N)
-var ans = Int.max
-var queue = [(0,0,1)]
-func bfs() {
+var counts = [Int]()
+
+func bfs(_ start:(Int,Int)) -> Int {
+    var queue = [start]
     var index = 0
+    var res = 0
     while queue.count > index {
         let cur = queue[index]
         
         for d in 0..<4 {
             let r = cur.0 + dirR[d]
             let c = cur.1 + dirC[d]
-            let count = cur.2 + 1
-            if r >= 0 && c >= 0 && r < N && c < M && visitied[r][c] == 0, graph[r][c] == 1 {
-                if r == N-1 && c == M-1 {
-                    ans = min(count, ans)
-                }
+            
+            if r >= 0 && c >= 0 && r < graph.count && c < graph[0].count && visitied[r][c] == 0,graph[r][c] == 1 {
                 visitied[r][c] = 1
-                queue.append((r,c,count))
-                
+                res+=1
+                queue.append((r,c))
             }
         }
         index+=1
     }
+    return res == 0 ? 1 : res
 }
 
-bfs()
-print(ans)
+for i in 0..<graph.count {
+    for j in 0..<graph[0].count {
+        if graph[i][j] == 1 && visitied[i][j] == 0 {
+            counts.append(bfs((i,j)))
+        }
+    }
+}
 
+print(counts.count)
+counts.sort(by: <)
+for c in counts {
+    print(c)
+}
