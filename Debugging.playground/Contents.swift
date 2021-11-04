1ff0a39a276c2a594d4b62f1a61815a5efb1f754
sanghun0724 <sanghun0724@naver.com>
2021-11-04 16:14:20 +0900

    https://www.acmicpc.net/problem/9205

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -2,47 +2,55 @@
 
 
 
-let k = Int(readLine()!)!
-var graph = [[Int]]()
-for _ in 1...k {
-    graph.append(Array(readLine()!.split(separator: " ").map{ Int($0)!}))
-}
-let dirR = [-1,1,0,0]
-let dirC = [0,0,-1,1]
-
-//var visitied = Array(repeating: Array(repeating: 0, count: graph[0].count), count: graph.count)
-var res = 0
-var temp = graph
-func bfs(start:(Int,Int),height:Int)  {
-    var queue = [start]
-    var index = 0
-    while queue.count > index {
-        let cur = queue[index]
-        
-        for d in 0..<4 {
-            let r = cur.0 + dirR[d]
-            let c = cur.1 + dirC[d]
-        
-            if r >= 0 && c >= 0 && r < graph.count && c < graph[0].count && graph[r][c] > height {
-                queue.append((r,c))
-                graph[r][c] = -1
-            }
-         }
-        index+=1
+let t = Int(readLine()!)!
+for _ in 1...t {
+    let n = Int(readLine()!)!
+    var home = [Int]()
+    var dest = [Int]()
+    var conv = [[Int]]()
+    for i in 1...n+2 {
+        if i == 1 {
+            home = Array(readLine()!).split(separator: " ").map{Int(String($0))!}
+        } else if i == n+2 {
+            dest = Array(readLine()!).split(separator: " ").map{Int(String($0))!}
+        } else {
+            conv.append(Array(readLine()!).split(separator: " ").map{Int(String($0))!})
+        }
     }
-    res+=1
-}
-var ans = 0
-for h in 1...100 {
-    for i in 0..<graph.count {
-        for j in 0..<graph[0].count {
-            if graph[i][j] > h {
-                bfs(start: (i,j), height: h)
-            ans = max(ans, res)
+    var nodes:[[Int]] = []
+    nodes.append(home)
+    nodes += conv
+    nodes.append(dest)
+    
+    var canGoTable = [[Bool]](repeating: [Bool](repeating: false, count: nodes.count), count: nodes.count)
+    for i in 0..<nodes.count {
+        for j in 0..<nodes.count {
+            if abs(nodes[i][0]  - nodes[j][0]) + abs(nodes[i][1] - nodes[j][1]) <= 1000 {
+                canGoTable[i][j] = true
             }
         }
     }
-    graph = temp
-    res = 0
+    
+    var result = [Bool](repeating: false, count: canGoTable.count)
+    result[0] = true
+    
+    var queue = [0]
+    
+    while !queue.isEmpty {
+        let now = queue.removeFirst()
+        
+        for i in 0..<canGoTable[now].count {
+            if canGoTable[now][i] {
+                if !result[i] {
+                    result[i] = true
+                    queue.append(i)
+                }
+            }
+        }
+    }
+    if result[nodes.count-1] {
+        print("happy")
+    } else {
+        print("sad")
+    }
 }
-print(ans == 0 ? 1 : ans)
