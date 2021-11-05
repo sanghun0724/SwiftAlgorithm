221b83d064ff8492738ac9cdedf0c7b9bc9cedad
sanghun0724 <sanghun0724@naver.com>
2021-11-05 22:37:04 +0900

    https://www.acmicpc.net/problem/2644 BFS

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,56 +1,41 @@
 
 
 
+let nums = Int(readLine()!)!
+let targets = readLine()!.split(separator: " ").map { Int(String($0))! }
+let k = Int(readLine()!)!
+var links = [[Int]](repeating: [], count: nums+1)
+for _ in 1...k {
+    let connection = readLine()!.split(separator: " ").map { Int($0)! }
+    links[connection[0]].append(connection[1])
+    links[connection[1]].append(connection[0])
+}
+let start = targets[0]
+let target = targets[1]
+var visited = [Int](repeating: 0, count: nums+1)
 
-let t = Int(readLine()!)!
-for _ in 1...t {
-    let n = Int(readLine()!)!
-    var home = [Int]()
-    var dest = [Int]()
-    var conv = [[Int]]()
-    for i in 1...n+2 {
-        if i == 1 {
-            home = Array(readLine()!).split(separator: " ").map{Int(String($0))!}
-        } else if i == n+2 {
-            dest = Array(readLine()!).split(separator: " ").map{Int(String($0))!}
-        } else {
-            conv.append(Array(readLine()!).split(separator: " ").map{Int(String($0))!})
-        }
-    }
-    var nodes:[[Int]] = []
-    nodes.append(home)
-    nodes += conv
-    nodes.append(dest)
-    
-    var canGoTable = [[Bool]](repeating: [Bool](repeating: false, count: nodes.count), count: nodes.count)
-    for i in 0..<nodes.count {
-        for j in 0..<nodes.count {
-            if abs(nodes[i][0]  - nodes[j][0]) + abs(nodes[i][1] - nodes[j][1]) <= 1000 {
-                canGoTable[i][j] = true
-            }
-        }
-    }
-    
-    var result = [Bool](repeating: false, count: canGoTable.count)
-    result[0] = true
-    
-    var queue = [0]
-    
-    while !queue.isEmpty {
-        let now = queue.removeFirst()
+
+func bfs() {
+    var queue = [start]
+    var index = 0
+
+    while queue.count > index {
+        let cur = queue[index]
         
-        for i in 0..<canGoTable[now].count {
-            if canGoTable[now][i] {
-                if !result[i] {
-                    result[i] = true
-                    queue.append(i)
-                }
+        for man in links[cur] {
+            if man != start && visited[man] == 0 {
+                visited[man] = visited[cur] + 1
+                queue.append(man)
             }
         }
-    }
-    if result[nodes.count-1] {
-        print("happy")
-    } else {
-        print("sad")
+        index+=1
     }
 }
+bfs()
+
+if visited[target] == 0 {
+    print(-1)
+} else {
+    print(visited[target])
+}
+
