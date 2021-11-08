9a0e95b8cecd39eadf6bbc2fcab31256d70577b3
sanghun0724 <sanghun0724@naver.com>
2021-11-08 19:20:42 +0900

    https://www.acmicpc.net/problem/14502

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,24 +1,83 @@
 
 
-
-public class ListNode {
-    public var val: Int
-    public var next: ListNode?
-    public init() { self.val = 0; self.next = nil; }
-    public init(_ val: Int) { self.val = val; self.next = nil; }
-    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+let line = readLine()!.split(separator: " ").map { Int(String($0))! }
+var graph = [[Int]]()
+for _ in 1...line[0] {
+    graph.append(readLine()!.split(separator: " ").map { Int(String($0))! })
 }
+let dirR = [-1,1,0,0]
+let dirC = [0,0,-1,1]
 
-class Solution {
-    func reverseList(_ head: ListNode?) -> ListNode? {
-        var resultNode:ListNode? = nil
-        var node = head
-        while node != nil {
-            var tmp = ListNode(node!.val,resultNode)
-            resultNode = tmp
-            node = node?.next
+
+//2   바이러스 퍼지게
+func bfs() {
+    for r in 0..<graph.count {
+        for c in 0..<graph[0].count {
+            if graph[r][c] == 2 {
+                var queue = [(r,c)]
+                var index = 0
+                
+                while queue.count > index {
+                    let cur = queue[index]
+                    
+                    for d in 0..<4 {
+                        let r = cur.0 + dirR[d]
+                        let c = cur.1 + dirC[d]
+                        
+                        if r >= 0 && c >= 0 && r < graph.count && c < graph[0].count && graph[r][c] == 0 {
+                            graph[r][c] = 2
+                            queue.append((r,c))
+                        }
+                    }
+                    index+=1
+                }
+            }
         }
-        return resultNode
     }
 }
 
+func check() -> Int{
+    var res = 0
+    for i in 0..<graph.count {
+        for j in 0..<graph[0].count {
+            if graph[i][j] == 0 {
+                res+=1
+            }
+        }
+    }
+    return res
+}
+
+var result = 0
+
+var tmp = graph
+
+loop:for i in 0..<graph.count {
+    for j in 0..<graph[0].count {
+        if tmp[i][j] == 0 {
+            tmp[i][j] = 1
+            for n in 0..<graph.count {
+                for m in 0..<graph[0].count {
+                    if tmp[n][m] == 0 {
+                        tmp[n][m] = 1
+                        for r in 0..<graph.count {
+                            for c in 0..<graph[0].count {
+                                if tmp[r][c] == 0 {
+                                    tmp[r][c] = 1
+                                    graph = tmp
+                                    bfs()
+                                    result = max(result,check())
+                                    tmp[r][c] = 0
+                                }
+                            }
+                        }
+                        tmp[n][m] = 0
+                    }
+                }
+            }
+            tmp[i][j] = 0
+        }
+    }
+}
+
+print(result)
