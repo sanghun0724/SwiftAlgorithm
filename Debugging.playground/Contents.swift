23d341d58663b2b6d5adf9fabb29074509ef896f
sanghun0724 <sanghun0724@naver.com>
2021-11-14 22:56:19 +0900

    N-Qeens BackTracking

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,32 +1,65 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     public var val: Int
- *     public var next: ListNode?
- *     public init(_ val: Int) {
- *         self.val = val
- *         self.next = nil
- *     }
- * }
- */
-
 class Solution {
-    func hasCycle(_ head: ListNode?) -> Bool {
-    if head == nil {
-        return false
-    }
-    
-    var slow = head
-    var fast = head
-    
-    while fast?.next != nil && fast?.next?.next != nil {
-        slow = slow?.next
-        fast = fast?.next?.next
-        
-        if slow === fast {
-            return true
+    var tmpR = Array(repeating: 0, count: 10)
+    var tmpC = Array(repeating: 0, count: 10)
+    func totalNQueens(_ n: Int) -> Int {
+        var total = 0
+        for i in 0..<n {
+            total += go(0,i,n)
         }
+        return total
     }
-    return false
+    func go(_ row:Int,_ col:Int,_ n:Int) -> Int {
+        
+        for i in 0..<row {
+            if tmpC[i] == col { return 0 }
+            if tmpR[i] == row { return 0 }
+            if (abs(col-tmpC[i]) == abs(row-tmpR[i])) { return 0 }
+        }
+        
+        if row == n-1 {
+            return 1
+        }
+        
+        tmpR[row] = row
+        tmpC[row] = col
+        
+        var res = 0
+        for i in 0..<n {
+            res += go(row+1, i,n)
+        }
+    
+        return res
     }
+}
+
+
+var n = Int(readLine()!)!
+var res = 0
+var tmpR = Array(repeating: 0, count: 16)
+var tmpC = Array(repeating: 0, count: 16)
+for i in 0..<n {
+    res += go(0,i)
+}
+print(res)
+
+func go(_ row:Int,_ col:Int) -> Int {
+    for i in 0..<row {
+        if row == tmpR[i] { return 0 }
+        if col == tmpC[i] { return 0 }
+        if (abs(col-tmpC[i]) == abs(row-tmpR[i])) { return 0 }
+    }
+    
+    if row == n-1 {
+        return 1
+    }
+    
+    tmpR[row] = row
+    tmpC[row] = col
+    
+    var r = 0
+    for i in 0..<n {
+        r += go(row+1, i)
+    }
+    
+    return r
 }
