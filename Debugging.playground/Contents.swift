cc396ac1bfa8dbaedde39d617da73919afa77fb9
sanghun0724 <sanghun0724@naver.com>
2021-11-16 18:16:38 +0900

    backtracking subset

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,65 +1,17 @@
 class Solution {
-    var tmpR = Array(repeating: 0, count: 10)
-    var tmpC = Array(repeating: 0, count: 10)
-    func totalNQueens(_ n: Int) -> Int {
-        var total = 0
-        for i in 0..<n {
-            total += go(0,i,n)
+    func subsets(_ nums: [Int]) -> [[Int]] {
+         func subsets(_ nums: [Int]) -> [[Int]] {
+        var ans:[[Int]] = []
+        for i in 0..<nums.count{
+            for j in 0..<ans.count{
+                var temp = ans[j]
+                temp.append(nums[i])
+                ans.append(temp)
+            }
+            ans.append([nums[i]])
         }
-        return total
+        ans.append([])
+        return ans
     }
-    func go(_ row:Int,_ col:Int,_ n:Int) -> Int {
-        
-        for i in 0..<row {
-            if tmpC[i] == col { return 0 }
-            if tmpR[i] == row { return 0 }
-            if (abs(col-tmpC[i]) == abs(row-tmpR[i])) { return 0 }
-        }
-        
-        if row == n-1 {
-            return 1
-        }
-        
-        tmpR[row] = row
-        tmpC[row] = col
-        
-        var res = 0
-        for i in 0..<n {
-            res += go(row+1, i,n)
-        }
-    
-        return res
     }
-}
-
-
-var n = Int(readLine()!)!
-var res = 0
-var tmpR = Array(repeating: 0, count: 16)
-var tmpC = Array(repeating: 0, count: 16)
-for i in 0..<n {
-    res += go(0,i)
-}
-print(res)
-
-func go(_ row:Int,_ col:Int) -> Int {
-    for i in 0..<row {
-        if row == tmpR[i] { return 0 }
-        if col == tmpC[i] { return 0 }
-        if (abs(col-tmpC[i]) == abs(row-tmpR[i])) { return 0 }
-    }
-    
-    if row == n-1 {
-        return 1
-    }
-    
-    tmpR[row] = row
-    tmpC[row] = col
-    
-    var r = 0
-    for i in 0..<n {
-        r += go(row+1, i)
-    }
-    
-    return r
 }
