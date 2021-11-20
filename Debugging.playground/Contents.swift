3dd40db466ff8110ba1c8045914ac073f99121c2
sanghun0724 <sanghun0724@naver.com>
2021-11-20 20:35:29 +0900

    WinterCoding Test

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,25 +1,111 @@
 
-class Solution {
-    func permute(_ nums: [Int]) -> [[Int]] {
-        class Solution {
-    func permute(_ nums: [Int]) -> [[Int]] {
-        var result = [[Int]]()
-        var nums = nums
-        recurse(0, &nums, &result)
-        return result
+
+import Foundation
+
+func solution(_ character:String, _ monsters:[String]) -> String {
+    var maxExe = 0.0
+    var totalExe = 0.0
+    var indexVal = 0
+    var res = ""
+    //player
+    let player = character.split(separator: " ").map{ Int(String($0))!}
+    var playerHP = player[0]
+    let playerAttack = player[1]
+    let playerDefence = player[2]
+    let tmp = playerHP
+    //Monster
+    loop:for i in 0..<monsters.count {
+        var monster = monsters[i].split(separator: " ").map { String($0) }
+        let monsterName = monster.removeFirst()
+        let monsterInfo = monster.map{ Int($0)! }
+        let value = Double(monsterInfo[0])
+        var monsterHP = monsterInfo[1]
+        let monsterAttack = monsterInfo[2]
+        let monsterDefence = monsterInfo[3]
+
+
+        var sec = 0.0
+        while true {
+            playerHP = tmp
+            sec+=1
+            if playerAttack - monsterDefence > 0 {
+                monsterHP -= (playerAttack-monsterDefence)
+            }
+            if monsterHP <= 0 {
+                break
+            }
+            if monsterAttack - playerDefence > 0 {
+                playerHP -= (monsterAttack-playerDefence)
+            }
+            if playerHP <= 0 {
+                continue loop;
+            }
+            if sec > 1000.0 {
+                continue loop;
+            }
+        }
+        let exe = value / sec
+        if maxExe < exe {
+            maxExe = exe
+            res = monsterName
+            totalExe = value
+            indexVal = i
+        } else if maxExe == exe {
+            if totalExe < value {
+                maxExe = exe
+                res = monsterName
+                totalExe = value
+                indexVal = i
+            } else if totalExe == value {
+                if indexVal > i {
+                    maxExe = exe
+                    res = monsterName
+                    totalExe = value
+                    indexVal = i
+                }
+            }
+        }
     }
+    return res
+}
+
+
+solution("10 5 2", ["Knight 3 10 10 3","Wizard 5 10 15 1","Beginner 1 1 15 1"])
+
+
+
+import Foundation
+
+func solution(_ time:Int, _ gold:Int, _ upgrade:[[Int]]) -> Int {
+    var res = 0
     
-    func recurse(_ first: Int, _ nums: inout [Int], _ result: inout [[Int]]) {
-        if first == nums.count {
-            result.append(nums)
-            return
+    
+    let firstTime =  upgrade[0][1]
+    res = gold * (time/firstTime)
+    
+    loop:for i in 1...upgrade.count {
+        
+        var tmpTime = time
+        var curMoney = 0
+        for i in 0..<upgrade.count - 1  {
+           let interval = upgrade[i][1]
+          
+            while curMoney < upgrade[i+1][1] {
+                tmpTime -= interval
+                curMoney += gold
+                if tmpTime < interval {
+                    res = max(res,curMoney)
+                    continue loop;
+                }
+            }
+            curMoney -= upgrade[i+1][0]
         }
-        for index in first..<nums.count {
-            nums.swapAt(first, index)
-            self.recurse(first+1, &nums, &result)
-            nums.swapAt(first, index)
+        if tmpTime > 0 {
+            tmpTime -= upgrade.last![1]
+            curMoney += gold
         }
+        res = max(res, curMoney)
     }
+    return res
 }
-    }
-}
+solution(100, 200, [[0, 5], [1500, 3], [3000, 1]])
