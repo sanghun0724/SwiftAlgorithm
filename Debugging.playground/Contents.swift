0afb0eda5a6f599444cdd11cb0360ebc14e94782
sanghun0724 <sanghun0724@naver.com>
2021-11-22 21:18:10 +0900

    https://www.acmicpc.net/problem/16234 BFS

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -5,69 +5,71 @@ let N = first[0]
 let L = first[1]
 let R = first[2]
 
+var union = [[(Int,Int)]]()
 var map = [[Int]]()
 for _ in 1...N {
     map.append(readLine()!.split(separator: " ").map { Int($0)! })
 }
-//bfs
 var days = 0
-var visiting = [[Int]](repeating: [Int](repeating: 0, count: map[0].count), count: map.count)
+var visiting = [[Int]](repeating: [Int](repeating: 0, count: N), count: N)
 var tmpVisiting = visiting
 
-var before = map
-var after = bfs(map)
 
-while true {
-    if before == after {
-        print(days)
-        break
-    } else {
-        days+=1
-        visiting = tmpVisiting
-        before = after
-        after = bfs(after)
-    }
-}
-
-func bfs(_ graph:[[Int]]) -> [[Int]] {
- var queue = [(0,0)]
+func bfs(_ locR:Int,_ locC:Int) {
+ var queue = [(locR,locC)]
  var index = 0
- var count = 1
- var tmp = Set<[Int]>()
+    
     while queue.count > index {
         let cur = queue[index]
         let r = cur.0
         let c = cur.1
+        visiting[r][c] = 1
         
-        for d in 0..<3 {
+        for d in 0..<4 {
             let row = r + dirR[d]
             let col = c + dirC[d]
-            
-            if row < 0 || col < 0 || row >= graph.count || col >= graph[0].count || visiting[row][col] == 1 {
+
+            if row < 0 || col < 0 || row >= N || col >= N || visiting[row][col] == 1 {
                 continue;
             }
-            
-            visiting[row][col] = 1
-            if abs(graph[r][c] - graph[row][col]) >= L && abs(graph[r][c] - graph[row][col]) <= R {
-                count+=1
-                tmp.insert([row,col])
-                tmp.insert([r,c])
+            if  abs(map[r][c] - map[row][col]) >= L && abs(map[r][c] - map[row][col]) <= R {
+                visiting[row][col] = 1
+                queue.append((row,col))
             }
-            
         }
         index+=1
     }
-    var total = 0
-    for loc in tmp {
-        total += graph[loc[0]][loc[1]]
-    }
-    let people = total / count
-    var res = graph
-    for loc in tmp {
-        res[loc[0]][loc[1]] = people
-    }
-    return res
+    union.append(queue)
 }
 
-
-
+while true {
+    union = [[(Int,Int)]]()
+    visiting = tmpVisiting
+    for i in 0..<N {
+        for j in 0..<N {
+            if visiting[i][j] == 0 {
+                bfs(i, j)
+            }
+        }
+    }
+    
+    if union.count == N * N {
+        break;
+    }
+    
+    for i in 0..<union.count {
+        var sum = 0
+        if union[i].count == 1 {
+            continue
+        }
+        
+        for j in union[i] {
+            sum += map[j.0][j.1]
+        }
+        for j in union[i] {
+            map[j.0][j.1] = sum / union[i].count
+        }
+    }
+    days+=1
+}
+print(days)
