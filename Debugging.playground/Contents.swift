1143377f71745b063bf1495b06c905cfbc3089b2
sanghun0724 <sanghun0724@naver.com>
2021-11-27 15:56:19 +0900

    https://leetcode.com/explore/learn/card/recursion-ii/472/backtracking/2798/

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,41 +1,21 @@
+var res = Set<[Int]>()
+func combine(_ n: Int, _ k: Int) -> [[Int]] {
+     helper([], n, k)
+    return Array(res)
+}
 
-func solveSudoku(_ board: inout [[Character]]) {
-    if board.isEmpty || board[0].isEmpty { return }
-    helper(&board)
-}
-func helper(_ board: inout [[Character]]) -> Bool{
-    func isValid(_ row:Int,_ col:Int,_ num:Character) -> Bool {
-        let n = board.count , m = board[0].count
-        
-        for r in 0..<n where board[r][col] == num {
-            return false
-        }
-        
-        for c in 0..<m where board[row][c] == num {
-            return false
-        }
-        
-        for r in row / 3 * 3 ..< row / 3 * 3 + 3 {
-            for c in col / 3 * 3 ..< col / 3 * 3 + 3  where board[r][c] == num {
-                return false
-            }
-        }
-        return true
+func helper(_ arr:[Int],_ n:Int,_ k:Int) {
+    if arr.count == k {
+        res.insert(arr)
+        return
     }
-    
-    for i in 0..<board.count {
-        for j in 0..<board[0].count where board[i][j] == "." {
-            for num in 1...9 where isValid(i, j, Character("\(num)")) {
-                    board[i][j] = Character("\(num)")
-                if helper(&board) {
-                    return true
-                } else {
-                    board[i][j] = "."
-                }
-            }
-            return false
-        }
+    var arr1 = arr
+    for i in 1...n {
+        if arr1.contains(i) { return }
+            arr1.append(i)
+        helper(arr1, n, k)
+            arr1.removeLast()
     }
-    return true
 }
+
 
