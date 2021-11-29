e79080e8c1a7a9058b2814394c1045c9ed35206d
sanghun0724 <sanghun0724@naver.com>
2021-11-29 23:03:25 +0900

    N-Queen bset soultion

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,23 +1,31 @@
-let n = Int(readLine()!)!
-let digits = readLine()!.split(separator: " ").map{Int($0)!}
-let operations = readLine()!.split(separator: " ").map{Int($0)!}
+func solution(_ n:Int) -> Int {
+    var chess = [Int].init(repeating: -1, count: n)
+    var answer = 0
 
-var Max = Int.min
-var Min = Int.max
+    func check(_ x: Int, _ y: Int) -> Bool {
+        for i in 0..<x {
+            if chess[i] == y || abs(x - i) - abs(y - chess[i]) == 0 { return false }
+        }
 
-
-func dfs(_ plus:Int,_ minus:Int,_ multiple:Int,_ divide:Int,_ index:Int,_ sum:Int) {
-    if index == n-1 {
-        Max = max(Max, sum)
-        Min = min(Min,sum)
+        return true
     }
-    
-    if plus > 0  { dfs(plus-1, minus, multiple, divide, index+1, sum+digits[index+1])}
-    if minus > 0 { dfs(plus, minus-1, multiple, divide, index+1, sum-digits[index+1])}
-    if multiple > 0 { dfs(plus, minus, multiple-1, divide, index+1, sum*digits[index+1])}
-    if divide > 0 { dfs(plus, minus, multiple, divide-1, index+1, sum/digits[index+1])}
-}
 
-dfs(operations[0], operations[1], operations[2], operations[3], 0, digits[0])
-print(Max)
-print(Min)
+    func dfs(_ cnt: Int) {
+        if cnt == n {
+            answer += 1
+            return
+        }
+
+        for i in 0..<n {
+            if check(cnt, i) {
+                chess[cnt] = i
+                dfs(cnt + 1)
+                chess[cnt] = -1
+            }
+        }
+    }
+
+    dfs(0)
+
+    return answer
+}
