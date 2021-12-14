4250f20851ed3c746db7d0a4fc9f21b03342cab3
sanghun0724 <sanghun0724@naver.com>
2021-12-14 22:11:28 +0900

    merge Sort

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,31 +1,35 @@
-func solution(_ n:Int) -> Int {
-    var chess = [Int].init(repeating: -1, count: n)
-    var answer = 0
 
-    func check(_ x: Int, _ y: Int) -> Bool {
-        for i in 0..<x {
-            if chess[i] == y || abs(x - i) - abs(y - chess[i]) == 0 { return false }
-        }
 
-        return true
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+}
+
+
+class Solution {
+func mergeTwoLists(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+   return recursion(l1, l2, nil)
+}
+func recursion(_ node1:ListNode?,_ node2:ListNode?,_ result:ListNode?) -> ListNode? {
+    guard node1 != nil || node2 != nil else {
+        return nil
     }
-
-    func dfs(_ cnt: Int) {
-        if cnt == n {
-            answer += 1
-            return
-        }
-
-        for i in 0..<n {
-            if check(cnt, i) {
-                chess[cnt] = i
-                dfs(cnt + 1)
-                chess[cnt] = -1
-            }
-        }
+   
+    var val1 = node1?.val ?? Int.max
+    var val2 = node2?.val ?? Int.max
+    
+    var result:ListNode
+    
+    if val1 > val2 {
+        result = ListNode(val2)
+        result.next = recursion(node1,node2?.next,result.next)
+    } else {
+        result = ListNode(val1)
+        result.next = recursion(node1?.next, node2,result.next)
     }
-
-    dfs(0)
-
-    return answer
+    return result
+  }
 }
