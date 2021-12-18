5c8bb7764f7fa177954ef7157db6c77b47dcc64b
sanghun0724 <sanghun0724@naver.com>
2021-12-18 15:04:16 +0900

    Largest Rectangle in Histogram

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,58 +1,51 @@
-class Solution {
-    func generateParenthesis(_ n: Int) -> [String] {
-        var list = [String]()
-        backTracking(&list, "", 0, 0, n)
-        return list
-    }
-    
-    func backTracking(_ list: inout [String],_ str:String,_ left:Int,_ right:Int,_ max:Int)  {
-        if str.count == max * 2 {
-            list.append(str)
-            return
-        }
-        
-        if left < max {
-            backTracking(&list, str + "(", left+1, right, max)
-        }
-        
-        if right < left {
-            backTracking(&list, str + ")", left, right+1, max)
-        }
-        
-    }
-    
-}
+//
+//func largestRectangleArea(_ heights: [Int]) -> Int {
+//    var res = 0
+//    var heights = heights
+//    for idx in 0..<heights.count {
+//      let tmp = backTracking( &heights,idx,idx,0)
+//        res < tmp ? (res=tmp) : (res=res)
+//    }
+//    return res
+//}
+//
+//func backTracking(_ heights: inout [Int],_ idx:Int,_ start:Int,_ val:Int) -> Int {
+//    if start > idx {
+//        return val
+//    }
+//    let val = heights[start] * idx+1 - start
+//    var tmp = backTracking(&heights, idx+1, start, val)
+//    return tmp
+//}
 
-func generateParenthesis(_ n: Int) -> [String] {
-      // count is the number of open parenthesis minus the number of close parenthesis
-      // in the partialString
-      typealias PartialResult = (partialString: String, count: Int)
-      var prior = [PartialResult]()
-      var current = [PartialResult]()
-      prior.append(("", 0))
-      
-      // Simple iteration, prior holds all possible partial results of size index - 1
-      // we accumulate all partial results of size index in current
-      // We run 2 * n iterations because we have to place n open parenthesis and n close parenthesis
-      for index in 1...(2 * n) {
-          for instance in prior {
-              // The number of remaining parenthesis to add is 2 * n - index, if count equals that
-              // everything else needs to be a close parenthesis
-              if instance.count < 2 * n - index {
-                  current.append((instance.partialString + "(", instance.count + 1))
-              }
-              
-              // If count is 0 there are no unclosed parenthesis in the partialString so we can only
-              // add an open parenthesis at this point
-              if instance.count > 0 {
-                  current.append((instance.partialString + ")", instance.count - 1))
-              }
-          }
+
+func largestRectangleArea(_ heights: [Int]) -> Int {
         
-          prior = current
-          current = [PartialResult]()
-      }
-    print(prior)
-      return prior.map({ $0.partialString })
-  }
-generateParenthesis(3)
+        var heights = heights
+        heights.append(0)
+        
+        var result = 0
+        
+        var stack = [Int]()
+        
+        for i in heights.indices {
+            
+            while stack.count > 0 && heights[i] <= heights[stack.last!] {
+                
+                var height = heights[stack.removeLast()]
+                var span = i
+                if stack.count > 0 {
+                    span = i - stack.last! - 1
+                }
+                result = max(result, span * height)
+                print(result)
+            }
+            
+            stack.append(i)
+            
+        }
+        
+        return result
+    }
+
+largestRectangleArea([2,1,5,6,2,3])
