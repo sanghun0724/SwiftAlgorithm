db437089c24dda05f1a66412e382ac681ca352f9
sanghun0724 <sanghun0724@naver.com>
2021-12-22 17:49:32 +0900

    more clear purmutations

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,51 +1,25 @@
-//
-//func largestRectangleArea(_ heights: [Int]) -> Int {
-//    var res = 0
-//    var heights = heights
-//    for idx in 0..<heights.count {
-//      let tmp = backTracking( &heights,idx,idx,0)
-//        res < tmp ? (res=tmp) : (res=res)
-//    }
-//    return res
-//}
-//
-//func backTracking(_ heights: inout [Int],_ idx:Int,_ start:Int,_ val:Int) -> Int {
-//    if start > idx {
-//        return val
-//    }
-//    let val = heights[start] * idx+1 - start
-//    var tmp = backTracking(&heights, idx+1, start, val)
-//    return tmp
-//}
-
-
-func largestRectangleArea(_ heights: [Int]) -> Int {
-        
-        var heights = heights
-        heights.append(0)
-        
-        var result = 0
-        
-        var stack = [Int]()
-        
-        for i in heights.indices {
-            
-            while stack.count > 0 && heights[i] <= heights[stack.last!] {
-                
-                var height = heights[stack.removeLast()]
-                var span = i
-                if stack.count > 0 {
-                    span = i - stack.last! - 1
-                }
-                result = max(result, span * height)
-                print(result)
-            }
-            
-            stack.append(i)
-            
-        }
-        
+func permute(_ nums: [Int]) -> [[Int]] {
+        var result = [[Int]]()
+        permute(nums, nums.count, [Int](), Set<Int>(), &result)
         return result
     }
-
-largestRectangleArea([2,1,5,6,2,3])
+    
+    func permute(_ nums: [Int], _ index: Int, _ permutation: [Int], _ visited: Set<Int>, _ result: inout [[Int]]) {
+        
+        for n in nums {
+            if visited.contains(n) { continue }
+            
+            var permutation = permutation
+            permutation.append(n)
+            
+            var visited = visited
+            visited.insert(n)
+        
+            permute(nums, index - 1, permutation, visited, &result)
+        }
+        
+        if index == 0 {
+            result.append(permutation)
+        }
+        
+    }
