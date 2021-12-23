7a30157ae5e34b6e2116225b5d46f41f0b1ce080
sanghun0724 <sanghun0724@naver.com>
2021-12-23 22:35:02 +0900

    Letter Combinations of a Phone Number

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,25 +1,61 @@
-func permute(_ nums: [Int]) -> [[Int]] {
-        var result = [[Int]]()
-        permute(nums, nums.count, [Int](), Set<Int>(), &result)
-        return result
+ import Foundation
+ 
+ var tmp = 0
+ func digitsString(digits:String) -> [String] {
+    var res = [String]()
+    for i in 2...9 {
+        if i == Int(digits)! {
+            tmp = (i-2)*3
+            if i == 7 {
+            res += [String(UnicodeScalar(97+tmp)!),String(UnicodeScalar(98+tmp)!),String(UnicodeScalar(99+tmp)!),String(UnicodeScalar(100+tmp)!) ]
+            } else if i == 9 {
+                res += [String(UnicodeScalar(98+tmp)!),String(UnicodeScalar(99+tmp)!),String(UnicodeScalar(100+tmp)!),String(UnicodeScalar(101+tmp)!) ]
+            } else if i == 8 {
+                res += [String(UnicodeScalar(98+tmp)!),String(UnicodeScalar(99+tmp)!),String(UnicodeScalar(100+tmp)!) ]
+            }
+            else {
+            res += [String(UnicodeScalar(97+tmp)!),String(UnicodeScalar(98+tmp)!),String(UnicodeScalar(99+tmp)!) ]
+            }
+        }
+    }
+    return res
+ }
+ 
+ 
+ func letterCombinations(_ digits: String) -> [String] {
+    if digits.isEmpty { return [] }
+    var strArr = [[String]]()
+    var res = [String]()
+    
+    for i in 0..<digits.count {
+        let arg = String(digits[digits.index(digits.startIndex, offsetBy: i)])
+        strArr.append(digitsString(digits: arg))
+    }
+   
+   
+    recursive(strArr, "", &res, 0)
+    
+    
+    
+    return res
+    }
+ 
+ func recursive (_ digitsArr:[[String]],_ str:String,_ res: inout [String],_ idx:Int) {
+    if str.count == digitsArr.count {
+        res.append(str)
+        return
     }
     
-    func permute(_ nums: [Int], _ index: Int, _ permutation: [Int], _ visited: Set<Int>, _ result: inout [[Int]]) {
-        
-        for n in nums {
-            if visited.contains(n) { continue }
-            
-            var permutation = permutation
-            permutation.append(n)
-            
-            var visited = visited
-            visited.insert(n)
-        
-            permute(nums, index - 1, permutation, visited, &result)
+    if digitsArr[idx].count == 4 {
+        for i in 0...3 {
+            recursive(digitsArr, str + digitsArr[idx][i], &res, idx+1)
         }
-        
-        if index == 0 {
-            result.append(permutation)
+    } else {
+        for i in 0...2 {
+            recursive(digitsArr, str + digitsArr[idx][i], &res, idx+1)
         }
-        
     }
+ }
+
+ letterCombinations("9")
+
