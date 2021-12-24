fb2ebbb97919687041c1bcce75cc612197379360
sanghun0724 <sanghun0724@naver.com>
2021-12-24 12:23:51 +0900

    more clear

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,61 +1,36 @@
- import Foundation
- 
- var tmp = 0
- func digitsString(digits:String) -> [String] {
-    var res = [String]()
-    for i in 2...9 {
-        if i == Int(digits)! {
-            tmp = (i-2)*3
-            if i == 7 {
-            res += [String(UnicodeScalar(97+tmp)!),String(UnicodeScalar(98+tmp)!),String(UnicodeScalar(99+tmp)!),String(UnicodeScalar(100+tmp)!) ]
-            } else if i == 9 {
-                res += [String(UnicodeScalar(98+tmp)!),String(UnicodeScalar(99+tmp)!),String(UnicodeScalar(100+tmp)!),String(UnicodeScalar(101+tmp)!) ]
-            } else if i == 8 {
-                res += [String(UnicodeScalar(98+tmp)!),String(UnicodeScalar(99+tmp)!),String(UnicodeScalar(100+tmp)!) ]
-            }
-            else {
-            res += [String(UnicodeScalar(97+tmp)!),String(UnicodeScalar(98+tmp)!),String(UnicodeScalar(99+tmp)!) ]
-            }
-        }
-    }
-    return res
- }
- 
+import Foundation
  
  func letterCombinations(_ digits: String) -> [String] {
     if digits.isEmpty { return [] }
-    var strArr = [[String]]()
     var res = [String]()
+    let dict: [Character : String] =
+            ["2" : "abc",
+            "3" : "def",
+            "4" : "ghi",
+            "5" : "jkl",
+            "6" : "mno",
+            "7" : "pqrs",
+            "8" : "tuv",
+            "9" : "wxyz"]
+    let vCount = digits.count
+    let digitArr = Array(digits)
     
-    for i in 0..<digits.count {
-        let arg = String(digits[digits.index(digits.startIndex, offsetBy: i)])
-        strArr.append(digitsString(digits: arg))
+    func dfs(_ idx:Int,list:[Character]) {
+        if idx == vCount {
+            res.append(list.map{ String($0) }.joined())
+            return
+        }
+        var list = list
+        if let word = dict[digitArr[idx]] {
+            for c in word {
+                list.append(c)
+                dfs(idx+1, list: list)
+                list.removeLast()
+            }
+        }
     }
-   
-   
-    recursive(strArr, "", &res, 0)
-    
-    
-    
+    dfs(0, list: [])
     return res
-    }
- 
- func recursive (_ digitsArr:[[String]],_ str:String,_ res: inout [String],_ idx:Int) {
-    if str.count == digitsArr.count {
-        res.append(str)
-        return
-    }
-    
-    if digitsArr[idx].count == 4 {
-        for i in 0...3 {
-            recursive(digitsArr, str + digitsArr[idx][i], &res, idx+1)
-        }
-    } else {
-        for i in 0...2 {
-            recursive(digitsArr, str + digitsArr[idx][i], &res, idx+1)
-        }
-    }
  }
-
- letterCombinations("9")
-
+ 
+letterCombinations("23")
