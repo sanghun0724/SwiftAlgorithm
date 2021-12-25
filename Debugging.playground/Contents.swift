a02f77b48c1833fefc6f8d113c8e417d0632fca9
sanghun0724 <sanghun0724@naver.com>
2021-12-25 15:18:44 +0900

    nQueen problem

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,36 +1,31 @@
-import Foundation
- 
- func letterCombinations(_ digits: String) -> [String] {
-    if digits.isEmpty { return [] }
-    var res = [String]()
-    let dict: [Character : String] =
-            ["2" : "abc",
-            "3" : "def",
-            "4" : "ghi",
-            "5" : "jkl",
-            "6" : "mno",
-            "7" : "pqrs",
-            "8" : "tuv",
-            "9" : "wxyz"]
-    let vCount = digits.count
-    let digitArr = Array(digits)
-    
-    func dfs(_ idx:Int,list:[Character]) {
-        if idx == vCount {
-            res.append(list.map{ String($0) }.joined())
-            return
+
+var N = Int(readLine()!)!
+var count = 0
+
+func NqueenPromising(_ q:[Int],_ row:Int) -> Bool{
+    if row < 1 { return true }
+    for i in 0..<row {
+        if q[row] == q[i] || row-i == abs(q[row] - q[i]) {
+            return false
         }
-        var list = list
-        if let word = dict[digitArr[idx]] {
-            for c in word {
-                list.append(c)
-                dfs(idx+1, list: list)
-                list.removeLast()
+    }
+    return true
+}
+
+func NQueen(_ q:[Int],_ row:Int) {
+    var q = q
+    for i in 0..<N {
+        q[row] = i
+        if NqueenPromising(q, row) {
+            if row == N-1 {
+                count+=1
+            } else {
+                NQueen(q, row+1)
             }
         }
     }
-    dfs(0, list: [])
-    return res
- }
- 
-letterCombinations("23")
+}
+var q = Array(repeating: 0, count: N)
+
+NQueen(q, 0)
+print(count)
