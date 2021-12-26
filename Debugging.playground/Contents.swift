8b68d79442ff38b1ea387f7dc6a5c0f3e0dc8acb
sanghun0724 <sanghun0724@naver.com>
2021-12-26 16:39:45 +0900

    스타터와링크

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,31 +1,51 @@
+var n = Int(readLine()!)!
+var graph = [[Int]]()
+for _ in 1...n {
+    graph.append(Array(readLine()!.split(separator: " ").map{ Int(String($0))!}))
+}
+var pick = [Int](repeating: 0, count: 20)
+var teamA = [Int](repeating: 0, count: 10)
+var teamB = [Int](repeating: 0, count: 10)
+var res = Int.max
 
-var N = Int(readLine()!)!
-var count = 0
-
-func NqueenPromising(_ q:[Int],_ row:Int) -> Bool{
-    if row < 1 { return true }
-    for i in 0..<row {
-        if q[row] == q[i] || row-i == abs(q[row] - q[i]) {
-            return false
+func calculate() {
+    var teamAsize = 0
+    var teamBsize = 0
+    for i in 0..<n {
+        if pick[i] == 0 {
+            teamA[teamAsize] = i
+            teamAsize+=1
+        } else {
+            teamB[teamBsize] = i
+            teamBsize+=1
         }
     }
-    return true
-}
-
-func NQueen(_ q:[Int],_ row:Int) {
-    var q = q
-    for i in 0..<N {
-        q[row] = i
-        if NqueenPromising(q, row) {
-            if row == N-1 {
-                count+=1
-            } else {
-                NQueen(q, row+1)
-            }
+    var sumA = 0 , sumB = 0
+    for i in 0..<n / 2 {
+        for j in i+1..<n / 2 {
+            sumA += graph[teamA[i]][teamA[j]] + graph[teamA[j]][teamA[i]]
+            sumB += graph[teamB[i]][teamB[j]] + graph[teamB[j]][teamB[i]]
         }
     }
+    if res > abs(sumA - sumB) {
+        res = abs(sumA - sumB)
+    }
 }
-var q = Array(repeating: 0, count: N)
 
-NQueen(q, 0)
-print(count)
+
+func dfs(_ cur:Int,_ pickCount:Int) {
+    if pickCount == (n/2) {
+        calculate()
+        return
+    }
+    
+    for i in cur..<n {
+        pick[i] = 1
+        dfs(i+1,pickCount+1)
+        pick[i] = 0
+    }
+    
+}
+
+dfs(0,0)
+print(res)
