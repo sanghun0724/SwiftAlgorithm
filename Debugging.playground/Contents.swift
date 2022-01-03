626166457cf275084a52e60ac86d3ba5b2efc40a
sanghun0724 <sanghun0724@naver.com>
2022-01-03 23:26:27 +0900

    백준 다이어트 통과

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,4 +1,3 @@
-
 let n = Int(readLine()!)!
 let standard = Array(readLine()!).split(separator: " ").map { Int(String($0))! }
 var map = [[Int]]()
@@ -18,51 +17,45 @@ struct node {
     var val:Int
 }
 
-func backTracking(_ gain:[node]) {
-    if check(gain) {
+func backTracking(_ idx:Int) {
+    check()
+    if  idx == n {
         return
     }
-    var gain = gain
-    for i in 0..<map.count {
-        if !visitied[i] {
-            gain.append(node(num: i, mp: map[i][0], mf: map[i][1], ms: map[i][2], mu: map[i][3], val: map[i][4]))
-            visitied[i] = true
-            backTracking(gain)
-            gain.removeLast()
-            visitied[i] = false
-        }
-    }
+        visitied[idx] = true
+        backTracking(idx+1)
+        visitied[idx] = false
+        backTracking(idx+1)
 }
 
-func check(_ arr:[node]) -> Bool {
-    if arr.isEmpty {
-        return false
-    }
+func check() {
+  
     var num = [Int]()
     var mp = 0
     var mf = 0
     var ms = 0
     var mu = 0
     var val = 0
-    for i in 0..<arr.count {
-        mp += arr[i].mp
-        mf += arr[i].mf
-        ms += arr[i].ms
-        mu += arr[i].mu
-        val += arr[i].val
-        num.append(arr[i].num)
+    for i in 0..<n {
+        if visitied[i] {
+            mp += map[i][0]
+            mf += map[i][1]
+            ms += map[i][2]
+            mu += map[i][3]
+            val += map[i][4]
+            num.append(i)
+        }
+       
     }
     if mp >= standard[0] && mf >= standard[1] && ms >= standard[2] && mu >= standard[3]  {
         if minimum > val {
             minimum = val
             res = num
         }
-        return true
     }
-    return false
 }
-backTracking([])
-print(minimum == Int.max ? 0 : minimum)
+backTracking(0)
+print(minimum == Int.max ? -1 : minimum)
 res.sort()
 res.forEach { (val) in
     print(val+1,terminator: " ")
