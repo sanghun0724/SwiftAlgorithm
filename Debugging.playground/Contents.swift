5d2740f463287b2b509e288b979b9afdc52c09a9
sanghun0724 <sanghun0724@naver.com>
2022-01-05 17:26:34 +0900

    Jump DP

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,45 +1,30 @@
-//1932
-let n = Int(readLine()!)!
+
+
+let N = Int(readLine()!)!
 var matrix = [[Int]]()
-for _ in 1...n {
-    matrix.append(readLine()!.split(separator: " ").map{ Int($0)!})
+for _ in 1...N {
+    matrix.append(readLine()!.split(separator: " ").map{ Int($0)! })
 }
+var visitied = [[Int]](repeating: [Int](repeating: 0, count: N), count: N)
 
+visitied[0][0] = 1
 
-for i in 1..<matrix.count {
-    for j in 0..<matrix[i].count {
-        if j == 0 {
-            matrix[i][j] = matrix[i-1][j] + matrix[i][j]
-        } else if j == matrix[i].count-1 {
-            matrix[i][j] = matrix[i-1][j-1] + matrix[i][j]
-        } else {
-        matrix[i][j] = max(matrix[i-1][j], matrix[i-1][j-1]) + matrix[i][j]
+for i in 0..<N {
+    for j in 0..<N {
+        if i == N-1 && j == N-1 || visitied[i][j] == 0 { continue }
+        
+        let jump = matrix[i][j]
+        let row = jump + i
+        let col = jump + j
+        
+        if row < N {
+            visitied[row][j] += visitied[i][j]
+        }
+        
+        if col < N {
+            visitied[i][col] += visitied[i][j]
+        }
     }
 }
-}
-print(matrix.last!.max()!)
-
-
-
-
-//9095
-let T = Int(readLine()!)!
-var nums = [Int]()
-(1...T).forEach { _ in
-    nums.append(Int(readLine()!)!)
-}
-
-var dp = [Int](repeating: -1, count: 11)
-dp[0] = 0
-dp[1] = 1
-dp[2] = 2
-dp[3] = 4
-
-for i in 4..<11 {
-    dp[i] = dp[i-3] + dp[i-2] + dp[i-1]
-}
-
-
-for num in nums {
-    print(dp[num])
-}
+print(visitied)
+print(visitied[N-1][N-1])
