3c0779413bf5e679ccac5c9bd496de9d7ea82707
sanghun0724 <sanghun0724@naver.com>
2022-01-10 21:32:35 +0900

    간단 그리디 동전0

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,29 +1,17 @@
+//간단 greedy
 let f = readLine()!.split(separator: " ").map { Int($0)! }
-let h = f[0]
-let w = f[1]
-let matrix = readLine()!.split(separator: " ").map { Int($0)! }
-var map = [[Bool]](repeating: [Bool](repeating: false, count: w), count: h)
-var res = 0
-
-for c in 0..<w {
-        for r in (h-matrix[c])..<h {
-            map[r][c] = true
-        }
+let N = f[0]
+var K = f[1]
+var coins = [Int]()
+for _ in 1...N {
+    coins.append(Int(readLine()!)!)
 }
-
-var tmp = false
-var loc = 0
-for r in 0..<h {
-    for c in 0..<w  {
-        if map[r][c] && tmp == false {
-            tmp = true
-            loc = c
-        } else if map[r][c] == true && tmp == true {
-            res += c - loc - 1
-            loc = c
-        }
+var res = 0
+coins.reverse()
+for coin in coins {
+    while K >= coin {
+        K -= coin
+        res+=1
     }
-    tmp = false
-    loc = 0
 }
 print(res)
