8690ab0b9054c40010c6f9cb0ecb34bf37ee9c17
sanghun0724 <sanghun0724@naver.com>
2022-01-10 22:44:40 +0900

    greedy interval 문제 백준버전

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,17 +1,22 @@
-//간단 greedy
-let f = readLine()!.split(separator: " ").map { Int($0)! }
-let N = f[0]
-var K = f[1]
-var coins = [Int]()
+let N = Int(readLine()!)!
+var map = [[Int]]()
 for _ in 1...N {
-    coins.append(Int(readLine()!)!)
+    map.append(readLine()!.split(separator: " ").map{ Int($0)! })
 }
-var res = 0
-coins.reverse()
-for coin in coins {
-    while K >= coin {
-        K -= coin
-        res+=1
+map.sort { val1, val2 in
+    if val1[1] == val2[1] {
+        return  val1[0] < val2[0]
+    } else {
+        return val1[1] < val2[1]
     }
 }
+
+var res = 0
+var t = 0
+for i in 0..<N {
+    if t > map[i][0] { continue }
+    res+=1
+    t = map[i][1]
+}
+
 print(res)
