08fbc95b2de94a65c90cdccb5ee11e7e6791ee1b
sanghun0724 <sanghun0724@naver.com>
2022-01-11 20:31:49 +0900

    백준 그리디 멀티탭 스케줄링

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,22 +1,53 @@
-let N = Int(readLine()!)!
-var map = [[Int]]()
-for _ in 1...N {
-    map.append(readLine()!.split(separator: " ").map{ Int($0)! })
-}
-map.sort { val1, val2 in
-    if val1[1] == val2[1] {
-        return  val1[0] < val2[0]
-    } else {
-        return val1[1] < val2[1]
-    }
-}
 
+let f = readLine()!.split(separator: " ").map { Int($0)! }
+let N = f[0]
+let K = f[1]
+let objects = readLine()!.split(separator: " ").map {Int($0)!}
+
+var tap = [Int]()
 var res = 0
-var t = 0
-for i in 0..<N {
-    if t > map[i][0] { continue }
-    res+=1
-    t = map[i][1]
+var check = false
+for i in 0..<K {
+    //1
+    if tap.contains(objects[i]) { continue }
+    
+    var pVal = -1
+    var removeVal = -1
+    //2
+    if tap.count == N {
+        var last = 0
+        for idx in 0..<tap.count {
+            if !(objects[i+1..<K].contains(tap[idx])) {
+                pVal = tap[idx]
+                break
+            } else {
+                let index = objects[i+1..<K].firstIndex(of: tap[idx])!
+                last = max(last,index)
+                let laterUse = objects[last]
+                removeVal = laterUse
+            }
+        }
+        if pVal != -1 && removeVal != -1 { removeVal = -1 }
+    }
+    
+    if removeVal != -1 {
+        let idx = tap.firstIndex(of: removeVal)!
+        tap.remove(at: idx)
+        res+=1
+    }
+    if pVal != -1 {
+        let idx = tap.firstIndex(of: pVal)!
+        tap.remove(at: idx)
+        res+=1
+    }
+    if tap.count < N {
+        tap.append(objects[i])
+    }
+    if i == K-1 {
+        if !tap.contains(objects[i]) {
+            res+=1
+        }
+    }
 }
 
 print(res)
