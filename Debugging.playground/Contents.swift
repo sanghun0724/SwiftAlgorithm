861003dd8fb5807dca89a2e66d7cc09fbe2f2200
sanghun0724 <sanghun0724@naver.com>
2022-01-14 23:44:00 +0900

    백준 사탕게임

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,13 +1,63 @@
 let n = Int(readLine()!)!
-var t = n
-var i = 1
-var res = 0
-var sum = 0
-while t > 0 {
-    t -= i
-    res+=1
-    sum+=i
-    i+=1
+var candys = [[Character]]()
+for _ in 0..<n {
+    candys.append(Array(readLine()!))
 }
-if sum == n { print(res) }
-else { print(res-1)}
+var res = 1
+
+func move() {
+    for i in 0..<n {
+        for j in 0..<n-1 {
+            var map = candys
+            var tmp = map[i][j]
+                map[i][j] = map[i][j+1]
+                map[i][j+1] = tmp
+                check(map)
+            
+                map = candys
+                tmp = map[j][i]
+                map[j][i] = map[j+1][i]
+                map[j+1][i] = tmp
+                check(map)
+        }
+    }
+}
+
+
+func check(_ matrix:[[Character]]) {
+    var tmpW = 0
+    var tmpH = 0
+    var width = matrix[0][0]
+    var height = matrix[0][0]
+    for i in 0..<n {
+        width = matrix[i][0]
+        height = matrix[0][i]
+        tmpH = 0
+        tmpW = 0
+        for j in 0..<n {
+            //width
+            if matrix[i][j] == width {
+                tmpW+=1
+                width = matrix[i][j]
+            } else {
+                res = max(res, tmpW)
+                tmpW = 1
+                width = matrix[i][j]
+            }
+            
+            if matrix[j][i] == height {
+                tmpH+=1
+                height = matrix[j][i]
+            } else {
+                res = max(res, tmpH)
+                tmpH = 1
+                height = matrix[j][i]
+            }
+            res = max(res, tmpW)
+            res = max(res, tmpH)
+        }
+    }
+}
+
+move()
+print(res)
