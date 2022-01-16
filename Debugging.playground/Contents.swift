960ff2dd97454110d821fee5d177c86f08733c55
sanghun0724 <sanghun0724@naver.com>
2022-01-16 16:57:03 +0900

    백준 큐

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,18 +1,57 @@
-let f = readLine()!.split(separator: " ").map { Int($0)! }
-let n = f[0]
-var k = f[1]
-var coins = [Int]()
-for _ in 1...n {
-    coins.append(Int(readLine()!)!)
-}
-var dp  = [Int](repeating: k+1, count: k+1)
-dp[0] = 0
-for i in 1..<dp.count {
-    for coin in coins {
-        if i - coin >= 0  {
-            dp[i] = min(dp[i], dp[i - coin] + 1)
-        }
+let n = Int(readLine()!)!
+//1
+var arr = [Int](repeating: -1, count: 1024)
+var idx = 0
+var i = 0
+while idx <= 9876543210 {
+    if isDown(idx) {
+        arr[i] = idx
+        i+=1
     }
+    idx+=1
 }
 
-print(dp[k] == k+1 ? -1 : dp[k])
+func isDown(_ val:Int) -> Bool {
+    var val = val
+    var tmp = -1
+    while val != 0 {
+       let rest = val%10
+        val = val/10
+        if tmp >= rest { return false }
+        tmp = rest
+    }
+    return true
+}
+
+if n <= 1022 {
+    print(arr[n])
+} else  {
+    print(-1)
+}
+
+//2
+let n = Int(readLine()!)!
+var cnt = 1
+var q = [Int](1...9)
+var cur = 0
+
+
+func check() -> Int {
+    if n == 0 { return 0 }
+    while cur < q.count {
+        let now = q[cur]
+        let last = now % 10
+        cur+=1
+        if n == cnt { return now }
+        if now == 9876543210 {return -1}
+        
+        for idx in 0..<last {
+            let next = now * 10 + idx
+            q.append(next)
+        }
+        cnt+=1
+    }
+    return -1
+}
+print(check())
+
