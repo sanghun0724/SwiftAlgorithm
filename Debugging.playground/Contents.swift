aed6e70a8cee35d69875e09427492d2203e4a02a
sanghun0724 <sanghun0724@naver.com>
2022-01-17 21:47:46 +0900

    백준 구현

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,57 +1,41 @@
 let n = Int(readLine()!)!
-//1
-var arr = [Int](repeating: -1, count: 1024)
-var idx = 0
-var i = 0
-while idx <= 9876543210 {
-    if isDown(idx) {
-        arr[i] = idx
-        i+=1
+var matrix = [[Int]]()
+for _ in 1...n {
+    matrix.append(readLine()!.split(separator: " ").map{Int($0)!})
+}
+let target = (n-1,n-1)
+let dirR = [1,0,1]
+let dirC = [0,1,1]
+var res = 0
+
+func dfs(_ idx:(Int,Int),_ dirction:String) {
+    if idx == target {
+        res+=1
+        return
     }
-    idx+=1
-}
-
-func isDown(_ val:Int) -> Bool {
-    var val = val
-    var tmp = -1
-    while val != 0 {
-       let rest = val%10
-        val = val/10
-        if tmp >= rest { return false }
-        tmp = rest
-    }
-    return true
-}
-
-if n <= 1022 {
-    print(arr[n])
-} else  {
-    print(-1)
-}
-
-//2
-let n = Int(readLine()!)!
-var cnt = 1
-var q = [Int](1...9)
-var cur = 0
-
-
-func check() -> Int {
-    if n == 0 { return 0 }
-    while cur < q.count {
-        let now = q[cur]
-        let last = now % 10
-        cur+=1
-        if n == cnt { return now }
-        if now == 9876543210 {return -1}
+    
+    
+    for i in 0..<3 {
+      let r = idx.0+dirR[i]
+      let c = idx.1+dirC[i]
         
-        for idx in 0..<last {
-            let next = now * 10 + idx
-            q.append(next)
+        if i == 1 {
+            if  r < matrix.count && c < matrix[0].count,matrix[r][c] == 0 && dirction != "col" {
+                dfs((r,c), "row")
+               
+            }
+        } else if i == 0 {
+            if  r < matrix.count && c < matrix[0].count,matrix[r][c] == 0 && dirction != "row" {
+                dfs((r,c), "col")
+                
+            }
+        } else {
+            if  r < matrix.count && c < matrix[0].count,matrix[r][c] == 0 ,matrix[r-1][c] == 0 && matrix[r][c-1] == 0 {
+                dfs((r,c), "asy")
+               
+            }
         }
-        cnt+=1
     }
-    return -1
 }
-print(check())
-
+dfs((0,1),"row")
+print(res)
