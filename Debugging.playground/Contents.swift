41c4575c49eb4894024398ec54a77cd3a516b210
sanghun0724 <sanghun0724@naver.com>
2022-01-20 17:18:36 +0900

    백준 DFS

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,45 +1,22 @@
 let f = readLine()!.split(separator: " ").map { Int($0)! }
-let n = f[0]
-let m = f[1]
-let k = f[2]
-var loc = [[Int]]()
-for _ in 1...k {
-    loc.append(readLine()!.split(separator: " ").map{Int($0)!})
-}
-let dirR = [1,0,-1,0]
-let dirC = [0,1,0,-1]
+let A = f[0]
+let B = f[1]
+
+//DFS
+var res = Int.max
 
 
-//BFS
-var res = 0
-var visitied = [[Bool]](repeating: [Bool](repeating: false, count: m+1), count: n+1)
-
-
-func bfs(_ start:[Int]) {
-    var queue = [start]
-    visitied[start[0]][start[1]] = true
-    var tmp = 1
-    var idx = 0
-    while idx < queue.count {
-        let cur = queue[idx]
-        
-        for i in 0..<4 {
-            let r = cur[0] + dirR[i]
-            let c = cur[1] + dirC[i]
-            if loc.contains([r,c]) && visitied[r][c] == false {
-                visitied[r][c] = true
-                tmp+=1
-                queue.append([r,c])
-            }
-        }
-        idx+=1
+func dfs(_ idx:Int,_ value:Int) {
+    if value == B {
+        res = min(res,idx)
+    } else if value > B {
+        return
     }
-    res = max(res,tmp)
+    
+    dfs(idx+1, value * 2)
+    dfs(idx+1, value * 10 + 1)
+    
 }
-
-
-for val in loc {
-    bfs(val)
-}
-
-print(res)
+dfs(0, A)
+if res == Int.max { print(-1)}
+else { print(res+1) }
