f4b099536dc308a549b4d88aa93070a1993e00cc
sanghun0724 <sanghun0724@naver.com>
2022-01-24 16:15:36 +0900

    숨박꼭질2 BFS

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,22 +1,33 @@
 let f = readLine()!.split(separator: " ").map { Int($0)! }
-let A = f[0]
-let B = f[1]
+let n = f[0]
+let k = f[1]
 
-//DFS
-var res = Int.max
+var queue = [(n,0)]
+var minArr = [Int](repeating: Int.max, count: 100001)
+minArr[n] = 0
+var count = 0
 
+var idx = 0
 
-func dfs(_ idx:Int,_ value:Int) {
-    if value == B {
-        res = min(res,idx)
-    } else if value > B {
-        return
+while idx < queue.count {
+    let x = queue[idx].0
+    let dist = queue[idx].1
+    
+    if dist > minArr[k] { break }
+    idx+=1
+    
+    if x == k {
+        minArr[k] = dist
+        count+=1
     }
-    
-    dfs(idx+1, value * 2)
-    dfs(idx+1, value * 10 + 1)
-    
+    let comp = [x*2,x-1,x+1]
+    for v in comp {
+        if 0 <= v && v <= 100000 && (dist + 1) <= minArr[v] {
+            minArr[v] = dist + 1
+            queue.append((v,dist+1))
+        }
+    }
 }
-dfs(0, A)
-if res == Int.max { print(-1)}
-else { print(res+1) }
+
+print(minArr[k])
+print(count)
