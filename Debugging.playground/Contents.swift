41f75eb5a2190f193171a5fdf872f122d9276be9
sanghun0724 <sanghun0724@naver.com>
2022-04-19 17:35:28 +0900

    backjoon 숨박꼭질 3

--- Debugging.playground/Contents.swift
+++ Debugging.playground/Contents.swift
@@ -1,39 +1,35 @@
 import Foundation
 
-let f = readLine()!.split(separator: " ").map { Int($0)! }
-let n = f[0]
-let k = f[1]
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+let n = input[0]
+let k = input[1]
 
-//출발 안했으니 시작은 0
+//출발 안했으니
 var queue = [(n,0)]
-var minArr:Array = [Int](repeating: Int.max, count: 100001)
-minArr[n] = 0
-var count = 0
-var idx = 0
+var visited = [Bool](repeating: false, count: 100001)
 
-while idx < queue.count {
-    let x = queue[idx].0
-    let dist = queue[idx].1
+while !queue.isEmpty {
+    let first = queue.removeFirst()
+    let curLoc = first.0
+    let curCount = first.1
     
-    if dist > minArr[k] { break }
-    idx+=1
+    if curLoc == k {
+       print(curCount)
+        break
+    }
     
-    let comp = [x+1,x-1,x*2]
-    for v in comp {
-        if v <= 0 && v <= 100000 && (dist + 1) <= minArr[v] {
-            minArr[v] = dist + 1
-            queue.append((v,dist+1))
+    let comb = [curLoc*2,curLoc-1,curLoc+1]
+    
+    for combEleMent in comb {
+        if combEleMent >= 0 && combEleMent < 100001 && !visited[combEleMent] {
+            visited[combEleMent] = true
+            if combEleMent == curLoc*2 {
+                queue.append((combEleMent,curCount))
+            } else {
+                queue.append((combEleMent,curCount+1))
+            }
+           
         }
+        
     }
 }
-
-
-
-
-
-
-
-
-
-
-
